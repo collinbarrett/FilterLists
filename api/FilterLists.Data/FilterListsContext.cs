03a0cf15c37d8e5c4b4e34d99947db91972c6c35
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-04-08 08:38:58 -0500

    minor updates

---
+++ api/FilterLists.Data/FilterListsContext.cs
@@ -0,0 +1,19 @@
+﻿using FilterLists.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilterLists.Data
+{
+    public class FilterListsContext : DbContext
+    {
+        public FilterListsContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public FilterListsContext()
+        {
+        }
+
+        public DbSet<List> Lists { get; set; }
+    }
+}
