7397abadec9abfddb054256cf650090d99935bfd
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-04-08 09:24:05 -0500

    add EF Generic Repository Pattern

---
+++ api/FilterLists.Models/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿//https://code.msdn.microsoft.com/Generic-Repository-Pattern-f133bca4/sourcecode?fileId=164016&pathId=1938870460
+
+using System;
+
+namespace FilterLists.Models
+{
+    public class BaseEntity
+    {
+        public long Id { get; set; }
+        public DateTime AddedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
