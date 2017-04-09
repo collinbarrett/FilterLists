dffe0733e626811e7ad376603ec593390ff20de3
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-04-09 15:00:52 -0500

    code-first migration

---
+++ api/FilterLists.Data/Migrations/20170409192934_FilterListsMigration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FilterLists.Data.Migrations
+{
+    public partial class FilterListsMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                "List",
+                table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    AddedDate = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    DescriptionSourceUrl = table.Column<string>(nullable: true),
+                    DonateUrl = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    ForumUrl = table.Column<string>(nullable: true),
+                    HomeUrl = table.Column<string>(nullable: true),
+                    IssuesUrl = table.Column<string>(nullable: true),
+                    ModifiedDate = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    ViewUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table => { table.PrimaryKey("PK_List", x => x.Id); });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                "List");
+        }
+    }
+}
