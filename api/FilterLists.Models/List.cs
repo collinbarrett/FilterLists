ff298cbd67236cb18d6dd4c77292340dc7c48294
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-04-10 06:52:54 -0500

    new EF migration for List model

--- api/FilterLists.Models/List.cs
+++ api/FilterLists.Models/List.cs
@@ -7,36 +7,36 @@ namespace FilterLists.Models
         [MaxLength(512)]
         public string Description { get; set; }
 
-        [MaxLength(6)]
-        [MinLength(2083)]
+        [MaxLength(2083)]
+        [MinLength(6)]
         public string DescriptionSourceUrl { get; set; }
 
-        [MaxLength(6)]
-        [MinLength(2083)]
+        [MaxLength(2083)]
+        [MinLength(6)]
         public string DonateUrl { get; set; }
 
-        [MaxLength(7)]
-        [MinLength(254)]
+        [MaxLength(254)]
+        [MinLength(7)]
         public string Email { get; set; }
 
-        [MaxLength(6)]
-        [MinLength(2083)]
+        [MaxLength(2083)]
+        [MinLength(6)]
         public string ForumUrl { get; set; }
 
-        [MaxLength(6)]
-        [MinLength(2083)]
+        [MaxLength(2083)]
+        [MinLength(6)]
         public string HomeUrl { get; set; }
 
-        [MaxLength(6)]
-        [MinLength(2083)]
+        [MaxLength(2083)]
+        [MinLength(6)]
         public string IssuesUrl { get; set; }
 
         [MaxLength(64)]
         public string Name { get; set; }
 
         [Required]
-        [MaxLength(6)]
-        [MinLength(2083)]
+        [MaxLength(2083)]
+        [MinLength(6)]
         public string ViewUrl { get; set; }
     }
 }
