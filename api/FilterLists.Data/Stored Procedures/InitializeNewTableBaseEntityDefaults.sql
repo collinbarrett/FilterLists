f181dafeaf914ea7e2102b80c7bf0f65fe577e37
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-04-10 17:49:21 -0500

    add .sql script for setting default date values on base entity

---
+++ api/FilterLists.Data/Stored Procedures/InitializeNewTableBaseEntityDefaults.sql
@@ -0,0 +1,8 @@
+--TODO: Possibly convert to stored procedure
+ALTER TABLE
+    filterlistsdata.List MODIFY COLUMN AddedDateUtc DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL;
+ALTER TABLE
+    filterlistsdata.List MODIFY COLUMN ModifiedDateUtc DATETIME DEFAULT NULL
+ON
+UPDATE
+    CURRENT_TIMESTAMP;
