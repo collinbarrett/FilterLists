940f0b8327d0ac30d804d37dffa3a7a28cd91f99
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-04-10 08:18:10 -0500

    annotation updates

---
+++ api/FilterLists.Data/Migrations/20170410130859_SetDefaultDateValue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FilterLists.Data.Migrations
+{
+    public partial class SetDefaultDateValue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- api/FilterLists.Models/BaseEntity.cs
+++ api/FilterLists.Models/BaseEntity.cs
@@ -1,6 +1,7 @@
 ﻿//https://code.msdn.microsoft.com/Generic-Repository-Pattern-f133bca4/sourcecode?fileId=164016&pathId=1938870460
 
 using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -14,10 +15,13 @@ namespace FilterLists.Models
 
         [Required]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        //TODO: Implement http://stackoverflow.com/a/38102266/2343739 so DefaultValue takes effect in db automatically
+        [DefaultValue("CURRENT_TIMESTAMP")]
         public DateTime AddedDateUtc { get; set; }
 
         [Required]
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        [DefaultValue("CURRENT_TIMESTAMP")]
         public DateTime ModifiedDateUtc { get; set; }
     }
 }
