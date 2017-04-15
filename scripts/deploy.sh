6bc830cdf47ea5fae6fe11f9169c71a1a7b9ef44
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-04-15 13:34:41 -0500

    force pseudo-tty

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
  #!/usr/bin/env bash
-sshpass -p $FTP_PASSWORD ssh -o StrictHostKeyChecking=no $FTP_USER@$FTP_HOST << !
+sshpass -p $FTP_PASSWORD ssh -tt -o StrictHostKeyChecking=no $FTP_USER@$FTP_HOST << !
    cd $FTP_DIR
    put -r /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/*
    bye
