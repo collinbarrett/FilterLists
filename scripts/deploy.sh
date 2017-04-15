03880d662dcf80b6159145dfd7c4dcb3008f6914
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-04-15 13:32:59 -0500

    move dir by cd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
  #!/usr/bin/env bash
-sshpass -p $FTP_PASSWORD ssh -o StrictHostKeyChecking=no $FTP_USER@$FTP_HOST:$FTP_DIR << !
+sshpass -p $FTP_PASSWORD ssh -o StrictHostKeyChecking=no $FTP_USER@$FTP_HOST << !
+   cd $FTP_DIR
    put -r /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/*
    bye
