07261ec0e28f1514a9b2d206f0efa10cfce11797
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-04-15 13:14:17 -0500

    try using sftp

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,7 @@
  #!/usr/bin/env bash
+sftp --password=$FTP_PASSWORD $FTP_USER@$FTP_HOST:$FTP_DIR
 for file in /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/*
 do
-  if [ "$file" != "appsettings.Production.json" ] && [ "$file" != "appsettings.Development.json" ]
-  then
-    echo "Uploading $f"
-    curl -T ${file} ftp://$FTP_HOST$FTP_DIR${file} --user $FTP_USER:$FTP_PASSWORD
-  fi
+  echo "Uploading $file"
+  put ${file}
 done
