7252380c12ca37a1595be885f112bd87bc58dd55
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-04-15 12:11:34 -0500

    skip_cleanup, encrypt FTP_DIR

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
  #!/usr/bin/env bash
 for file in /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/*
 do
-   curl -T ${file} sftp://$FTP_HOST%/var/www/api.filterlists/${file} --user $FTP_USER:$FTP_PASSWORD
+   curl -T ${file} sftp://$FTP_HOST$FTP_DIR${file} --user $FTP_USER:$FTP_PASSWORD
 done
