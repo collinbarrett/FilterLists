969a5123a7e3d420c36582feffc1e4e3f08cf470
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-04-15 13:40:29 -0500

    try scp

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,2 @@
  #!/usr/bin/env bash
-sshpass -p $FTP_PASSWORD ssh -tt -o StrictHostKeyChecking=no $FTP_USER@$FTP_HOST << !
-   cd $FTP_DIR
-   put -r /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/*
-   bye
+sshpass -p $FTP_PASSWORD scp -r /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/* $FTP_USER@$FTP_HOST:$FTP_DIR
