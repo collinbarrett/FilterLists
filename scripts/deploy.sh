e61336bfcc390103cd2fe059987c69e3688e9365
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-04-15 12:36:39 -0500

    tweak deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,8 @@
  #!/usr/bin/env bash
 for file in /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/*
 do
-   curl -T ${file} sftp://$FTP_HOST$FTP_DIR${file} --user $FTP_USER:$FTP_PASSWORD
+  if [ "$file" != "appsettings.Production.json" ] && [ "$file" != "appsettings.Development.json" ]
+  then
+    echo "Uploading $f"
+    curl -T ${file} ftp://$FTP_HOST$FTP_DIR${file} --user $FTP_USER:$FTP_PASSWORD
 done
