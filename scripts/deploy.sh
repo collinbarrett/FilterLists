d44138c6fe2ba6ffdbff04f79ec1d8b34785f4ae
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-04-15 13:16:57 -0500

    prefix "put" with "sftp"

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@ sftp --password=$FTP_PASSWORD $FTP_USER@$FTP_HOST:$FTP_DIR
 for file in /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/*
 do
   echo "Uploading $file"
-  put ${file}
+  sftp put ${file}
 done
