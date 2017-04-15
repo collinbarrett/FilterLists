0e1a2fe0ede58b69c10294808010b48e79c27a5e
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-04-15 13:48:19 -0500

    try adding verbose scp output

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
  #!/usr/bin/env bash
-sshpass -p $FTP_PASSWORD scp -r /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/* $FTP_USER@$FTP_HOST:$FTP_DIR
+sshpass -p $FTP_PASSWORD scp -v -r /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/* $FTP_USER@$FTP_HOST:$FTP_DIR
