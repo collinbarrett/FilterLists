75be9ffb64503872b9990cf965310f129c00e73e
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-04-15 11:59:56 -0500

    remove unused var

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,4 @@
  #!/usr/bin/env bash
-set ftpdir=/var/www/api.filterlists
-
 for file in /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/*
 do
    curl -T ${file} sftp://$FTP_HOST%/var/www/api.filterlists/${file} --user $FTP_USER:$FTP_PASSWORD
