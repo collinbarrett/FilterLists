1ce9e50efe46169228e3661f586d36a5a01f7be9
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-04-15 11:52:28 -0500

    tweak deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,7 @@
 set localdir=/home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/
 set ftpdir=/var/www/api.filterlists
 
-setlocal enableDelayedExpansion
-
-for /F %%x in ('dir /B/D %localdir%') do (
-  set FILENAME=%localdir%\%%x
-  curl -T !FILENAME! sftp://$FTP_HOST/%ftpdir%/ --user $FTP_USER:$FTP_PASSWORD
-)
+for file in %localdir%*
+do
+   curl -T ${file} sftp://$FTP_HOST%ftpdir%/${file} --user $FTP_USER:$FTP_PASSWORD
+done
