7f20574cdab3e7680c5fed3fa780f8dd5716ab7d
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-04-15 13:29:33 -0500

    disable StrictHostKeyChecking

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,4 @@
  #!/usr/bin/env bash
-export SSHPASS=$FTP_PASSWORD
-sshpass -e sftp -oBatchMode=no -b - $FTP_USER@$FTP_HOST:$FTP_DIR << !
+sshpass -p $FTP_PASSWORD ssh -o StrictHostKeyChecking=no $FTP_USER@$FTP_HOST:$FTP_DIR << !
    put -r /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/*
    bye
