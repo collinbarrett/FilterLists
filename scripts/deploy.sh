4e474907f422dbafe6bfac970d22e3fa125fb379
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-04-16 13:54:54 -0500

    add runtime to publish path for self-contained deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
  #!/usr/bin/env bash
  #TODO: use ssh keys rather than pw
-sshpass -p $FTP_PASSWORD scp -o StrictHostKeyChecking=no -r /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/* $FTP_USER@$FTP_HOST:$FTP_DIR
+sshpass -p $FTP_PASSWORD scp -o StrictHostKeyChecking=no -r /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/ubuntu.16.04-x64/* $FTP_USER@$FTP_HOST:$FTP_DIR
