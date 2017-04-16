ca23f5e0c1f298a3e7bfb7b45eeabbe515d6372d
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-04-16 15:00:46 -0500

    typo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
  #!/usr/bin/env bash
  #TODO: use ssh keys rather than pw
- sudo chmod a+x /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/ubuntu.16.04-x64/FilterLists.Api.dll
+sudo chmod a+x /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/ubuntu.16.04-x64/FilterLists.Api.dll
 sshpass -p $FTP_PASSWORD scp -o StrictHostKeyChecking=no -r /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/ubuntu.16.04-x64/* $FTP_USER@$FTP_HOST:$FTP_DIR
