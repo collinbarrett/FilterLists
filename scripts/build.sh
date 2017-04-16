1f720baf5c1fd6aaf9699f710b8306e9cbbffd10
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-04-16 15:38:05 -0500

    remove framework switch on publish

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,2 @@
  #!/usr/bin/env bash
-sudo dotnet restore && sudo dotnet publish -c Release -r ubuntu.16.04-x64 -f netcoreapp1.1
+sudo dotnet restore && sudo dotnet publish -c Release -r ubuntu.16.04-x64
