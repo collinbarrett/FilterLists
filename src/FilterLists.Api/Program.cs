21dccf4c9f1856078c21e5889b44df476b986133
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-04-17 15:29:05 -0500

    add back commented dependencies, optimize references

--- src/FilterLists.Api/Program.cs
+++ src/FilterLists.Api/Program.cs
@@ -1,24 +1,21 @@
 ﻿using System.IO;
-//using JetBrains.Annotations;
+using JetBrains.Annotations;
 using Microsoft.AspNetCore.Hosting;
 
 namespace FilterLists.Api
 {
-    //[UsedImplicitly]
+    [UsedImplicitly]
     public class Program
     {
-        //[UsedImplicitly]
+        [UsedImplicitly]
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .UseUrls("http://localhost:5123")
-                //.UseApplicationInsights()
                 .Build();
-
             host.Run();
         }
     }
