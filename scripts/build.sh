6fb2f034ce7858f2fe5c1b74d13b48219481354c
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-04-18 13:10:30 -0500

    add back dotnet restore

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+dotnet restore
 dotnet test /test/FilterLists.Data.Tests/FilterLists.Data.Tests.csproj
 dotnet test /test/FilterLists.Services.Tests/FilterLists.Services.Tests.csproj
 dotnet publish -c release -r ubuntu.16.04-x64
