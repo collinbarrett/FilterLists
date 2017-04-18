9305a5a657fb9d64ffacaf16f2a32f4a34ae8181
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-04-18 13:00:40 -0500

    specify projs. to dotnet test

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 dotnet restore
 dotnet publish -c release -r ubuntu.16.04-x64
-dotnet test
+dotnet test /test/FilterLists.Data.Tests/FilterLists.Data.Tests.csproj
+dotnet test /test/FilterLists.Services.Tests/FilterLists.Services.Tests.csproj
