5ffcde931fafe827e0f9708b89177534980c2118
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-06-25 10:06:13 -0500

    remove DataLoad dep., instead deploy separately

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,3 +3,4 @@
 chmod a+x /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/ubuntu.16.04-x64/publish/FilterLists.Api.dll
 chmod a+x /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/ubuntu.16.04-x64/publish/FilterLists.DataLoad.exe
 sshpass -p $FTP_PASSWORD scp -o StrictHostKeyChecking=no -r /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/ubuntu.16.04-x64/publish/* $FTP_USER@$FTP_HOST:$FTP_DIR
+sshpass -p $FTP_PASSWORD scp -o StrictHostKeyChecking=no -r /home/travis/build/collinbarrett/FilterLists/src/FilterLists.DataLoad/bin/Release/netcoreapp1.1/ubuntu.16.04-x64/publish/* $FTP_USER@$FTP_HOST:$FTP_DIR
