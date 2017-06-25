408b3a5c9d149c0893f5b49ef98917ab3fcc1913
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-06-25 09:26:43 -0500

    try fixing DataLoad in Travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 #TODO: use ssh keys rather than pw
 chmod a+x /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/ubuntu.16.04-x64/publish/FilterLists.Api.dll
+chmod a+x /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/ubuntu.16.04-x64/publish/FilterLists.DataLoad.exe
 sshpass -p $FTP_PASSWORD scp -o StrictHostKeyChecking=no -r /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp1.1/ubuntu.16.04-x64/publish/* $FTP_USER@$FTP_HOST:$FTP_DIR
