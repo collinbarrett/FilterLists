8eec6e97e8132f44233576b11614139760f77afb
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-10-16 17:09:36 -0500

    Revert "removing DataLoad, pivoting to EF seeding from csv instead"
    This reverts commit 6a884a00f056e65f42b7f432c518f2b0869071a7.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,3 +2,5 @@
 #TODO: use ssh keys rather than pw
 chmod a+x /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp2.0/ubuntu.16.04-x64/publish/FilterLists.Api.dll
 sshpass -p $FTP_PASSWORD scp -o StrictHostKeyChecking=no -r /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp2.0/ubuntu.16.04-x64/publish/* $FTP_USER@$FTP_HOST:$FTP_DIR
+chmod a+x /home/travis/build/collinbarrett/FilterLists/src/FilterLists.DataLoad/bin/Release/netcoreapp2.0/ubuntu.16.04-x64/publish/FilterLists.DataLoad
+sshpass -p $FTP_PASSWORD scp -o StrictHostKeyChecking=no -r /home/travis/build/collinbarrett/FilterLists/src/FilterLists.DataLoad/bin/Release/netcoreapp2.0/ubuntu.16.04-x64/publish/* $FTP_USER@$FTP_HOST:$FTP_DIR
---
+++ src/FilterLists.DataLoad/Program.cs
@@ -0,0 +1,20 @@
+﻿using FilterLists.Services.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FilterLists.DataLoad
+{
+    //TODO: trigger on job or ideally via GitHub webhook when any .csv is updated in master
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IServiceCollection services = new ServiceCollection();
+            var startup = new Startup();
+            startup.ConfigureServices(services);
+            var serviceProvider = services.BuildServiceProvider();
+
+            var tableService = serviceProvider.GetService<ITableService>();
+            tableService.UpdateTables();
+        }
+    }
+}
