2bb11771f284ad0e4c98470a9037f69f45fd5420
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-10-23 11:59:28 -0500

    initial add swagger/swashbuckle

--- src/FilterLists.Api/Startup.cs
+++ src/FilterLists.Api/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace FilterLists.Api
 {
@@ -22,6 +23,23 @@ namespace FilterLists.Api
             services.AddFilterListsRepositories(Configuration);
             services.AddFilterListsServices();
             services.AddFilterListsApi();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Title = "FilterLists API",
+                        Version = "v1",
+                        Description =
+                            "FilterLists is the independent and comprehensive directory of all public filter and hosts lists for advertisements, trackers, malware, and annoyances.",
+                        Contact = new Contact {Url = "https://filterlists.com/contact/"},
+                        License = new License
+                        {
+                            Name = "Use under GPL-3.0",
+                            Url = "https://github.com/collinbarrett/FilterLists/blob/master/LICENSE"
+                        }
+                    });
+            });
         }
 
         public void Configure(IApplicationBuilder app)
@@ -29,7 +47,14 @@ namespace FilterLists.Api
             app.UseForwardedHeaders(new ForwardedHeadersOptions
             {
                 ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-            }).UseMvc();
+            });
+
+            app.UseSwagger().UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "FilterLists API V1");
+            });
+
+            app.UseMvc();
         }
     }
 }
