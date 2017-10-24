127446e47cf8b30f6bd55e493ff5784cf94e155a
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-10-24 17:25:41 -0500

    delete DataLoad (all logic in Services, call from API)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-#TODO: use ssh keys rather than pw
 chmod a+x /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp2.0/ubuntu.16.04-x64/publish/FilterLists.Api.dll
 sshpass -p $FTP_PASSWORD scp -o StrictHostKeyChecking=no -r /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp2.0/ubuntu.16.04-x64/publish/* $FTP_USER@$FTP_HOST:$FTP_DIR
-chmod a+x /home/travis/build/collinbarrett/FilterLists/src/FilterLists.DataLoad/bin/Release/netcoreapp2.0/ubuntu.16.04-x64/publish/FilterLists.DataLoad
-sshpass -p $FTP_PASSWORD scp -o StrictHostKeyChecking=no -r /home/travis/build/collinbarrett/FilterLists/src/FilterLists.DataLoad/bin/Release/netcoreapp2.0/ubuntu.16.04-x64/publish/* $FTP_USER@$FTP_HOST:$FTP_DIR
