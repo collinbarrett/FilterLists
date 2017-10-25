73c4367a341f37c20a5090add98f53febf470edb
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-10-25 17:24:23 -0500

    try removing previous artifacts
    ref #135

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/bash -x
+#!/usr/bin/env bash
+set -x
 sshpass -p $FTP_PASSWORD ssh -o StrictHostKeyChecking=no $FTP_USER@$FTP_HOST 'rm -rf /'$FTP_DIR'/{*,.*}'
 chmod a+x /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp2.0/ubuntu.16.04-x64/publish/FilterLists.Api.dll
 sshpass -p $FTP_PASSWORD scp -o StrictHostKeyChecking=no -r /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp2.0/ubuntu.16.04-x64/publish/* $FTP_USER@$FTP_HOST:$FTP_DIR
