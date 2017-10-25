49804823a691482d62d1a6dd64aed0c4421d0191
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-10-25 13:10:21 -0500

    remove BaseEntity properties from sample

--- src/FilterLists.Data/Json/JsonSampleGenerator.cs
+++ src/FilterLists.Data/Json/JsonSampleGenerator.cs
@@ -9,21 +9,25 @@ namespace FilterLists.Data.Json
     {
         public static void WriteSampleToFile()
         {
+            var sampleList = (JObject) JToken.FromObject(new List
+            {
+                Author = "John Doe",
+                Description = "A sample list to filter out advertisements.",
+                DescriptionSourceUrl = "https://mysample.list",
+                DonateUrl = "https://mysample.list/donate/",
+                Email = "contact@mysample.list",
+                ForumUrl = "https://mysample.list/forum/",
+                HomeUrl = "https://mysample.list/",
+                IssuesUrl = "https://github.com/mysamplelist/issues",
+                Name = "My Sample List",
+                ViewUrl = "https://mysample.list/list.txt"
+            });
+            sampleList.Remove("Id");
+            sampleList.Remove("AddedDateUtc");
+            sampleList.Remove("ModifiedDateUtc");
             File.WriteAllText(
                 Path.GetFullPath(Path.Combine(AppContext.BaseDirectory + @"\", @"..\..\..\..\..\data\")) +
-                "ListSample.json", ((JObject) JToken.FromObject(new List
-                {
-                    Author = "John Doe",
-                    Description = "A sample list to filter out advertisements.",
-                    DescriptionSourceUrl = "https://mysample.list",
-                    DonateUrl = "https://mysample.list/donate/",
-                    Email = "contact@mysample.list",
-                    ForumUrl = "https://mysample.list/forum/",
-                    HomeUrl = "https://mysample.list/",
-                    IssuesUrl = "https://github.com/mysamplelist/issues",
-                    Name = "My Sample List",
-                    ViewUrl = "https://mysample.list/list.txt"
-                })).ToString());
+                "ListSample.json", sampleList.ToString());
         }
     }
 }
