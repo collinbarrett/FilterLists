f9327ae8d3a27886f7fcf18f66c6a2d2fe0b0f83
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-10-29 17:36:42 -0500

    add wwwroot back to API proj
    ref #131

--- src/FilterLists.Api/Program.cs
+++ src/FilterLists.Api/Program.cs
@@ -24,7 +24,6 @@ namespace FilterLists.Api
         {
             return WebHost.CreateDefaultBuilder(args)
                 .UseUrls("http://*:5000;")
-                .UseWebRoot("apiroot")
                 .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
