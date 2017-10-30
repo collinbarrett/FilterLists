dba6f73f1a8e3e7d02e357897778b4ae3a78aece
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-10-30 14:16:37 -0500

    finish wiring Rules junction

--- src/FilterLists.Data/Entities/Rule.cs
+++ src/FilterLists.Data/Entities/Rule.cs
@@ -1,7 +1,10 @@
-﻿namespace FilterLists.Data.Entities
+﻿using System.Collections.Generic;
+
+namespace FilterLists.Data.Entities
 {
     public class Rule : BaseEntity
     {
+        public ICollection<FilterListRule> FilterListRules { get; set; }
         public string Raw { get; set; }
     }
 }
