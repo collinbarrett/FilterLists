2b71f3a987c6a88b744e4866e0503d2da0596aa6
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-10-30 20:21:01 -0500

    Revert "strip Razor sample to barebones"
    This reverts commit f51c5d0dfabe7315c54ae423b8f9dfdc1b3fc5e3.

--- src/FilterLists.Web/Pages/About.cshtml.cs
+++ src/FilterLists.Web/Pages/About.cshtml.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FilterLists.Web.Pages
 {
-    public class IndexModel : PageModel
+    public class AboutModel : PageModel
     {
+        public string Message { get; set; }
+
         public void OnGet()
         {
-
+            Message = "Your application description page.";
         }
     }
 }
--- src/FilterLists.Web/Startup.cs
+++ src/FilterLists.Web/Startup.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -14,11 +18,13 @@ namespace FilterLists.Web
 
         public IConfiguration Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -26,14 +32,18 @@ namespace FilterLists.Web
                 app.UseDeveloperExceptionPage();
                 app.UseBrowserLink();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
 
             app.UseStaticFiles();
 
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
-                    "default",
-                    "{controller}/{action=Index}/{id?}");
+                    name: "default",
+                    template: "{controller}/{action=Index}/{id?}");
             });
         }
     }
