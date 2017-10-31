62f4a34df409528456ea156dac922825bbf5229a
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-10-31 18:55:07 -0500

    add Rules ef migration

--- src/FilterLists.Api/Migrations/20171031235408_add Rules.Designer.cs
+++ src/FilterLists.Api/Migrations/20171031235408_add Rules.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace FilterLists.Api.Migrations
 {
     [DbContext(typeof(FilterListsDbContext))]
-    partial class FilterListsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171031235408_add Rules")]
+    partial class addRules
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
