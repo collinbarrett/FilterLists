7ee5b821446daddb8033080e795765639d17909a
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2017-10-31 10:00:24 -0500

    move fetchdata to home, remove other sample pages

--- src/FilterLists.Web/ClientApp/components/NavMenu.tsx
+++ src/FilterLists.Web/ClientApp/components/NavMenu.tsx
@@ -22,16 +22,6 @@ export class NavMenu extends React.Component<{}, {}> {
                                        <span className="glyphicon glyphicon-home"></span> Home
                                    </NavLink>
                                </li>
-                               <li>
-                                   <NavLink to={ "/counter" } activeClassName="active">
-                                       <span className="glyphicon glyphicon-education"></span> Counter
-                                   </NavLink>
-                               </li>
-                               <li>
-                                   <NavLink to={ "/fetchdata" } activeClassName="active">
-                                       <span className="glyphicon glyphicon-th-list"></span> Fetch data
-                                   </NavLink>
-                               </li>
                            </ul>
                        </div>
                    </div>
