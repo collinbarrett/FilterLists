44473d11de1bb00b83a8f349a21df26fb85933d0
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-01-18 16:20:11 -0600

    Revert "replace material-ui with antd"
    This reverts commit 79c0520197f6c799121a4f6dee421276ef2d974b.

--- src/FilterLists.Web/ClientApp/components/Home.tsx
+++ src/FilterLists.Web/ClientApp/components/Home.tsx
@@ -1,7 +1,14 @@
 import * as React from "react";
 import { RouteComponentProps } from "react-router";
 import "isomorphic-fetch";
-import { Table } from 'antd';
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import {
+    Table,
+    TableHead,
+    TableBody,
+    TableRow,
+    TableCell,
+    } from "material-ui";
 
 interface IFilterListsState {
     filterLists: IFilterList[];
@@ -33,12 +40,26 @@ export class Home extends React.Component<RouteComponentProps<{}>, IFilterListsS
             : Home.renderFilterListsTable(this.state.filterLists);
 
         return <div>
+                   <MuiThemeProvider>
                        {contents}
+                   </MuiThemeProvider>
                </div>;
     }
 
     private static renderFilterListsTable(filterLists: IFilterList[]) {
         return <Table>
+                   <TableHead>
+                       <TableRow>
+                           <TableCell>List</TableCell>
+                       </TableRow>
+                   </TableHead>
+                   <TableBody>
+                       {filterLists.map(filterList =>
+                           <TableRow key={filterList.id}>
+                                <TableCell><h2>{filterList.name}</h2>{filterList.description}</TableCell>
+                           </TableRow>
+                       )}
+                   </TableBody>
                </Table>;
     }
 }
