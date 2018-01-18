5f9e31877c89c2ff420aeb974c778f45b18b80ba
Collin Barrett <collinbarrett@users.noreply.github.com>
2018-01-18 05:35:37 -0600

    add remainder of seed data

--- src/FilterLists.Data/FilterListsDbContext.cs
+++ src/FilterLists.Data/FilterListsDbContext.cs
@@ -11,15 +11,14 @@ namespace FilterLists.Data
         {
         }
 
-        public DbSet<FilterList> FilterLists { get; set; }
-        public DbSet<Language> Languages { get; set; }
-        public DbSet<License> Licenses { get; set; }
-        public DbSet<Maintainer> Maintainers { get; set; }
-        public DbSet<Rule> Rules { get; set; }
-        public DbSet<Software> Software { get; set; }
-        public DbSet<Syntax> Syntaxes { get; set; }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            ApplyConfigurationsEntities(modelBuilder);
+            ApplyConfigurationsJunctions(modelBuilder);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private static void ApplyConfigurationsEntities(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new FilterListTypeConfiguration());
             modelBuilder.ApplyConfiguration(new LanguageTypeConfiguration());
@@ -28,15 +27,39 @@ namespace FilterLists.Data
             modelBuilder.ApplyConfiguration(new RuleTypeConfiguration());
             modelBuilder.ApplyConfiguration(new SoftwareTypeConfiguration());
             modelBuilder.ApplyConfiguration(new SyntaxTypeConfiguration());
+        }
 
+        private static void ApplyConfigurationsJunctions(ModelBuilder modelBuilder)
+        {
             modelBuilder.ApplyConfiguration(new FilterListLanguageTypeConfiguration());
             modelBuilder.ApplyConfiguration(new FilterListMaintainerTypeConfiguration());
             modelBuilder.ApplyConfiguration(new FilterListRuleTypeConfiguration());
             modelBuilder.ApplyConfiguration(new ForkTypeConfiguration());
             modelBuilder.ApplyConfiguration(new MergeTypeConfiguration());
             modelBuilder.ApplyConfiguration(new SoftwareSyntaxTypeConfiguration());
-
-            base.OnModelCreating(modelBuilder);
         }
+
+        #region Entities
+
+        public DbSet<FilterList> FilterLists { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<License> Licenses { get; set; }
+        public DbSet<Maintainer> Maintainers { get; set; }
+        public DbSet<Rule> Rules { get; set; }
+        public DbSet<Software> Software { get; set; }
+        public DbSet<Syntax> Syntaxes { get; set; }
+
+        #endregion
+
+        #region Junctions
+
+        public DbSet<FilterListLanguage> FilterListLanguages { get; set; }
+        public DbSet<FilterListMaintainer> FilterListMaintainers { get; set; }
+        public DbSet<FilterListRule> FilterListRules { get; set; }
+        public DbSet<Fork> Forks { get; set; }
+        public DbSet<Merge> Merges { get; set; }
+        public DbSet<SoftwareSyntax> SoftwareSyntaxes { get; set; }
+
+        #endregion
     }
 }
