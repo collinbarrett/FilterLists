7a81cc202fa55663c2de211eb361ffb0f8a5fc3e
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-01-18 04:15:45 -0600

    skip generics for now
    see #183

--- src/FilterLists.Api/DbContextExtension.cs
+++ src/FilterLists.Api/DbContextExtension.cs
@@ -29,17 +29,45 @@ namespace FilterLists.Api
 
         public static void EnsureSeeded(this FilterListsDbContext context)
         {
-            if (context.FilterLists.Any()) return;
-            Seed<License>(context);
-            Seed<Syntax>(context);
-            Seed<FilterList>(context);
+            SeedLicenses(context);
+            SeedSyntaxes(context);
+            SeedFilterLists(context);
         }
 
+        //TODO: fix generic method to remove duplication of entity-specific methods
         private static void Seed<T>(DbContext context)
         {
+            //if (context.?.Any()) return;
             var rows = JsonConvert.DeserializeObject<List<T>>(
                 File.ReadAllText(SeedDirectory + Path.DirectorySeparatorChar + typeof(T).Name + ".json"));
             context.AddRange(rows);
+            context.SaveChanges();
+        }
+
+        private static void SeedLicenses(FilterListsDbContext context)
+        {
+            if (context.Licenses.Any()) return;
+            var types = JsonConvert.DeserializeObject<List<License>>(
+                File.ReadAllText(SeedDirectory + Path.DirectorySeparatorChar + typeof(License).Name + ".json"));
+            context.AddRange(types);
+            context.SaveChanges();
+        }
+
+        private static void SeedSyntaxes(FilterListsDbContext context)
+        {
+            if (context.Syntaxes.Any()) return;
+            var types = JsonConvert.DeserializeObject<List<Syntax>>(
+                File.ReadAllText(SeedDirectory + Path.DirectorySeparatorChar + typeof(Syntax).Name + ".json"));
+            context.AddRange(types);
+            context.SaveChanges();
+        }
+
+        private static void SeedFilterLists(FilterListsDbContext context)
+        {
+            if (context.FilterLists.Any()) return;
+            var types = JsonConvert.DeserializeObject<List<FilterList>>(
+                File.ReadAllText(SeedDirectory + Path.DirectorySeparatorChar + typeof(FilterList).Name + ".json"));
+            context.AddRange(types);
             context.SaveChanges();
         }
     }
