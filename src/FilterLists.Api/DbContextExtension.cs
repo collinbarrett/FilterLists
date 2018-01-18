edb4e137717181d81d89098c76051cbf0764d4cb
Collin Barrett <collinbarrett@users.noreply.github.com>
2018-01-17 21:14:06 -0600

    add Syntax seed

--- src/FilterLists.Api/DbContextExtension.cs
+++ src/FilterLists.Api/DbContextExtension.cs
@@ -31,13 +31,14 @@ namespace FilterLists.Api
         {
             if (context.FilterLists.Any()) return;
             Seed<License>(context);
+            Seed<Syntax>(context);
             Seed<FilterList>(context);
         }
 
         private static void Seed<T>(DbContext context)
         {
             var rows = JsonConvert.DeserializeObject<List<T>>(
-                File.ReadAllText(SeedDirectory + Path.DirectorySeparatorChar + typeof(T) + ".json"));
+                File.ReadAllText(SeedDirectory + Path.DirectorySeparatorChar + typeof(T).Name + ".json"));
             context.AddRange(rows);
             context.SaveChanges();
         }
