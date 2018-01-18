ccbb2c987326c28bcf342459fd6ecfc5f0a02d12
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-01-18 07:22:08 -0600

    expand db create script

--- scripts/createMySqlDb.sql
+++ scripts/createMySqlDb.sql
@@ -1,1 +1,4 @@
+CREATE USER 'filterlists'@'localhost' IDENTIFIED BY 'password';
 CREATE DATABASE filterlistsdata CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+GRANT ALL PRIVILEGES ON filterlistsdata.* TO 'filterlists'@'localhost';
+FLUSH PRIVILEGES;
