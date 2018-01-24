8bba881c845986eae8db17ca917a717ec7b7ff10
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-01-24 05:49:35 -0600

    work towards seed API action for all entities

--- src/FilterLists.Api/V1/Controllers/ListsController.cs
+++ src/FilterLists.Api/V1/Controllers/ListsController.cs
@@ -1,41 +1,27 @@
 ﻿using System;
-using FilterLists.Services;
+using FilterLists.Services.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace FilterLists.Api.V1.Controllers
 {
-    [ApiVersion("1.0")]
-    [Produces("application/json")]
-    [ResponseCache(CacheProfileName = "Long-Lived")]
-    public class ListsController : Controller
+    public class ListsController : BaseController
     {
         private readonly FilterListService filterListService;
-        private readonly IMemoryCache memoryCache;
 
-        public ListsController(IMemoryCache memoryCache, FilterListService filterListService)
+        public ListsController(IMemoryCache memoryCache, SeedService seedService, FilterListService filterListService) :
+            base(memoryCache, seedService)
         {
-            this.memoryCache = memoryCache;
             this.filterListService = filterListService;
         }
 
         [HttpGet]
         public IActionResult Index()
         {
-            return memoryCache.GetOrCreate(CacheKeys.Entry, entry =>
+            return MemoryCache.GetOrCreate(CacheKeys.Entry, entry =>
             {
                 entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(86400);
                 return Json(filterListService.GetAllSummaries());
-            });
-        }
-
-        [HttpGet]
-        public IActionResult Seed()
-        {
-            return memoryCache.GetOrCreate(CacheKeys.Entry, entry =>
-            {
-                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(86400);
-                return Json(filterListService.GetAllSeeds());
             });
         }
     }
