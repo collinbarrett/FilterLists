fd81aacd37fc6e93c272ccdfc559099cc9ad91d6
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-01-24 07:10:37 -0600

    extract middle level Entity controller for generic Seed method

--- src/FilterLists.Api/V1/Controllers/BaseController.cs
+++ src/FilterLists.Api/V1/Controllers/BaseController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using FilterLists.Services.Models;
-using FilterLists.Services.Services;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace FilterLists.Api.V1.Controllers
@@ -12,24 +9,10 @@ namespace FilterLists.Api.V1.Controllers
     public class BaseController : Controller
     {
         protected readonly IMemoryCache MemoryCache;
-        protected readonly SeedService SeedService;
 
-        public BaseController(IMemoryCache memoryCache, SeedService seedService)
+        protected BaseController(IMemoryCache memoryCache)
         {
             MemoryCache = memoryCache;
-            SeedService = seedService;
-        }
-
-        [HttpGet]
-        public IActionResult Seed()
-        {
-            var controllerName = ControllerContext.RouteData.Values["controller"].ToString();
-            return MemoryCache.GetOrCreate(CacheKeys.Entry, entry =>
-            {
-                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(86400);
-                //TODO: GetAll<T>() by seed type of request controller
-                return Json(SeedService.GetAll<FilterListSeedDto>());
-            });
         }
     }
 }
