e6d378286bc905bae8252308e9fe479277764e97
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-01-25 14:36:47 -0600

    strip out all caching for now (was leaking across endpoints)

--- src/FilterLists.Api/CacheKeys.cs
+++
@@ -1,9 +0,0 @@
-﻿// https://github.com/aspnet/Docs/blob/master/aspnetcore/performance/caching/memory/sample/WebCache/CacheKeys.cs
-
-namespace FilterLists.Api
-{
-    public static class CacheKeys
-    {
-        public static readonly string Entry = "_Entry";
-    }
-}
--- src/FilterLists.Api/V1/Controllers/BaseController.cs
+++ src/FilterLists.Api/V1/Controllers/BaseController.cs
@@ -1,19 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Caching.Memory;
 
 namespace FilterLists.Api.V1.Controllers
 {
     [ApiVersion("1.0")]
-    [Produces("application/json")]
-    [ResponseCache(CacheProfileName = "Long-Lived")]
     public class BaseController : Controller
     {
-        protected const int MemoryCacheSlidingExpirationSeconds = 30;
-        protected readonly IMemoryCache MemoryCache;
-
-        protected BaseController(IMemoryCache memoryCache)
+        protected BaseController()
         {
-            MemoryCache = memoryCache;
         }
     }
 }
