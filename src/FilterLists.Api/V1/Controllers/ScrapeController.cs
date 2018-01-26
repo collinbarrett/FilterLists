13fbfabdff32d5150485880be05c01f5ce958294
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-01-26 12:59:31 -0600

    begin work on ScrapeService

---
+++ src/FilterLists.Api/V1/Controllers/ScrapeController.cs
@@ -0,0 +1,22 @@
+﻿using FilterLists.Services.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilterLists.Api.V1.Controllers
+{
+    public class ScrapeController : BaseController
+    {
+        private readonly ScrapeService scrapeService;
+
+        public ScrapeController(ScrapeService scrapeService)
+        {
+            this.scrapeService = scrapeService;
+        }
+
+        [HttpPost]
+        public void Index()
+        {
+            //TODO: Convert endpoint into scheduled job, don't expose on prod
+            //scrapeService.Scrape();
+        }
+    }
+}
---
+++ src/FilterLists.Services/Services/ScrapeService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FilterLists.Data;
+using FilterLists.Data.Entities;
+
+namespace FilterLists.Services.Services
+{
+    public class ScrapeService
+    {
+        private readonly FilterListsDbContext filterListsDbContext;
+
+        public ScrapeService(FilterListsDbContext filterListsDbContext)
+        {
+            this.filterListsDbContext = filterListsDbContext;
+        }
+
+        public void Scrape()
+        {
+            var list = filterListsDbContext.FilterLists.First();
+            var snapshot = GetContent(list.ViewUrl);
+            filterListsDbContext.Snapshots.Add(new Snapshot
+            {
+                Content = snapshot.Result,
+                FilterListId = list.Id
+            });
+            filterListsDbContext.SaveChangesAsync();
+        }
+
+        private static async Task<string> GetContent(string url)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var httpResponseMessage = await httpClient.GetAsync(url))
+                {
+                    if (httpResponseMessage.IsSuccessStatusCode)
+                        return await httpResponseMessage.Content.ReadAsStringAsync();
+                }
+            }
+
+            return null;
+        }
+    }
+}
