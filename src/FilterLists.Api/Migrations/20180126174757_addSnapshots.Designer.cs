e473c18241197f086311f4ed50f5486cdc8f1e47
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-01-26 11:56:20 -0600

    add snapshot migration

--- src/FilterLists.Api/Migrations/20180126174757_addSnapshots.Designer.cs
+++ src/FilterLists.Api/Migrations/20180126174757_addSnapshots.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace FilterLists.Api.Migrations
 {
     [DbContext(typeof(FilterListsDbContext))]
-    partial class FilterListsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180126174757_addSnapshots")]
+    partial class addSnapshots
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
