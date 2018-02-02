85fc0f14489bfa71a4c22df5a08c2411e9897d4a
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-02-02 07:03:04 -0600

    TryEfDefaultValueForVarchar

---
+++ src/FilterLists.Api/Migrations/20180202130034_TryEfDefaultValueForVarchar.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FilterLists.Api.Migrations
+{
+    public partial class TryEfDefaultValueForVarchar : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                "EmailAddress",
+                "filterlists",
+                "VARCHAR(126)",
+                nullable: true,
+                defaultValueSql: "NULL",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(126) DEFAULT NULL",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                "EmailAddress",
+                "filterlists",
+                "VARCHAR(126) DEFAULT NULL",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(126)",
+                oldNullable: true,
+                oldDefaultValueSql: "NULL");
+        }
+    }
+}
