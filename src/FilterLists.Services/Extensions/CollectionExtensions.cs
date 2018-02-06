d0faa7ac8b6d9f35a8b9ebc11274bb281d7347fe
Collin Barrett <collinbarrett@users.noreply.github.com>
2018-02-05 18:25:14 -0600

    extract ICollection ext. method to its own file

---
+++ src/FilterLists.Services/Extensions/CollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace FilterLists.Services.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static void AddRange<T>(this ICollection<T> destination, IEnumerable<T> source)
+        {
+            foreach (var item in source)
+                destination.Add(item);
+        }
+    }
+}
