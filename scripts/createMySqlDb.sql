0dd289b8dbabfa40e80ae7d3a25a6966d06f4a7d
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-02-07 13:26:35 -0600

    use case-sensitive collation

--- scripts/createMySqlDb.sql
+++ scripts/createMySqlDb.sql
@@ -1,4 +1,4 @@
 CREATE USER 'filterlists'@'localhost' IDENTIFIED BY 'password';
-CREATE DATABASE filterlistsdata CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE filterlistsdata CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 GRANT ALL PRIVILEGES ON filterlistsdata.* TO 'filterlists'@'localhost';
 FLUSH PRIVILEGES;
