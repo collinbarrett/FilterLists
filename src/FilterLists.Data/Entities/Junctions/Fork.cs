650c5a99ef6cff5497d04ee5ed208668963b11fd
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-02-09 13:49:22 -0600

    minor reformatting

--- src/FilterLists.Data/Entities/Junctions/Fork.cs
+++ src/FilterLists.Data/Entities/Junctions/Fork.cs
@@ -4,7 +4,6 @@
     {
         public int ForkFilterListId { get; set; }
         public FilterList ForkFilterList { get; set; }
-
         public int UpstreamFilterListId { get; set; }
         public FilterList UpstreamFilterList { get; set; }
     }
