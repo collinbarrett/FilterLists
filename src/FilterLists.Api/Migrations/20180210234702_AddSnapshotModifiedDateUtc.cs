a5a1ee93a34bf85aa4685abadc6bb3309c10a6bb
Collin Barrett <collinbarrett@users.noreply.github.com>
2018-02-10 17:49:32 -0600

    add Snapshot ModifiedDateUtc

---
+++ src/FilterLists.Api/Migrations/20180210234702_AddSnapshotModifiedDateUtc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FilterLists.Api.Migrations
+{
+    public partial class AddSnapshotModifiedDateUtc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                "ModifiedDateUtc",
+                "snapshots",
+                "TIMESTAMP",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP()");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                "ModifiedDateUtc",
+                "snapshots");
+        }
+    }
+}
