8ade9fba23d44d471450a752d7edaf801adcac15
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-02-10 16:03:46 -0600

    enable ModifiedDate on Snapshots

---
+++ src/FilterLists.Api/Migrations/20180210220228_EnableModifiedDateOnSnapshots.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FilterLists.Api.Migrations
+{
+    public partial class EnableModifiedDateOnSnapshots : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                "ModifiedDateUtc",
+                "snapshots",
+                "TIMESTAMP",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                "ModifiedDateUtc",
+                "snapshots");
+        }
+    }
+}
