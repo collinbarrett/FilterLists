36b6c80c190ec6ad4c588d7f15ee11c1c7253936
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-02-17 09:13:15 -0600

    API lists endpoint updates

--- src/FilterLists.Services/FilterListService/FilterListDetailsDto.cs
+++ src/FilterLists.Services/FilterListService/FilterListDetailsDto.cs
@@ -1,11 +1,21 @@
-﻿namespace FilterLists.Services.FilterListService
+﻿using System;
+
+namespace FilterLists.Services.FilterListService
 {
     public class FilterListDetailsDto
     {
-        public int Id { get; set; }
         public string Description { get; set; }
         public string DescriptionSourceUrl { get; set; }
+        public DateTime? DiscontinuedDate { get; set; }
+        public string DonateUrl { get; set; }
+        public string EmailAddress { get; set; }
+        public string ForumUrl { get; set; }
+        public string HomeUrl { get; set; }
+        public string IssuesUrl { get; set; }
         public string Name { get; set; }
+        public string PolicyUrl { get; set; }
+        public DateTime? PublishedDate { get; set; }
+        public string SubmissionUrl { get; set; }
         public string ViewUrl { get; set; }
     }
 }
--- src/FilterLists.Services/FilterListService/FilterListSummaryDto.cs
+++ src/FilterLists.Services/FilterListService/FilterListSummaryDto.cs
@@ -3,7 +3,6 @@
     public class FilterListSummaryDto
     {
         public int Id { get; set; }
-        public string Description { get; set; }
         public string Name { get; set; }
         public string ViewUrl { get; set; }
     }
