65d550e9963485e87798188a7e7db2744bc28317
Collin Barrett <collinbarrett@users.noreply.github.com>
2018-02-25 13:21:12 -0600

    add Maintainers to List Details API endpoint

---
+++ src/FilterLists.Services/FilterListService/MaintainerDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FilterLists.Services.FilterListService
+{
+    public class MaintainerDto
+    {
+        public int Id { get; set; }
+        public string EmailAddress { get; set; }
+        public string HomeUrl { get; set; }
+        public string Name { get; set; }
+        public string TwitterHandle { get; set; }
+    }
+}
