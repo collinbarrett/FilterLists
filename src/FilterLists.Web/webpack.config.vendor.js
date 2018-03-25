86ac43ba9543e1a107f473b99269778bc4ae572e
Collin Barrett <collinbarrett@users.noreply.github.com>
2018-03-25 17:38:17 -0500

    Revert "remove UglifyJsPlugin"
    This reverts commit 40b9ae959984c31130ec425cc33ce0d96a040f8a.

--- src/FilterLists.Web/webpack.config.vendor.js
+++ src/FilterLists.Web/webpack.config.vendor.js
@@ -45,7 +45,11 @@ module.exports = (env) => {
                 new webpack.DefinePlugin({
                     'process.env.NODE_ENV': isDevBuild ? '"development"' : '"production"'
                 })
-            ]
+            ].concat(isDevBuild
+                ? []
+                : [
+                    new webpack.optimize.UglifyJsPlugin()
+                ])
         }
     ];
 };
