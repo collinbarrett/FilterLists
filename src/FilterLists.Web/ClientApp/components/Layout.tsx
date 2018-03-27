fc8b2ad37ac7ba7eaacbcc0d7e7f9998f1d14123
Collin Barrett <collinbarrett@users.noreply.github.com>
2018-03-27 05:07:04 -0500

    normalize descriptions, other misc layout tweaks

--- src/FilterLists.Web/ClientApp/components/Layout.tsx
+++ src/FilterLists.Web/ClientApp/components/Layout.tsx
@@ -13,7 +13,7 @@ export class Layout extends React.Component<ILayoutProps, {}> {
                                <img src="logo_filterlists.png" alt="FilterLists" className="img-fluid"/>
                            </a>
                        </h1>
-                       <p className="ml-1 mr-1">
+                       <p className="ml-2 mr-2">
                            The independent, comprehensive directory of filter and host lists for advertisements, trackers, malware, and annoyances.
                        </p>
                    </div>
