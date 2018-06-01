8e93f8159f5d83b16e5b898d94338b8871a975d9
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-06-01 15:24:29 -0500

    worked through 2.x to 2.1 migration guide, Pomelo seems unsopported still causing EF issues
    https://docs.microsoft.com/en-us/aspnet/core/migration/20_21?view=aspnetcore-2.1
    
    https://github.com/PomeloFoundation/Pomelo.EntityFrameworkCore.MySql/issues/514
    
    https://stackoverflow.com/questions/50650463/specifically-target-older-version-of-a-net-standard-asset-entityframeworkcore

--- src/FilterLists.Web/Startup.cs
+++ src/FilterLists.Web/Startup.cs
@@ -1,27 +1,34 @@
+using JetBrains.Annotations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FilterLists.Web
 {
+    [UsedImplicitly]
     public class Startup
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        [UsedImplicitly]
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        [UsedImplicitly]
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,19 +43,16 @@ namespace FilterLists.Web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
             }
 
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseCookiePolicy();
             app.UseMvc(routes =>
             {
-                routes.MapRoute(
-                    "default",
-                    "{controller=Home}/{action=Index}/{id?}");
-
-                routes.MapSpaFallbackRoute(
-                    "spa-fallback",
-                    new {controller = "Home", action = "Index"});
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+                routes.MapSpaFallbackRoute("spa-fallback", new {controller = "Home", action = "Index"});
             });
         }
     }
