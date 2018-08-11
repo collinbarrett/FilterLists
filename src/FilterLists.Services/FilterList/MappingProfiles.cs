ea6608b576bfcf189af913f45a782bdfc7283aea
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-08-11 14:47:05 -0500

    mark MappingProfiles used implicitly

--- src/FilterLists.Services/FilterList/MappingProfiles.cs
+++ src/FilterLists.Services/FilterList/MappingProfiles.cs
@@ -2,9 +2,11 @@
 using AutoMapper;
 using FilterLists.Data.Entities;
 using FilterLists.Services.FilterList.Models;
+using JetBrains.Annotations;
 
 namespace FilterLists.Services.FilterList
 {
+    [UsedImplicitly]
     public class MappingProfiles : Profile
     {
         public MappingProfiles()
