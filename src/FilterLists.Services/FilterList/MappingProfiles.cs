e3f310c500077b372b4c38872335ddc2d39dfae9
Collin Barrett <collinbarrett@users.noreply.github.com>
2018-08-11 16:02:21 -0500

    improved GetAllSummariesAsync()
    ref #330

--- src/FilterLists.Services/FilterList/MappingProfiles.cs
+++ src/FilterLists.Services/FilterList/MappingProfiles.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using AutoMapper;
 using FilterLists.Data.Entities;
 using FilterLists.Services.FilterList.Models;
@@ -21,7 +22,16 @@ namespace FilterLists.Services.FilterList
             CreateMap<Data.Entities.FilterList, ListSummaryDto>()
                 .ForMember(dto => dto.Languages,
                     conf => conf.MapFrom(list => list.FilterListLanguages.Select(listLangs => listLangs.Language)))
-                .ForMember(dto => dto.AddedDate, conf => conf.MapFrom(list => list.CreatedDateUtc));
+                .ForMember(dto => dto.AddedDate, conf => conf.MapFrom(list => list.CreatedDateUtc))
+                .ForMember(dto => dto.UpdatedDate,
+                    conf => conf.MapFrom(list =>
+                        list.Snapshots.Where(s =>
+                                s.IsCompleted && s.HttpStatusCode == "200" &&
+                                (s.AddedSnapshotRules.Count > 0 || s.RemovedSnapshotRules.Count > 0))
+                            .OrderByDescending(s => s.CreatedDateUtc)
+                            .Select(s => s.CreatedDateUtc)
+                            .Cast<DateTime?>()
+                            .FirstOrDefault()));
 
         private void CreateListDetailsDtoMap() =>
             CreateMap<Data.Entities.FilterList, ListDetailsDto>()
