a09d141e9d1872db22e0b1233df8f240ce1389b8
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-08-10 20:00:51 -0500

    revert webpack config to initial

--- src/FilterLists.Web/webpack.config.js
+++ src/FilterLists.Web/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = (env) => {
             entry: { 'main': "./ClientApp/boot.tsx" },
             resolve: { extensions: [".js", ".jsx", ".ts", ".tsx"] },
             output: {
-                path: path.join(__dirname, bundleOutputDir),
+                path: path.join(window.__dirname, bundleOutputDir),
                 filename: "[name].js",
                 publicPath: "dist/"
             },
@@ -23,16 +23,7 @@ module.exports = (env) => {
                         test: /\.css$/,
                         use: isDevBuild
                             ? ["style-loader", "css-loader"]
-                            : ExtractTextPlugin.extract({
-                                use: [
-                                    {
-                                        loader: "css-loader",
-                                        options: {
-                                            minimize: { discardComments: { removeAll: true } }
-                                        }
-                                    }
-                                ]
-                            })
+                            : ExtractTextPlugin.extract({ use: "css-loader?minimize" })
                     },
                     { test: /\.(png|jpg|jpeg|gif|svg)$/, use: "url-loader?limit=25000" }
                 ]
@@ -40,24 +31,22 @@ module.exports = (env) => {
             plugins: [
                 new CheckerPlugin(),
                 new webpack.DllReferencePlugin({
-                    context: __dirname,
+                    context: window.__dirname,
                     manifest: require("./wwwroot/dist/vendor-manifest.json")
                 })
             ].concat(isDevBuild
                 ? [
+                    // Plugins that apply in development builds only
                     new webpack.SourceMapDevToolPlugin({
-                        filename: "[file].map",
+                        filename: "[file].map", // Remove this line if you prefer inline source maps
                         moduleFilenameTemplate:
                             path.relative(bundleOutputDir,
-                                "[resourcePath]")
+                                "[resourcePath]") // Point sourcemap entries to the original file locations on disk
                     })
                 ]
                 : [
-                    new webpack.optimize.UglifyJsPlugin({
-                        output: {
-                            comments: false
-                        }
-                    }),
+                    // Plugins that apply in production builds only
+                    new webpack.optimize.UglifyJsPlugin(),
                     new ExtractTextPlugin("site.css")
                 ])
         }
