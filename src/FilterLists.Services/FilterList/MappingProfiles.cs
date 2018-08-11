33b0b2ecb4ad0aa28901dae773eb14927711f137
Collin Barrett <collinbarrett@users.noreply.github.com>
2018-08-11 14:29:50 -0500

    refactor mapping profiles
    ref #330

--- src/FilterLists.Services/FilterList/MappingProfiles.cs
+++ src/FilterLists.Services/FilterList/MappingProfiles.cs
@@ -9,10 +9,19 @@ namespace FilterLists.Services.FilterList
     {
         public MappingProfiles()
         {
+            CreateListSummaryDtoMap();
+            CreateListDetailsDtoMap();
+            CreateListMaintainerDtoMap();
+            CreateListSyntaxDtoMap();
+        }
+
+        private void CreateListSummaryDtoMap() =>
             CreateMap<Data.Entities.FilterList, ListSummaryDto>()
                 .ForMember(dto => dto.Languages,
                     conf => conf.MapFrom(list => list.FilterListLanguages.Select(listLangs => listLangs.Language)))
                 .ForMember(dto => dto.AddedDate, conf => conf.MapFrom(list => list.CreatedDateUtc));
+
+        private void CreateListDetailsDtoMap() =>
             CreateMap<Data.Entities.FilterList, ListDetailsDto>()
                 .ForMember(dto => dto.Languages,
                     conf => conf.MapFrom(list => list.FilterListLanguages.Select(listLangs => listLangs.Language.Name)))
@@ -20,13 +29,16 @@ namespace FilterLists.Services.FilterList
                     conf => conf.MapFrom(list =>
                         list.FilterListMaintainers.Select(listMaints => listMaints.Maintainer)))
                 .ForMember(dto => dto.AddedDate, conf => conf.MapFrom(list => list.CreatedDateUtc));
+
+        private void CreateListMaintainerDtoMap() =>
             CreateMap<Maintainer, ListMaintainerDto>()
                 .ForMember(dto => dto.AdditionalLists,
                     conf => conf.MapFrom(maint =>
                         maint.FilterListMaintainers.Select(listMaints => listMaints.FilterList)));
+
+        private void CreateListSyntaxDtoMap() =>
             CreateMap<Syntax, ListSyntaxDto>()
                 .ForMember(dto => dto.SupportedSoftware,
                     conf => conf.MapFrom(syntax => syntax.SoftwareSyntaxes.Select(softSyn => softSyn.Software)));
-        }
     }
 }
