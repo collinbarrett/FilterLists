2de78d1fde755c06b55f43a7b2c3775c03764fc4
Collin Barrett <collinbarrett@users.noreply.github.com>
2018-08-11 09:29:20 -0500

    tweak dynamic page size for mobile
    ref #329

--- src/FilterLists.Web/ClientApp/components/Home.tsx
+++ src/FilterLists.Web/ClientApp/components/Home.tsx
@@ -67,7 +67,9 @@ export class Home extends React.Component<RouteComponentProps<{}>, IHomeState> {
     }
 
     updatePageSize() {
-        this.setState({ pageSize: (window.innerHeight - (361)) / 52 });
+        this.setState({
+            pageSize: window.innerHeight - (window.innerWidth > 540 ? 361 : 320) / 52
+        });
     }
 
     private static renderTagline(state: IHomeState) {
