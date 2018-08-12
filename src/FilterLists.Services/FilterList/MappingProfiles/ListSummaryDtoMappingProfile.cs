4e83f9c7793933b59add7bdc6eda4076b3c3ee31
Collin Barrett <collinbarrett@users.noreply.github.com>
2018-08-12 14:53:35 -0500

    revert extracting Func<>s
    despite trying many forms, the AsQueryable() (see [here](https://stackoverflow.com/a/51812063/2343739)) generates broken mysql code

--- src/FilterLists.Services/FilterList/MappingProfiles/ListSummaryDtoMappingProfile.cs
+++ src/FilterLists.Services/FilterList/MappingProfiles/ListSummaryDtoMappingProfile.cs
@@ -9,20 +9,14 @@ namespace FilterLists.Services.FilterList.MappingProfiles
     [UsedImplicitly]
     public class ListSummaryDtoMappingProfile : Profile
     {
-        private readonly Func<Data.Entities.Snapshot, bool> isDiffSnapshot =
-            s => s.AddedSnapshotRules.Count > 0 || s.RemovedSnapshotRules.Count > 0;
-
-        private readonly Func<Data.Entities.Snapshot, bool> isSuccessfulSnapshot =
-            s => s.IsCompleted && s.HttpStatusCode == "200";
-
         public ListSummaryDtoMappingProfile() =>
             CreateMap<Data.Entities.FilterList, ListSummaryDto>()
                 .ForMember(d => d.AddedDate, c => c.MapFrom(l => l.CreatedDateUtc))
                 .ForMember(d => d.Languages, c => c.MapFrom(l => l.FilterListLanguages.Select(la => la.Language)))
                 .ForMember(d => d.UpdatedDate,
                     c => c.MapFrom(l =>
-                        l.Snapshots.Where(isSuccessfulSnapshot)
-                         .Where(isDiffSnapshot)
+                        l.Snapshots.Where(s => s.IsCompleted && s.HttpStatusCode == "200")
+                         .Where(s => s.AddedSnapshotRules.Count > 0 || s.RemovedSnapshotRules.Count > 0)
                          .OrderByDescending(s => s.CreatedDateUtc)
                          .Select(s => s.CreatedDateUtc)
                          .Cast<DateTime?>()
