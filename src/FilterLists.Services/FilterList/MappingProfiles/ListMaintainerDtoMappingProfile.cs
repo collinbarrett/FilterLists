81ac1270e8cf1e99e74b0b62220f5aea436c23bd
Collin Barrett <collinbarrett@users.noreply.github.com>
2018-08-11 20:17:50 -0500

    refactor mapping profiles

---
+++ src/FilterLists.Services/FilterList/MappingProfiles/ListMaintainerDtoMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using AutoMapper;
+using FilterLists.Data.Entities;
+using FilterLists.Services.FilterList.Models;
+using JetBrains.Annotations;
+
+namespace FilterLists.Services.FilterList.MappingProfiles
+{
+    [UsedImplicitly]
+    public class ListMaintainerDtoMappingProfile : Profile
+    {
+        public ListMaintainerDtoMappingProfile() =>
+            CreateMap<Maintainer, ListMaintainerDto>()
+                .ForMember(dto => dto.AdditionalLists,
+                    conf => conf.MapFrom(maint =>
+                        maint.FilterListMaintainers.Select(listMaints => listMaints.FilterList)));
+    }
+}
---
+++ src/FilterLists.Services/FilterList/MappingProfiles/ListSummaryDtoMappingProfile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using AutoMapper;
+using FilterLists.Services.FilterList.Models;
+using JetBrains.Annotations;
+
+namespace FilterLists.Services.FilterList.MappingProfiles
+{
+    [UsedImplicitly]
+    public class ListSummaryDtoMappingProfile : Profile
+    {
+        public ListSummaryDtoMappingProfile() =>
+            CreateMap<Data.Entities.FilterList, ListSummaryDto>()
+                .ForMember(dto => dto.AddedDate, conf => conf.MapFrom(list => list.CreatedDateUtc))
+                .ForMember(dto => dto.Languages,
+                    conf => conf.MapFrom(list => list.FilterListLanguages.Select(listLangs => listLangs.Language)))
+                .ForMember(dto => dto.UpdatedDate,
+                    conf => conf.MapFrom(list =>
+                        list.Snapshots.Where(s =>
+                                s.IsCompleted && s.HttpStatusCode == "200" &&
+                                (s.AddedSnapshotRules.Count > 0 || s.RemovedSnapshotRules.Count > 0))
+                            .OrderByDescending(s => s.CreatedDateUtc)
+                            .Select(s => s.CreatedDateUtc)
+                            .Cast<DateTime?>()
+                            .FirstOrDefault()));
+    }
+}
