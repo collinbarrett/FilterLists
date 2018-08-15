432fba63b4d12237eaea424c07516ff600fcb68d
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-08-15 06:04:44 -0500

    misc SnapshotService cleanup
    ref #343

--- src/FilterLists.Services/Snapshot/RawRuleLinterExtensions.cs
+++ src/FilterLists.Services/Snapshot/RawRuleLinterExtensions.cs
@@ -1,35 +1,38 @@
-﻿namespace FilterLists.Services.Snapshot
+﻿using System;
+
+namespace FilterLists.Services.Snapshot
 {
     public static class RawRuleLinterExtensions
     {
         public static string LintRawRule(this string rule)
         {
-            rule = rule?.TrimLeadingAndTrailingWhitespace();
-            rule = rule?.DropIfEmpty();
-            rule = rule?.DropIfComment();
+            rule = rule.Trim();
+            rule = rule.DropIfEmpty();
             rule = rule?.DropIfTooLong();
+            rule = rule?.DropIfComment();
             rule = rule?.DropIfContainsBackslashSingleQuote();
             rule = rule?.TrimSingleBackslashFromEnd();
             return rule;
         }
 
-        private static string TrimLeadingAndTrailingWhitespace(this string rule)
-        {
-            char[] charsToTrim = {' ', '\t'};
-            return rule.Trim(charsToTrim);
-        }
-
-        private static string DropIfEmpty(this string rule) => rule == "" ? null : rule;
-
+        private static string DropIfEmpty(this string rule) =>
+            rule == string.Empty ? null : rule;
+        
+        private static string DropIfTooLong(this string rule) =>
+            rule.Length > 8192 ? null : rule;
+        
         private static string DropIfComment(this string rule) =>
-            rule.StartsWith(@"!") && !rule.StartsWith(@"!#") || rule.StartsWith(@"!##") ? null : rule;
-
-        private static string DropIfTooLong(this string rule) => rule.Length > 8192 ? null : rule;
+            rule.StartsWith(@"!", StringComparison.Ordinal) && !rule.StartsWith(@"!#", StringComparison.Ordinal) ||
+            rule.StartsWith(@"!##", StringComparison.Ordinal)
+                ? null
+                : rule;
 
         private static string DropIfContainsBackslashSingleQuote(this string rule) =>
             rule.Contains(@"\'") ? null : rule;
 
         private static string TrimSingleBackslashFromEnd(this string rule) =>
-            rule.EndsWith(@"\") && !rule.EndsWith(@"\\") ? rule.Remove(rule.Length - 1) : rule;
+            rule.EndsWith(@"\", StringComparison.Ordinal) && !rule.EndsWith(@"\\", StringComparison.Ordinal)
+                ? rule.Remove(rule.Length - 1)
+                : rule;
     }
 }
