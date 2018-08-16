04efb2abca2739688fcc800024a42a582e1bf080
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-08-16 17:53:19 -0500

    refactor Agent telemetry

--- src/FilterLists.Agent/Program.cs
+++ src/FilterLists.Agent/Program.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using FilterLists.Services.DependencyInjection.Extensions;
 using FilterLists.Services.Snapshot;
-using Microsoft.ApplicationInsights;
-using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,51 +9,42 @@ namespace FilterLists.Agent
     public static class Program
     {
         private const int BatchSize = 1;
-        private const string AiConfigSetting = "ApplicationInsights:InstrumentationKey";
-        private static IConfigurationRoot configurationRoot;
+        private const string AppInsightsKeyConfig = "ApplicationInsights:InstrumentationKey";
+        private static IConfigurationRoot configRoot;
         private static ServiceProvider serviceProvider;
-        private static TelemetryClient telemetryClient;
+        private static Logger logger;
 
         public static void Main()
         {
-            BuildConfigurationRoot();
-            InstantiateTelemetryClient();
+            BuildConfigRoot();
             BuildServiceProvider();
-            CaptureSnapshots(BatchSize);
+            using (BuildLogger())
+            {
+                CaptureSnapshots(BatchSize);
+            }
         }
 
-        private static void BuildConfigurationRoot() =>
-            configurationRoot = new ConfigurationBuilder()
-                                .SetBasePath(Directory.GetCurrentDirectory())
-                                .AddJsonFile("appsettings.json", false)
-                                .Build();
-
-        private static void InstantiateTelemetryClient()
-        {
-            TelemetryConfiguration.Active.InstrumentationKey = configurationRoot[AiConfigSetting];
-            telemetryClient = new TelemetryClient();
-        }
+        private static void BuildConfigRoot() =>
+            configRoot = new ConfigurationBuilder()
+                         .SetBasePath(Directory.GetCurrentDirectory())
+                         .AddJsonFile("appsettings.json", false)
+                         .Build();
 
         private static void BuildServiceProvider()
         {
             var serviceCollection = new ServiceCollection();
-            serviceCollection.AddFilterListsAgentServices(configurationRoot);
+            serviceCollection.AddFilterListsAgentServices(configRoot);
             serviceProvider = serviceCollection.BuildServiceProvider();
         }
+
+        private static Logger BuildLogger() => logger = new Logger(configRoot[AppInsightsKeyConfig]);
 
         private static void CaptureSnapshots(int batchSize)
         {
             var snapshotService = serviceProvider.GetService<SnapshotService>();
-            Log("Capturing FilterList snapshots...");
+            logger.Log("Capturing FilterList snapshots...");
             snapshotService.CaptureAsync(batchSize).Wait();
-            Log("Snapshots captured.");
-            telemetryClient.Flush();
-        }
-
-        private static void Log(string message)
-        {
-            Console.WriteLine(message);
-            telemetryClient.TrackTrace(message);
+            logger.Log("Snapshots captured.");
         }
     }
 }
