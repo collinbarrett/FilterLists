443cc46545626f9295fb9552ff3f723dc6f485ee
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-08-15 19:48:54 -0500

    update column in helper script

--- scripts/sql/snapshotTimeAnalysis.sql
+++ scripts/sql/snapshotTimeAnalysis.sql
@@ -3,7 +3,7 @@ SELECT
  ModifiedDateUtc - CreatedDateUtc As ProcessingTimeSecs,
  CreatedDateUtc,
  ModifiedDateUtc,
- IsCompleted
+ WasSuccessful
 FROM snapshots
 WHERE ModifiedDateUtc - CreatedDateUtc > 30 OR IsCompleted = 0
 ORDER BY ProcessingTimeSecs DESC
