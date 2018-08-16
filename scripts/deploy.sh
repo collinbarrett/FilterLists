429ed19967fef041757b42aa91ffc6575160bd7b
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-08-15 19:31:29 -0500

    try restarting mysql on every api deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,6 +15,7 @@ sshpass -p $FTP_PASSWORD scp -o StrictHostKeyChecking=no -r /home/travis/build/c
 chmod a+x /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp2.1/ubuntu.16.04-x64/publish/FilterLists.Api.dll
 sshpass -p $FTP_PASSWORD scp -o StrictHostKeyChecking=no -r /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp2.1/ubuntu.16.04-x64/publish/* $FTP_USER@$FTP_HOST:$FTP_DIR
 sshpass -p $FTP_PASSWORD ssh -o StrictHostKeyChecking=no $FTP_USER@$FTP_HOST 'sudo systemctl restart filterlists.api.service'
+sshpass -p $FTP_PASSWORD ssh -o StrictHostKeyChecking=no $FTP_USER@$FTP_HOST 'sudo service mysql restart'
 
 #deploy Web
 chmod a+x /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Web/bin/Release/netcoreapp2.1/ubuntu.16.04-x64/publish/FilterLists.Web.dll
