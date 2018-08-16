337d4d969237028149bd26989f19e8ff61f84193
Collin Barrett <collinbarrett@users.noreply.github.com>
2018-08-16 17:53:19 -0500

    refactor Agent telemetry

---
+++ src/FilterLists.Agent/Logger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace FilterLists.Agent
+{
+    public class Logger : IDisposable
+    {
+        private readonly TelemetryClient telemetryClient;
+
+        public Logger(string appInsightsKey)
+        {
+            TelemetryConfiguration.Active.InstrumentationKey = appInsightsKey;
+            telemetryClient = new TelemetryClient();
+        }
+
+        public void Dispose()
+        {
+            telemetryClient.Flush();
+            Thread.Sleep(5000);
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+            telemetryClient.TrackTrace(message);
+        }
+    }
+}
