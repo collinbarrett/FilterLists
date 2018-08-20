397d1ec9dbdb04250fdd0e20c9970f1d267296cd
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-08-19 20:42:07 -0500

    re-add mysql restart just before API boots up

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,6 +14,7 @@ sshpass -p $FTP_PASSWORD scp -o StrictHostKeyChecking=no -r /home/travis/build/c
 #deploy API
 sshpass -p $FTP_PASSWORD ssh -o StrictHostKeyChecking=no $FTP_USER@$FTP_HOST 'sudo systemctl stop filterlists.api.service'
 sshpass -p $FTP_PASSWORD scp -o StrictHostKeyChecking=no -r /home/travis/build/collinbarrett/FilterLists/src/FilterLists.Api/bin/Release/netcoreapp2.1/ubuntu.18.04-x64/publish/* $FTP_USER@$FTP_HOST:$FTP_DIR
+sshpass -p $FTP_PASSWORD ssh -o StrictHostKeyChecking=no $FTP_USER@$FTP_HOST 'sudo service mysql restart'
 sshpass -p $FTP_PASSWORD ssh -o StrictHostKeyChecking=no $FTP_USER@$FTP_HOST 'sudo systemctl start filterlists.api.service'
 
 #deploy Web
