2901cbbdbee903bb7d2cfe5e8e4d1cce3882318f
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-08-21 14:38:37 -0500

    extract url validator to extension method

---
+++ src/FilterLists.Services/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+namespace FilterLists.Services.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsValidHttpOrHttpsUrl(this string source) =>
+            Uri.TryCreate(source, UriKind.Absolute, out var uriResult) &&
+            new[] {Uri.UriSchemeHttps, Uri.UriSchemeHttp}.Contains(uriResult.Scheme);
+    }
+}
