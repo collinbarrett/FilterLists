eb3d33455494b85c0b27b559c8b10cd3c9954aa3
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-08-22 10:54:57 -0500

    add Tag seed API endpoints
    ref #65

---
+++ src/FilterLists.Api/V1/Controllers/ListsTagsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using FilterLists.Data.Entities.Junctions;
+using FilterLists.Services.Seed;
+using FilterLists.Services.Seed.Models.Junctions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace FilterLists.Api.V1.Controllers
+{
+    public class ListsTagsController : BaseController
+    {
+        public ListsTagsController(IMemoryCache memoryCache, SeedService seedService)
+            : base(memoryCache, seedService)
+        {
+        }
+
+        [HttpGet("seed")]
+        public async Task<IActionResult> Seed() =>
+            Json(await MemoryCache.GetOrCreate("ListsTagsController_Seed", entry =>
+            {
+                entry.AbsoluteExpirationRelativeToNow = FourHoursFromNow;
+                return SeedService.GetAllAsync<FilterListTag, FilterListTagSeedDto>(
+                    typeof(FilterListTag).GetProperty("FilterListId"),
+                    typeof(FilterListTag).GetProperty("TagId"));
+            }));
+    }
+}
