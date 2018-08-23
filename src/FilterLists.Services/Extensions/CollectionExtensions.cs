33e0766a34d95b98dbaea989c1df47a650ce6838
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-08-23 13:19:10 -0500

    re-work SnapshotRule model

--- src/FilterLists.Services/Extensions/CollectionExtensions.cs
+++ src/FilterLists.Services/Extensions/CollectionExtensions.cs
@@ -4,16 +4,6 @@ namespace FilterLists.Services.Extensions
 {
     public static class CollectionExtensions
     {
-        //https://stackoverflow.com/a/26360010/2343739
-        public static void AddRange<T>(this ICollection<T> destination, IEnumerable<T> source)
-        {
-            if (destination is List<T> list)
-                list.AddRange(source);
-            else
-                foreach (var item in source)
-                    destination.Add(item);
-        }
-
         public static void AddIfNotNullOrEmpty(this ICollection<string> set, string item)
         {
             if (!string.IsNullOrEmpty(item))
