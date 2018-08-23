bf135fe6afe0ae8f96552dbb5d95363a4e7c1eba
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-08-23 16:03:42 -0500

    DropRuleRawUniqueIndexAndTrimming
    ref #380

--- src/FilterLists.Data/FilterListsDbContext.cs
+++ src/FilterLists.Data/FilterListsDbContext.cs
@@ -34,7 +34,6 @@ namespace FilterLists.Data
         {
             base.OnModelCreating(modelBuilder);
             ApplyConfigurations(modelBuilder);
-            ApplyIndexes(modelBuilder);
         }
 
         private static void ApplyConfigurations(ModelBuilder modelBuilder)
@@ -56,11 +55,6 @@ namespace FilterLists.Data
             modelBuilder.ApplyConfiguration(new MergeTypeConfiguration());
             modelBuilder.ApplyConfiguration(new SnapshotRuleTypeConfiguration());
             modelBuilder.ApplyConfiguration(new SoftwareSyntaxTypeConfiguration());
-        }
-
-        private static void ApplyIndexes(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Rule>(b => { b.HasIndex(e => e.Raw).IsUnique(); });
         }
     }
 }
