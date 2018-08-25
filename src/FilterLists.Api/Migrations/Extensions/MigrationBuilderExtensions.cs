d81269aeb2f374bf8d43c2cc40f1af8a8f6f9055
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-08-25 15:51:57 -0500

    groundwork for #395
    wip

---
+++ src/FilterLists.Api/Migrations/Extensions/MigrationBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using Microsoft.EntityFrameworkCore.Migrations.Operations.Builders;
+
+namespace FilterLists.Api.Migrations.Extensions
+{
+    public static class MigrationBuilderExtensions
+    {
+        public static OperationBuilder<SqlOperation> CreateIndex(this MigrationBuilder migrationBuilder, string name,
+            string table, string column, int prefixLength, string schema = null, bool unique = false,
+            string filter = null)
+        {
+            throw new NotImplementedException();
+            //return migrationBuilder.Sql($"CREATE INDEX {name} ON {table} (key(10));");
+        }
+
+        public static OperationBuilder<SqlOperation> DropIndex(this MigrationBuilder migrationBuilder, string name,
+            string table = null, string schema = null)
+        {
+            throw new NotImplementedException();
+            //return migrationBuilder.Sql($"CREATE INDEX index_name ON misc_info (key(10));");
+        }
+    }
+}
