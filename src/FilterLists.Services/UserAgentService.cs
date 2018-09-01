c5790a998561c09db0ef868472d2d0706746d9c7
Collin Barrett <collinbarrett@users.noreply.github.com>
2018-09-01 14:36:13 -0500

    bump default ua

--- src/FilterLists.Services/UserAgentService.cs
+++ src/FilterLists.Services/UserAgentService.cs
@@ -13,7 +13,7 @@ namespace FilterLists.Services
         private const string UaStringSourceClass = "get-the-list";
 
         private const string UaStringDefault =
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36";
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36";
 
         public static async Task<string> GetMostPopularString()
         {
