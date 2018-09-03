ec9b12a0ef6ce1d3091160efe8fafd31fb93f984
Collin Barrett <collinbarrett@users.noreply.github.com>
2018-09-03 14:49:54 -0500

    add Sofwater to summary endpoint
    ref #268

--- src/FilterLists.Services/FilterList/MappingProfiles/ListSummaryDtoMappingProfile.cs
+++ src/FilterLists.Services/FilterList/MappingProfiles/ListSummaryDtoMappingProfile.cs
@@ -11,6 +11,9 @@ namespace FilterLists.Services.FilterList.MappingProfiles
         public ListSummaryDtoMappingProfile() =>
             CreateMap<Data.Entities.FilterList, ListSummaryDto>()
                 .ForMember(d => d.Languages, o => o.MapFrom(l => l.FilterListLanguages.Select(la => la.Language)))
+                .ForMember(d => d.Software,
+                    o => o.MapFrom(l =>
+                        l.Syntax.SoftwareSyntaxes.Select(ss => ss.Software.Name)))
                 .ForMember(d => d.Tags, o => o.MapFrom(l => l.FilterListTags.Select(m => m.Tag)))
                 .ForMember(d => d.UpdatedDate,
                     o => o.MapFrom(l =>
