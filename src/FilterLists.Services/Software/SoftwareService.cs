f3ee48117b0c35a6d5850c10569b0981786505d9
Collin Barrett <collinbarrett@users.noreply.github.com>
2018-09-03 15:58:00 -0500

    add software endpoint
    ref #268

---
+++ src/FilterLists.Services/Software/SoftwareService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using FilterLists.Data;
+using FilterLists.Services.Software.Models;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilterLists.Services.Software
+{
+    [UsedImplicitly]
+    public class SoftwareService : Service
+    {
+        public SoftwareService(FilterListsDbContext dbContext, IConfigurationProvider mapConfig)
+            : base(dbContext, mapConfig)
+        {
+        }
+
+        public async Task<IEnumerable<SoftwareDto>> GetAll() =>
+            await DbContext.Software.ProjectTo<SoftwareDto>(MapConfig).ToListAsync();
+    }
+}
