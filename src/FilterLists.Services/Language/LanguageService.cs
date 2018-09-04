9f19122047a33716430256978e8d61df3553e2f8
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-09-03 19:24:59 -0500

    add languages endpoint
    ref #379

---
+++ src/FilterLists.Services/Language/LanguageService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using FilterLists.Data;
+using FilterLists.Services.Language.Models;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilterLists.Services.Language
+{
+    [UsedImplicitly]
+    public class LanguageService : Service
+    {
+        public LanguageService(FilterListsDbContext dbContext, IConfigurationProvider mapConfig)
+            : base(dbContext, mapConfig)
+        {
+        }
+
+        public async Task<IEnumerable<LanguageDto>> GetAllTargeted() =>
+            await DbContext.Languages.Where(l => l.FilterListLanguages.Any())
+                           .OrderBy(s => s.Name)
+                           .ProjectTo<LanguageDto>(MapConfig)
+                           .ToListAsync();
+    }
+}
