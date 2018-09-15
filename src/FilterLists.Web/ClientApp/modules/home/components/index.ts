f75af198076d15edb2bac9f9bd52d70c76eea09f
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-09-15 12:03:38 -0500

    fix some pathing issues with ListsTable component

--- src/FilterLists.Web/ClientApp/modules/home/components/index.ts
+++ src/FilterLists.Web/ClientApp/modules/home/components/index.ts
@@ -1,6 +1,6 @@
 ﻿import { DetailsExpander } from "./detailsExpander";
-import { ListsTable } from "./ListsTable";
-import { IColumnVisibility } from "./ListsTable";
+import { ListsTable } from "./listsTable";
+import { IColumnVisibility } from "./listsTable";
 import { Tagline } from "./Tagline";
 
 export {
