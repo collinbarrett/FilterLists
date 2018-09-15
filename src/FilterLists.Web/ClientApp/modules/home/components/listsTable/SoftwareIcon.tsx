6824390b1349bd2400081c38ca99f2911337e423
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-09-15 17:52:50 -0500

    add more software icons
    ref #470

--- src/FilterLists.Web/ClientApp/modules/home/components/listsTable/SoftwareIcon.tsx
+++ src/FilterLists.Web/ClientApp/modules/home/components/listsTable/SoftwareIcon.tsx
@@ -4,6 +4,9 @@ import img2 from "../../../../imgs/software/2-Adblock-Plus.svg";
 import img3 from "../../../../imgs/software/3-AdGuard.png";
 import img4 from "../../../../imgs/software/4-DNS66.png";
 import img5 from "../../../../imgs/software/5-Nano-Adblocker.png";
+import img6 from "../../../../imgs/software/6-AdBlock.png";
+import img7 from "../../../../imgs/software/7-AdAway.png";
+import img8 from "../../../../imgs/software/8-Personal-Blocklist.png";
 
 interface IProps {
     id: number;
@@ -28,5 +31,8 @@ const icons: { [id: number]: IIcon; } = {
     2: { image: img2, imageTitle: "Adblock Plus" },
     3: { image: img3, imageTitle: "AdGuard" },
     4: { image: img4, imageTitle: "DNS66" },
-    5: { image: img5, imageTitle: "Nano Adblocker" }
+    5: { image: img5, imageTitle: "Nano Adblocker" },
+    6: { image: img6, imageTitle: "AdBlock" },
+    7: { image: img7, imageTitle: "AdAway" },
+    8: { image: img8, imageTitle: "Personal Blocklist" }
 };
