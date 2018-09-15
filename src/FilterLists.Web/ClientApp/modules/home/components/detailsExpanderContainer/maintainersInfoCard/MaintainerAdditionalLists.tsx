30ea652b5bff14121164929960572833d6061ec9
Collin Barrett <collinbarrett@users.noreply.github.com>
2018-09-14 20:46:17 -0500

    fix maintainer buttons alignment

--- src/FilterLists.Web/ClientApp/modules/home/components/detailsExpanderContainer/maintainersInfoCard/MaintainerAdditionalLists.tsx
+++ src/FilterLists.Web/ClientApp/modules/home/components/detailsExpanderContainer/maintainersInfoCard/MaintainerAdditionalLists.tsx
@@ -7,14 +7,16 @@ interface IProps {
 }
 
 export const MaintainerAdditionalLists = (props: IProps) => {
-    return props.additionalLists.length > 0
-               ? <div className="col-9">
-                     <div>
-                         <h4>More by {props.name}:</h4>
-                         <ul>
-                             {props.additionalLists.map((l: IMaintainerAdditionalListDto) => <li>{l.name}</li>)}
-                         </ul>
-                     </div>
-                 </div>
-               : null;
+    return <div className="col-9">
+               {props.additionalLists.length > 0
+                    ? <div>
+
+                          <h4>More by {props.name}:</h4>
+                          <ul>
+                              {props.additionalLists.map((l: IMaintainerAdditionalListDto) => <li>{l.name}</li>)}
+                          </ul>
+
+                      </div>
+                    : null}
+           </div>;
 };
