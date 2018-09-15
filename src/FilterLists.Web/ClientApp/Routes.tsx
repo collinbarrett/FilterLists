ed08b2d04093d34a36f334627ea38b9cfca5d6e5
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-09-15 12:46:50 -0500

    redirect unsupported routes

--- src/FilterLists.Web/ClientApp/Routes.tsx
+++ src/FilterLists.Web/ClientApp/Routes.tsx
@@ -1,8 +1,13 @@
 import * as React from "react";
 import { Layout } from "./Layout";
-import { Route } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 import { Home } from "./modules";
 
 export const Routes = <Layout>
-                          <Route exact path="/" component={ Home }/>
+                          <Switch>
+                              <Route exact path="/" component={Home}/>
+                              <Route>
+                                  <Redirect to="/"/>
+                              </Route>
+                          </Switch>
                       </Layout>;
