c4e53c37c0d021c9e2d3765018480311b6ce1c21
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-09-16 08:23:14 -0500

    AddIsAbpSubscribable
    ref #28

---
+++ src/FilterLists.Api/Migrations/20180916131916_AddIsAbpSubscribable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FilterLists.Api.Migrations
+{
+    public partial class AddIsAbpSubscribable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                "IsAbpSubscribable",
+                "software",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                "IsAbpSubscribable",
+                "software");
+        }
+    }
+}
--- src/FilterLists.Services/Seed/Models/SoftwareSeedDto.cs
+++ src/FilterLists.Services/Seed/Models/SoftwareSeedDto.cs
@@ -8,6 +8,7 @@ namespace FilterLists.Services.Seed.Models
         public uint Id { get; set; }
         public string DownloadUrl { get; set; }
         public string HomeUrl { get; set; }
+        public bool IsAbpSubscribable { get; set; }
         public string Name { get; set; }
     }
 }
