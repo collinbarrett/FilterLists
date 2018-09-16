4fd4bb5b894f3e48962681be8398cd33cb9aaa44
Collin Barrett <collinbarrett@users.noreply.github.com>
2018-09-16 08:23:14 -0500

    AddIsAbpSubscribable
    ref #28

--- src/FilterLists.Data/Entities/Software.cs
+++ src/FilterLists.Data/Entities/Software.cs
@@ -9,6 +9,7 @@ namespace FilterLists.Data.Entities
     {
         public string DownloadUrl { get; set; }
         public string HomeUrl { get; set; }
+        public bool IsAbpSubscribable { get; set; }
         public string Name { get; set; }
         public ICollection<SoftwareSyntax> SoftwareSyntaxes { get; set; }
     }
