bd6343984595a05d5ece36ed562c997a25e052d4
Collin Barrett <collinbarrett@users.noreply.github.com>
2018-09-21 20:00:02 -0500

    fix api version in last

--- src/FilterLists.Api/V1/Controllers/BaseController.cs
+++ src/FilterLists.Api/V1/Controllers/BaseController.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Caching.Memory;
 
 namespace FilterLists.Api.V1.Controllers
 {
-    [ApiVersion("1")]
+    [ApiVersion("1.0")]
     //TODO: use versioning without needing to manually specify in swagger-ui (https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/370)
     [Route("v{version:apiVersion}/[controller]")]
     [ResponseCache(Duration = 86400)]
