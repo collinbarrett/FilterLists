96f8c014f18ff2e702d021027684e4f50f756d81
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-09-21 19:57:01 -0500

    Squashed commit of the following:
    commit 5624d5049d0efa99dbb2d7c8453c0c756eb283aa
    Author: Collin Barrett <collinbarrett@users.noreply.github.com>
    Date:   Fri Sep 21 19:48:36 2018 -0500
    
        move index to new endpoint for testing
    
        ref #505
    
    commit e49f3b6312372078f9e57a451cfae4df06145597
    Author: Collin Barrett <collinbarrett@users.noreply.github.com>
    Date:   Fri Sep 21 19:40:29 2018 -0500
    
        restore camelCase
    
    commit 94fa3c5f4ef1e47c80108c0648a5f3597d305ba7
    Author: Collin Barrett <collinbarrett@users.noreply.github.com>
    Date:   Fri Sep 21 19:20:38 2018 -0500
    
        ignore nulls and empties from json
    
    commit 5bc704ffad168ae90cbe232161d3b205d8855b07
    Author: collinbarrett <collinbarrett@users.noreply.github.com>
    Date:   Fri Sep 21 12:40:25 2018 -0500
    
        add maintainers
    
    commit 68bab5bb1c3394e960ec2695c6f51f5adace1cec
    Merge: 106edf5 71a3d75
    Author: collinbarrett <collinbarrett@users.noreply.github.com>
    Date:   Fri Sep 21 10:35:43 2018 -0500
    
        Merge branch 'master' into refactor-api
    
    commit 106edf5b96d4d2296374ef60c8d31923316199d2
    Author: Collin Barrett <collinbarrett@users.noreply.github.com>
    Date:   Thu Sep 20 06:15:49 2018 -0500
    
        wip
    
    commit 1387ef5836165f716b59df19ca6fca5959c65ee8
    Author: Collin Barrett <collinbarrett@users.noreply.github.com>
    Date:   Thu Sep 20 05:54:16 2018 -0500
    
        wip
    
    commit fcebd19ecb583dd76112c0c4d2c1d9b1c2f0d766
    Author: Collin Barrett <collinbarrett@users.noreply.github.com>
    Date:   Wed Sep 19 21:11:47 2018 -0500
    
        wip
    
    commit e9d78450e85120a0002d5a4f528aca6992d63646
    Author: Collin Barrett <collinbarrett@users.noreply.github.com>
    Date:   Wed Sep 19 18:23:08 2018 -0500
    
        wip
    
    commit bb915dcfe605278a941e5ac0cfc50c363d744c41
    Author: collinbarrett <collinbarrett@users.noreply.github.com>
    Date:   Wed Sep 19 15:49:12 2018 -0500
    
        RuleCount wip
    
    commit 8e1c0fffed9af0485dec07f33ee130d6b9ecb305
    Author: collinbarrett <collinbarrett@users.noreply.github.com>
    Date:   Wed Sep 19 15:17:28 2018 -0500
    
        use IReadOnlyCollection
    
        per https://stackoverflow.com/questions/52368567/optimization-of-correlated-subqueries-with-automapper?noredirect=1#comment91770378_52368567
    
    commit 6362d5891c728cf83bf2e3bdbb4b244c56aafdc9
    Author: Collin Barrett <collinbarrett@users.noreply.github.com>
    Date:   Wed Sep 19 05:38:54 2018 -0500
    
        wip

---
+++ src/FilterLists.Services/FilterList/Models/ListIndexRecord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace FilterLists.Services.FilterList.Models
+{
+    [UsedImplicitly]
+    public class ListIndexRecord
+    {
+        public int Id { get; set; }
+        public List<int> LanguageIds { get; set; }
+        public List<int> MaintainerIds { get; set; }
+        public string Name { get; set; }
+        public int? RuleCount { get; set; }
+        public int? SyntaxId { get; set; }
+        public List<int> TagIds { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string ViewUrl { get; set; }
+        public List<string> ViewUrlMirrors { get; set; }
+    }
+}
