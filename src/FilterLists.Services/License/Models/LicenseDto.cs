59f63e395b59202214bd305ae57e06b41f2f56ec
collinbarrett <collinbarrett@users.noreply.github.com>
2018-09-26 11:14:26 -0500

    add API licenses IGet endpoints
    ref #505

---
+++ src/FilterLists.Services/License/Models/LicenseDto.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+
+namespace FilterLists.Services.License.Models
+{
+    [UsedImplicitly]
+    public class LicenseDto
+    {
+        public int Id { get; set; }
+        public string DescriptionUrl { get; set; }
+        public string Name { get; set; }
+    }
+}
