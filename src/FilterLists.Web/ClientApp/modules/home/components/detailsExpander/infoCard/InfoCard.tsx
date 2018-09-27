4913595803375db69d8222c1807b0d99e081487d
Collin Barrett <collinbarrett@users.noreply.github.com>
2018-09-27 18:03:51 -0500

    tidy InfoCard

--- src/FilterLists.Web/ClientApp/modules/home/components/detailsExpander/infoCard/InfoCard.tsx
+++ src/FilterLists.Web/ClientApp/modules/home/components/detailsExpander/infoCard/InfoCard.tsx
@@ -26,25 +26,25 @@ interface IProps {
     updatedDate: string;
 };
 
-export const InfoCard = (props: IProps) => 
-           <div className="col-9">
-               <TagGroup tags={props.tags}/>
-               <div className="d-md-none">
-                   {props.syntax
-                        ? props.syntax.supportedSoftware.map(
-                            (s: ISyntaxSupportedSoftware, i: number) => <a href={s.homeUrl} key={i}>
-                                                                            <SoftwareIcon id={s.id} key={i}/>
-                                                                        </a>)
-                        : null}
-               </div>
-               <Description {...props} url={props.descriptionSourceUrl}/>
-               <ul className="list-group list-group-flush">
-                   <Languages languages={...props.languages}/>
-                   <RuleCount {...props}/>
-                   <DiscontinuedDate date={props.discontinuedDate}/>
-                   <UpdatedDate {...props}/>
-                   <PublishedDate date={props.publishedDate}/>
-                   <Syntax {...props.syntax}/>
-                   <License {...props.license}/>
-               </ul>
-           </div>;
+export const InfoCard = (props: IProps) =>
+    <div className="col-9">
+        <TagGroup tags={props.tags}/>
+        <div className="d-md-none">
+            {props.syntax
+                 ? props.syntax.supportedSoftware.map(
+                     (s: ISyntaxSupportedSoftware, i: number) => <a href={s.homeUrl} key={i}>
+                                                                     <SoftwareIcon id={s.id} key={i}/>
+                                                                 </a>)
+                 : null}
+        </div>
+        <Description {...props} url={props.descriptionSourceUrl}/>
+        <ul className="list-group list-group-flush">
+            <Languages {...props}/>
+            <RuleCount {...props}/>
+            <DiscontinuedDate date={props.discontinuedDate}/>
+            <UpdatedDate {...props}/>
+            <PublishedDate date={props.publishedDate}/>
+            <Syntax {...props.syntax}/>
+            <License {...props.license}/>
+        </ul>
+    </div>;
