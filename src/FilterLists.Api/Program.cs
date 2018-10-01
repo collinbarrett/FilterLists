47d242248d6997b8e80c90478db8d9e137856937
thuanpt <thuanpt.dev@gmail.com>
2018-10-01 14:37:51 +0700

    add async seeding in Main()

--- src/FilterLists.Api/Program.cs
+++ src/FilterLists.Api/Program.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using FilterLists.Api.DependencyInjection.Extensions;
+using FilterLists.Data.Seed.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FilterLists.Api
 {
     public static class Program
     {
-        public static void Main(string[] args) => CreateWebHostBuilder(args).Build().Run();
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().MigrateDbContext<Data.FilterListsDbContext>((context,service)=>
+            {
 
+                var dataPath = service.GetService<IConfiguration>()["DataDirectory:Path"].ToString();
+                new SeedFilterListsDbContext().SeedOrUpdateAsync(context,dataPath).Wait();
+            }).Run();
+        }
         private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                    .UseUrls("http://localhost:5000")
