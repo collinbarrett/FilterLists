57e50eff930586cf1e329aa27a591652785f30d6
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-10-09 19:39:49 -0500

    add Subscribe column filterable by viewUrl
    closes #311

--- src/FilterLists.Web/ClientApp/modules/home/components/listsTable/columns/DetailsButton.tsx
+++ src/FilterLists.Web/ClientApp/modules/home/components/listsTable/columns/DetailsButton.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Column, RowRenderProps } from "react-table";
+import "../../linkButtons/button.css";
 
 export const DetailsButton = {
     Header: <span title="Learn more about the FilterList.">Details</span>,
@@ -8,17 +9,17 @@ export const DetailsButton = {
     expander: true,
     Expander: ({ isExpanded, row }: RowRenderProps) => Expander({ isExpanded, row }),
     style: { textAlign: "center" },
-    width: 90
+    width: 110
 } as Column;
 
 const Expander = (props: RowRenderProps) =>
     <div>
         {props.isExpanded
-             ? <button className="btn btn-primary btn-block active"
+             ? <button className="btn btn-primary btn-block active fl-btn-link"
                        title={`Collapse details about ${props.row.name}.`}>
                    Details
                </button>
-             : <button className="btn btn-primary btn-block"
+             : <button className="btn btn-primary btn-block fl-btn-link"
                        title={`Learn more about ${props.row.name}.`}>
                    Details
                </button>}
