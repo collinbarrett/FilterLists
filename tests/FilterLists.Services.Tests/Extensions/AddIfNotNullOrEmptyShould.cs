5dda4c8e03930be15259213b437b27c4d7afcedb
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-10-12 21:37:14 -0500

    finish batch of AddIfNotNullOrEmptyShould tests

--- tests/FilterLists.Services.Tests/Extensions/AddIfNotNullOrEmptyShould.cs
+++ tests/FilterLists.Services.Tests/Extensions/AddIfNotNullOrEmptyShould.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using FilterLists.Services.Extensions;
 using Xunit;
@@ -6,15 +7,34 @@ namespace FilterLists.Services.Tests.Extensions
 {
     public class AddIfNotNullOrEmptyShould
     {
+        public AddIfNotNullOrEmptyShould() =>
+            sut = new Collection<string>();
+
+        private readonly ICollection<string> sut;
+        private string item;
+
         [Fact]
         public void AddIfNotNullOrEmpty()
         {
-            var sut = new Collection<string>();
-            const string item = "item";
-
+            item = "item";
             sut.AddIfNotNullOrEmpty(item);
-
             Assert.Contains(item, sut);
+        }
+
+        [Fact]
+        public void NotAddIfEmpty()
+        {
+            item = "";
+            sut.AddIfNotNullOrEmpty(item);
+            Assert.DoesNotContain(item, sut);
+        }
+
+        [Fact]
+        public void NotAddIfNull()
+        {
+            item = null;
+            sut.AddIfNotNullOrEmpty(item);
+            Assert.DoesNotContain(item, sut);
         }
     }
 }
