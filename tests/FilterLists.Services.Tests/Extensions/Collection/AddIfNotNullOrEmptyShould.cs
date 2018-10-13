9f8f6a4c6d62fc53414621142995a421a6c00905
collinbarrett <collinbarrett@users.noreply.github.com>
2018-10-13 11:39:38 -0500

    add IsValidHttpOrHttpsUrlShould tests

---
+++ tests/FilterLists.Services.Tests/Extensions/Collection/AddIfNotNullOrEmptyShould.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using FilterLists.Services.Extensions;
+using Xunit;
+
+namespace FilterLists.Services.Tests.Extensions.Collection
+{
+    public class AddIfNotNullOrEmptyShould
+    {
+        public AddIfNotNullOrEmptyShould() =>
+            sut = new Collection<string>();
+
+        private readonly ICollection<string> sut;
+        private string item;
+
+        [Fact]
+        public void AddIfNotNullOrEmpty()
+        {
+            item = "item";
+            sut.AddIfNotNullOrEmpty(item);
+            Assert.Contains(item, sut);
+        }
+
+        [Fact]
+        public void NotAddIfEmpty()
+        {
+            item = "";
+            sut.AddIfNotNullOrEmpty(item);
+            Assert.DoesNotContain(item, sut);
+        }
+
+        [Fact]
+        public void NotAddIfNull()
+        {
+            item = null;
+            sut.AddIfNotNullOrEmpty(item);
+            Assert.DoesNotContain(item, sut);
+        }
+    }
+}
---
+++ tests/FilterLists.Services.Tests/Extensions/String/IsValidHttpOrHttpsUrlShould.cs
@@ -0,0 +1,29 @@
+﻿using FilterLists.Services.Extensions;
+using Xunit;
+
+namespace FilterLists.Services.Tests.Extensions.String
+{
+    public class IsValidHttpOrHttpsUrlShould
+    {
+        [Fact]
+        public void ReturnFalseIfNotHttpOrHttps()
+        {
+            const string abp = "abp://www.google.com";
+            Assert.False(abp.IsValidHttpOrHttpsUrl());
+        }
+
+        [Fact]
+        public void ReturnTrueIfValidHttpsUrl()
+        {
+            const string httpsUrl = "https://www.google.com";
+            Assert.True(httpsUrl.IsValidHttpOrHttpsUrl());
+        }
+
+        [Fact]
+        public void ReturnTrueIfValidHttpUrl()
+        {
+            const string httpUrl = "http://www.google.com";
+            Assert.True(httpUrl.IsValidHttpOrHttpsUrl());
+        }
+    }
+}
