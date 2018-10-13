334666c335911e5da7074e399a89da6eea1e518d
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-10-13 11:39:38 -0500

    add IsValidHttpOrHttpsUrlShould tests

---
+++ tests/FilterLists.Services.Tests/Extensions/Collection/AddRangeShould.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using FilterLists.Services.Extensions;
+using Xunit;
+
+namespace FilterLists.Services.Tests.Extensions.Collection
+{
+    public class AddRangeShould
+    {
+        public AddRangeShould() =>
+            sut = new Collection<string>();
+
+        private readonly ICollection<string> sut;
+        private IEnumerable<string> range;
+
+        [Fact]
+        public void AddCollectionOfStrings()
+        {
+            range = new Collection<string> {"item1", "item2"};
+            sut.AddRange(range);
+            Assert.True(!range.Except(sut).Any());
+        }
+
+        [Fact]
+        public void AddListOfStrings()
+        {
+            range = new List<string> {"item1", "item2"};
+            sut.AddRange(range);
+            Assert.True(!range.Except(sut).Any());
+        }
+    }
+}
