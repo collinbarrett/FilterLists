c78abcc5882aad0d31d19e5fc44a8d32d174385e
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-10-15 11:40:40 -0500

    add GetMostPopularStringAsyncShould test

--- src/FilterLists.Services/UserAgentService.cs
+++ src/FilterLists.Services/UserAgentService.cs
@@ -13,7 +13,7 @@ namespace FilterLists.Services
         private const string UaStringDefault =
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36";
 
-        public static async Task<string> GetMostPopularString()
+        public static async Task<string> GetMostPopularStringAsync()
         {
             try
             {
---
+++ tests/FilterLists.Services.Tests/UserAgent/GetMostPopularStringAsyncShould.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace FilterLists.Services.Tests.UserAgent
+{
+    public class GetMostPopularStringAsyncShould
+    {
+        [Fact]
+        public async void ReturnStringContainingMozilla()
+        {
+            var actualString = await UserAgentService.GetMostPopularStringAsync();
+            Assert.Contains("Mozilla", actualString);
+        }
+    }
+}
