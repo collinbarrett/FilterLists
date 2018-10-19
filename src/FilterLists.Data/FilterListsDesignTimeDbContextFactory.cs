acfd89ba9f11afd29a17d34c4ab8a5768c9c5b05
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-10-19 07:26:10 -0500

    disable blocked FilterListsDesignTimeDbContextFactory
    ref #594

--- src/FilterLists.Data/FilterListsDesignTimeDbContextFactory.cs
+++ src/FilterLists.Data/FilterListsDesignTimeDbContextFactory.cs
@@ -5,27 +5,28 @@ using Microsoft.EntityFrameworkCore.Design;
 
 namespace FilterLists.Data
 {
-    [UsedImplicitly]
-    public class FilterListsDesignTimeDbContextFactory : IDesignTimeDbContextFactory<FilterListsDbContext>
-    {
-        private readonly int commandTimeoutSecs = (int)TimeSpan.FromMinutes(30).TotalSeconds;
+    //TODO: [BLOCKED] pass connection string https://github.com/aspnet/EntityFrameworkCore/issues/8332
+    //[UsedImplicitly]
+    //public class FilterListsDesignTimeDbContextFactory : IDesignTimeDbContextFactory<FilterListsDbContext>
+    //{
+    //    private readonly int commandTimeoutSecs = (int)TimeSpan.FromMinutes(30).TotalSeconds;
 
-        /// <summary>
-        ///     increases command timeout for design time migrations
-        /// </summary>
-        public FilterListsDbContext CreateDbContext(string[] args)
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<FilterListsDbContext>();
-            optionsBuilder.UseMySql(
-                @"", //BLOCKED: pass connection string https://github.com/aspnet/EntityFrameworkCore/issues/8332
-                o =>
-                {
-                    o.MigrationsAssembly("FilterLists.Api");
-                    o.CommandTimeout(commandTimeoutSecs);
-                }
-            );
+    //    /// <summary>
+    //    ///     increases command timeout for design time migrations
+    //    /// </summary>
+    //    public FilterListsDbContext CreateDbContext(string[] args)
+    //    {
+    //        var optionsBuilder = new DbContextOptionsBuilder<FilterListsDbContext>();
+    //        optionsBuilder.UseMySql(
+    //            @"",
+    //            o =>
+    //            {
+    //                o.MigrationsAssembly("FilterLists.Api");
+    //                o.CommandTimeout(commandTimeoutSecs);
+    //            }
+    //        );
 
-            return new FilterListsDbContext(optionsBuilder.Options);
-        }
-    }
+    //        return new FilterListsDbContext(optionsBuilder.Options);
+    //    }
+    //}
 }
