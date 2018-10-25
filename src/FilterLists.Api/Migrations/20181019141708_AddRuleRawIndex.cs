13644308f550cc184879f7aeb2ac0d834e227c70
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2018-10-25 12:04:21 -0500

    restore index size, try converting to mariadb in CI
    ref #296

--- src/FilterLists.Api/Migrations/20181019141708_AddRuleRawIndex.cs
+++ src/FilterLists.Api/Migrations/20181019141708_AddRuleRawIndex.cs
@@ -11,7 +11,7 @@ namespace FilterLists.Api.Migrations
                 "IX_rules_Raw",
                 "rules",
                 "Raw",
-                10); //191);
+                3072);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
