11a06694db80800ca53efebac4728bc4999ee7ec
Imre Kristoffer Eilertsen <imreeil42@gmail.com>
2019-02-14 08:37:14 +0100

    Update index.ts

--- src/FilterLists.Web/ClientApp/modules/home/components/softwareIcon/imgs/index.ts
+++ src/FilterLists.Web/ClientApp/modules/home/components/softwareIcon/imgs/index.ts
@@ -1,4 +1,4 @@
-﻿import img1 from "./01-uBlock-Origin.svg";
+import img1 from "./01-uBlock-Origin.svg";
 import img2 from "./02-Adblock-Plus.svg";
 import img3 from "./03-AdGuard.png";
 import img4 from "./04-DNS66.png";
@@ -7,15 +7,21 @@ import img6 from "./06-AdBlock.png";
 import img7 from "./07-AdAway.png";
 import img8 from "./08-Personal-Blocklist.png";
 import img10 from "./10-Redirector.png";
+import img11 from "./11-Hosts-File-Editor.png";
 import img12 from "./12-Gas-Mask.png";
 import img13 from "./13-MinerBlock.svg";
 import img14 from "./14-Pi-hole.png";
 import img15 from "./15-uBlock.svg";
 import img16 from "./16-Internet-Explorer-TPL.png";
+import img17 from "./17-Google-Hit-Hider-by-Domain.png";
 import img18 from "./18-FireHOL.png";
 import img19 from "./19-Samsung-Knox.png";
 import img20 from "./20-Little-Snitch.png";
 import img21 from "./21-Privoxy.png";
+import img22 from "./22-Diversion.png";
+import img23 from "./23-dnsmasq.png";
+import img24 from "./24-Slimjet.png";
+import img25 from "./25-uMatrix.png";
 
 export {
     img1,
@@ -27,13 +33,19 @@ export {
     img7,
     img8,
     img10,
+    img11,
     img12,
     img13,
     img14,
     img15,
     img16,
+    img17,
     img18,
     img19,
     img20,
-    img21
+    img21,
+    img22,
+    img23,
+    img24,
+    img25
     };
