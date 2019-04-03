252733f0d2b0d2058a67326148adff18af9d87dd
Imre Kristoffer Eilertsen <imreeil42@gmail.com>
2019-04-03 14:27:13 +0200

    Update ViewButton.tsx

--- src/FilterLists.Web/ClientApp/modules/home/components/linkButtons/ViewButton.tsx
+++ src/FilterLists.Web/ClientApp/modules/home/components/linkButtons/ViewButton.tsx
@@ -10,6 +10,7 @@ export const ViewButton = (props: IProps) => {
 let buttonClass: string | undefined;
 let text: string;
     
+    let buttonClass
     if (props.url.indexOf(".onion/") > 0) {
         buttonClass = "btn-success";
     } else if (props.url.indexOf(".7z") > 0) {
@@ -23,11 +24,13 @@ let text: string;
     else if (props.url.indexOf(".7z",".zip") > 0) { title = `Download ${props.name} as a compressed archive.`; }
     else { title = `View ${props.name} in its raw format`; };
     
+    let text
     if (props.url.indexOf(".7z") > 0) { text = `Download`; }
 else { text = `View`; };
 
 return props.url
     ? <LinkButton href={props.url}
                   title={title}
-                  text={text}/>
+                  text={text}
+                  buttonClass={buttonClass}/>
     : null;
