b851472c98fc99f4b61d4c9470f980467776db67
Imre Kristoffer Eilertsen <imreeil42@gmail.com>
2019-04-03 14:16:29 +0200

    Let's see how well this goes

--- src/FilterLists.Web/ClientApp/modules/home/components/linkButtons/ViewButton.tsx
+++ src/FilterLists.Web/ClientApp/modules/home/components/linkButtons/ViewButton.tsx
@@ -4,12 +4,33 @@ import { LinkButton } from "./LinkButton";
 interface IProps {
     name: string;
     url: string;
-    text?: string;
 };
 
-export const ViewButton = (props: IProps) =>
-    props.url
+export const ViewButton = (props: IProps) => {
+let buttonClass: string | undefined;
+let text: string;
+    
+    if (props.url.indexOf(".onion/") > 0) {
+        buttonClass = "btn-success";
+        titlePrefix = "Tor address - ";
+    } else if (props.url.indexOf(".7z",".zip") > 0) {
+        buttonClass = "btn-warning";
+        titlePrefix = "Not Secure - ";
+    } else {
+        buttonClass = undefined;
+        titlePrefix = "";
+    }
+    
+    let title;
+    if (props.url.indexOf(".onion/") > 0) { title = `Tor address - View ${props.name} in its raw format.`; }
+    else if (props.url.indexOf(".7z",".zip") > 0) { title = `Download ${props.name} as a compressed archive.`; }
+    else { title = `View ${props.name} in its raw format`; };
+    
+    if (props.url.indexOf(".7z",".zip") > 0) { text = `Download`; }
+else { text = `View`; };
+
+return props.url
     ? <LinkButton href={props.url}
-                  title={`View ${props.name} in its raw format.`}
-                  text={props.text || "View"}/>
+                  title={title}
+                  text={text}/>
     : null;
