1c7bd8b7e910d15e512f5b5361bfedc938ed7518
Imre Kristoffer Eilertsen <imreeil42@gmail.com>
2019-04-04 07:57:07 +0200

    I can just as well try with additional compressed formats

--- src/FilterLists.Web/ClientApp/modules/home/components/linkButtons/ViewButton.tsx
+++ src/FilterLists.Web/ClientApp/modules/home/components/linkButtons/ViewButton.tsx
@@ -11,7 +11,9 @@ export const ViewButton = (props: IProps) => {
     
     let title;
     if (props.url.indexOf(".onion/") > 0) { title = `Tor address - View ${props.name} in its raw format.`; }
-    else if (props.url.indexOf(".zip") > 0) { title = `Download ${props.name} as a compressed archive.`; }
+    else if (props.url.indexOf(".zip") > 0) { title = `Download ${props.name} as a ZIP compressed archive.`; }
+    else if (props.url.indexOf(".7z") > 0) { title = `Download ${props.name} as a 7Z compressed archive.`; }
+    else if (props.url.indexOf(".tar.gz") > 0) { title = `Download ${props.name} as a compressed tarball archive.`; }
     else { title = `View ${props.name} in its raw format`; };
 
 return props.url
