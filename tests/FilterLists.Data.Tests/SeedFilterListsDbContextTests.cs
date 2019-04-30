abb1149aece362af8621f47edfd336d702658bf1
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-04-30 06:57:45 -0500

    data int test wip

--- tests/FilterLists.Data.Tests/SeedFilterListsDbContextTests.cs
+++ tests/FilterLists.Data.Tests/SeedFilterListsDbContextTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using FilterLists.Data.Seed.Extensions;
 using Microsoft.EntityFrameworkCore;
@@ -8,35 +9,26 @@ namespace FilterLists.Data.Tests
 {
     public class SeedFilterListsDbContextTests
     {
-        private const string CiTestServerConnString = "Server=localhost;Uid=travis;";
-        private const string CiTestDatabaseConnString = CiTestServerConnString + "Database=filterlistsdatatest;";
-
-        /// <summary>
-        ///     requires "CREATE USER 'travis'@'localhost';" on non-Travis mysql instance
-        /// </summary>
         [Fact]
         public async void SeedOrUpdateAsync_DoesNotThrowException()
         {
+            var connString = "Server=mariadb;Database=filterlists;Uid=root;Pwd=filterlists;";
             var options = new DbContextOptionsBuilder<FilterListsDbContext>()
-                          .UseMySql(CiTestDatabaseConnString, m => m.MigrationsAssembly("FilterLists.Api"))
+                          .UseMySql(connString, m => m.MigrationsAssembly("FilterLists.Api"))
                           .Options;
             using (var context = new FilterListsDbContext(options))
             {
-                await context.Database.EnsureDeletedAsync();
-                using (var conn = new MySqlConnection(CiTestServerConnString))
+                using (var conn = new MySqlConnection(connString))
                 {
                     await conn.OpenAsync();
                     var cmd = new MySqlCommand(
-                        "CREATE DATABASE filterlistsdatatest CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci;",
+                        "CREATE DATABASE filterlists CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci;",
                         conn);
                     await cmd.ExecuteNonQueryAsync();
                 }
 
                 await context.Database.MigrateAsync();
-                if (Directory.Exists("../../../../../data"))
-                    await SeedFilterListsDbContext.SeedOrUpdateAsync(context, "../../../../../data");
-                else
-                    await SeedFilterListsDbContext.SeedOrUpdateAsync(context, "../../../../../../../../data");
+                await SeedFilterListsDbContext.SeedOrUpdateAsync(context, "../../../data");
             }
         }
     }
