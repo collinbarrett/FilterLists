cb6860c9da708a92d6cab0f5631e99f09c5ed4f1
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-06-25 16:00:02 -0500

    flesh out todo list

--- src/FilterLists.Agent/Program.cs
+++ src/FilterLists.Agent/Program.cs
@@ -2,8 +2,23 @@
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
+            // get raw list urls and IDs
+
+            // foreach
+
+                // download raw list
+
+                // persist raw list to disk with standardized name and overwriting the previous version
+
+            // git add .
+
+            // git commit
+
+            // foreach
+
+                // upsert into MariaDB Rules table https://stackoverflow.com/questions/15271202/mysql-load-data-infile-with-on-duplicate-key-update
         }
     }
 }
