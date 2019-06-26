3f00981b98ebff6f91e79c9b0ab58e48e97176b6
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-06-26 12:59:35 -0500

    wire up Autofac

--- src/FilterLists.Agent/Program.cs
+++ src/FilterLists.Agent/Program.cs
@@ -1,24 +1,49 @@
-﻿namespace FilterLists.Agent
+﻿using System;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FilterLists.Agent
 {
     public static class Program
     {
+        //TODO:  get raw list urls and IDs
+        //TODO:  foreach list, download and persist raw list to disk with standardized name and overwriting the previous version
+        //TODO:  git add .
+        //TODO:  git commit
+        //TODO:  foreach list, upsert into MariaDB Rules table https://stackoverflow.com/questions/15271202/mysql-load-data-infile-with-on-duplicate-key-update
+
+        private static IServiceProvider _serviceProvider;
+
         public static void Main()
         {
-            // get raw list urls and IDs
+            RegisterServices();
 
-            // foreach
+            //var service = _serviceProvider.GetService<IDemoService>();
+            //service.DoSomething();
 
-                // download raw list
+            DisposeServices();
+        }
 
-                // persist raw list to disk with standardized name and overwriting the previous version
+        private static void RegisterServices()
+        {
+            var containerBuilder = new ContainerBuilder();
+            //builder.RegisterType<DemoService>().As<IDemoService>();
+            containerBuilder.Populate(new ServiceCollection());
+            var container = containerBuilder.Build();
+            _serviceProvider = new AutofacServiceProvider(container);
+        }
 
-            // git add .
-
-            // git commit
-
-            // foreach
-
-                // upsert into MariaDB Rules table https://stackoverflow.com/questions/15271202/mysql-load-data-infile-with-on-duplicate-key-update
+        private static void DisposeServices()
+        {
+            switch (_serviceProvider)
+            {
+                case null:
+                    return;
+                case IDisposable disposableServiceProvider:
+                    disposableServiceProvider.Dispose();
+                    break;
+            }
         }
     }
 }
