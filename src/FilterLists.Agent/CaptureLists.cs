e2ecd7910dcaf308b8f2ba57f78a03f32944db09
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-06-26 14:07:14 -0500

    wire up mediatR

---
+++ src/FilterLists.Agent/CaptureLists.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace FilterLists.Agent
+{
+    public class CaptureLists
+    {
+        public class Command : IRequest
+        {
+        }
+
+        public class Handler : AsyncRequestHandler<Command>
+        {
+            protected override Task Handle(Command request, CancellationToken cancellationToken) => Task.CompletedTask;
+        }
+    }
+}
