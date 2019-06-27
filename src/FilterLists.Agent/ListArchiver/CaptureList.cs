4bfbfe1d5101432fc30c2860f6d6f8fa9b49433a
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-06-26 19:57:14 -0500

    add stub for CaptureList

---
+++ src/FilterLists.Agent/ListArchiver/CaptureList.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using FilterLists.Agent.Entities;
+using MediatR;
+
+namespace FilterLists.Agent.ListArchiver
+{
+    public static class CaptureList
+    {
+        public class Command : IRequest
+        {
+            public Command(ListInfo listInfo)
+            {
+                ListInfo = listInfo;
+            }
+
+            public ListInfo ListInfo { get; }
+        }
+
+        public class Handler : AsyncRequestHandler<Command>
+        {
+            private readonly HttpClient _httpClient;
+
+            public Handler(HttpClient httpClient)
+            {
+                _httpClient = httpClient;
+            }
+
+            protected override async Task Handle(Command request, CancellationToken cancellationToken)
+            {
+            }
+        }
+    }
+}
