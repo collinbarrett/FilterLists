50abb3ea054d33d97ede166a7a3c70df1505b11c
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-06-30 14:18:44 -0500

    refactor towards SRP

--- src/FilterLists.Agent/ListArchiver/CaptureLists.cs
+++ src/FilterLists.Agent/ListArchiver/CaptureLists.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
-using FilterLists.Agent.Entities;
 using FilterLists.Agent.Infrastructure;
-using LibGit2Sharp;
 using MediatR;
-using RestSharp;
 
 namespace FilterLists.Agent.ListArchiver
 {
@@ -18,7 +13,6 @@ namespace FilterLists.Agent.ListArchiver
 
         public class Handler : AsyncRequestHandler<Command>
         {
-            private const string GitRepoDirectory = @"archives";
             private readonly IFilterListsApiClient _apiClient;
             private readonly IMediator _mediator;
 
@@ -30,28 +24,9 @@ namespace FilterLists.Agent.ListArchiver
 
             protected override async Task Handle(Command request, CancellationToken cancellationToken)
             {
-                var lists = await GetListInfo();
+                var lists = await _apiClient.GetListInfo();
                 await _mediator.Send(new DownloadLists.Command(lists), cancellationToken);
-                CommitDownloadedLists();
-            }
-
-            private async Task<IEnumerable<ListInfo>> GetListInfo()
-            {
-                var listsRequest = new RestRequest("lists");
-                return await _apiClient.ExecuteAsync<IEnumerable<ListInfo>>(listsRequest);
-            }
-
-            private static void CommitDownloadedLists()
-            {
-                if (!Repository.IsValid(GitRepoDirectory))
-                    Repository.Init(GitRepoDirectory);
-                using (var repo = new Repository(GitRepoDirectory))
-                {
-                    Commands.Stage(repo, "*");
-                    var utcNow = DateTime.UtcNow;
-                    var signature = new Signature("FilterLists.Agent", "noreply@filterlists.com", utcNow);
-                    repo.Commit(utcNow.ToShortDateString() + " FilterLists archive by FilterLists.Agent", signature, signature);
-                }
+                await _mediator.Send(new CommitDownloadedLists.Command(), cancellationToken);
             }
         }
     }
---
+++ src/FilterLists.Agent/ListArchiver/CommitDownloadedLists.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using LibGit2Sharp;
+using MediatR;
+
+namespace FilterLists.Agent.ListArchiver
+{
+    public static class CommitDownloadedLists
+    {
+        public class Command : IRequest
+        {
+        }
+
+        public class Handler : RequestHandler<Command>
+        {
+            private const string GitRepoDirectory = @"archives";
+
+            protected override void Handle(Command request)
+            {
+                if (!Repository.IsValid(GitRepoDirectory))
+                    Repository.Init(GitRepoDirectory);
+                using (var repo = new Repository(GitRepoDirectory))
+                {
+                    Commands.Stage(repo, "*");
+                    var utcNow = DateTime.UtcNow;
+                    var signature = new Signature("FilterLists.Agent", "noreply@filterlists.com", utcNow);
+                    repo.Commit(utcNow.ToShortDateString() + " FilterLists archive by FilterLists.Agent", signature, signature);
+                }
+            }
+        }
+    }
+}
