480a2245c0f4e1f4753c92b9e29f8263851db7d6
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-07-01 14:42:50 -0500

    further support for url validation

--- src/FilterLists.Agent/Features/Urls/ValidateUrls.cs
+++ src/FilterLists.Agent/Features/Urls/ValidateUrls.cs
@@ -1,33 +1,84 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using FilterLists.Agent.Core.Entities;
-using FilterLists.Agent.Core.Interfaces;
+using System.Threading.Tasks.Dataflow;
+using FilterLists.Agent.Infrastructure.Clients;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace FilterLists.Agent.Features.Urls
 {
     public static class ValidateUrls
     {
-        public class Command : IRequest
+        public class Command : IRequest<IEnumerable<Uri>>
         {
-        }
-
-        public class Handler : AsyncRequestHandler<Command>
-        {
-            private readonly IMediator _mediator;
-            private readonly IUrlsRepository _repo;
-
-            public Handler(IMediator mediator, IUrlsRepository urlsRepository)
+            public Command(IEnumerable<Uri> urls)
             {
-                _mediator = mediator;
-                _repo = urlsRepository;
+                Urls = urls;
             }
 
-            protected override async Task Handle(Command request, CancellationToken cancellationToken)
+            public IEnumerable<Uri> Urls { get; }
+        }
+
+        public class Handler : IRequestHandler<Command, IEnumerable<Uri>>
+        {
+            private const int MaxDegreeOfParallelism = 5;
+            private readonly HttpClient _httpClient;
+            private readonly ILogger<Handler> _logger;
+
+            public Handler(AgentHttpClient agentHttpClient, ILogger<Handler> logger)
             {
-                var listUrls = await _repo.GetAllAsync<ListUrls>();
-                throw new NotImplementedException();
+                _httpClient = agentHttpClient.Client;
+                _logger = logger;
+            }
+
+            public async Task<IEnumerable<Uri>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var validator = BuildValidator(cancellationToken);
+                var brokenUrls = new List<Uri>();
+                foreach (var url in request.Urls)
+                    await validator.SendAsync(url, cancellationToken);
+                validator.Complete();
+                while (await validator.OutputAvailableAsync(cancellationToken))
+                {
+                    var (url, result) = await validator.ReceiveAsync(cancellationToken);
+                    if (!result)
+                        brokenUrls.Add(url);
+                }
+
+                await validator.Completion;
+                return brokenUrls;
+            }
+
+            private TransformBlock<Uri, (Uri, bool)> BuildValidator(CancellationToken cancellationToken)
+            {
+                return new TransformBlock<Uri, (Uri, bool)>(
+                    async u =>
+                    {
+                        var errorMessage = $"The following URL is broken: {u}.";
+                        try
+                        {
+                            var response = await _httpClient.GetAsync(u, cancellationToken);
+                            if (response.IsSuccessStatusCode)
+                                return (u, true);
+                            _logger.LogError($"{errorMessage} {response.StatusCode}");
+                            return (u, false);
+                        }
+                        catch (HttpRequestException ex)
+                        {
+                            _logger.LogError(ex, errorMessage);
+                            return (u, false);
+                        }
+                        catch (TaskCanceledException ex)
+                        {
+                            _logger.LogError(ex, errorMessage);
+                            return (u, false);
+                        }
+                    },
+                    new ExecutionDataflowBlockOptions {MaxDegreeOfParallelism = MaxDegreeOfParallelism}
+                );
             }
         }
     }
