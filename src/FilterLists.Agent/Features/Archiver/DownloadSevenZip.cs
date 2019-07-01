92c1297089754033d17ed73af47cf283aacd0076
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-06-30 20:38:18 -0500

    re-add initial support for 7zip

---
+++ src/FilterLists.Agent/Features/Archiver/DownloadSevenZip.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using FilterLists.Agent.Core.Entities;
+using FilterLists.Agent.Infrastructure.Clients;
+using MediatR;
+using SharpCompress.Archives;
+using SharpCompress.Archives.SevenZip;
+using SharpCompress.Common;
+
+namespace FilterLists.Agent.Features.Archiver
+{
+    public static class DownloadSevenZip
+    {
+        public class Command : IRequest
+        {
+            public Command(ListInfo listInfo)
+            {
+                ListInfo = listInfo;
+            }
+
+            public ListInfo ListInfo { get; }
+        }
+
+        public class Handler : AsyncRequestHandler<Command>
+        {
+            private const string RepoDirectory = @"archives";
+            private readonly HttpClient _httpClient;
+
+            public Handler(AgentHttpClient httpClient)
+            {
+                _httpClient = httpClient.Client;
+            }
+
+            protected override async Task Handle(Command request, CancellationToken cancellationToken)
+            {
+                var destinationDirectoryPath = Path.Combine(RepoDirectory, $"{request.ListInfo.Id}");
+                using (var response = await _httpClient.GetAsync(request.ListInfo.ViewUrl, cancellationToken))
+                {
+                    if (response.IsSuccessStatusCode)
+                        using (var input = await response.Content.ReadAsStreamAsync())
+                        using (var archive = SevenZipArchive.Open(input))
+                        {
+                            foreach (var entry in archive.Entries)
+                                if (!entry.IsDirectory)
+                                    entry.WriteToDirectory(destinationDirectoryPath,
+                                        new ExtractionOptions {ExtractFullPath = true, Overwrite = true});
+                        }
+                }
+            }
+        }
+    }
+}
