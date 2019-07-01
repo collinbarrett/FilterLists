b6da5df75591b827cd58c24d86177e8fcf7e1a3b
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-06-30 19:43:20 -0500

    wip refactor DownloadList flow

---
+++ src/FilterLists.Agent/Extensions/UriExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace FilterLists.Agent.Extensions
+{
+    public static class UriExtensions
+    {
+        public static string GetExtension(this Uri uri)
+        {
+            return Path.GetExtension(uri.AbsolutePath);
+        }
+    }
+}
