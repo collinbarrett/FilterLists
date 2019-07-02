aef399ef2d1592a70da1f3598b43d078b13f6c36
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-07-02 12:43:39 -0500

    fix GitHubClient config

--- src/FilterLists.Agent/Features/Urls/CreateOrUpdateGitHubIssue.cs
+++ src/FilterLists.Agent/Features/Urls/CreateOrUpdateGitHubIssue.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using FilterLists.Agent.Features.Urls.Models.ValidationResults;
 using MediatR;
+using Octokit;
 
 namespace FilterLists.Agent.Features.Urls
 {
@@ -21,6 +22,13 @@ namespace FilterLists.Agent.Features.Urls
 
         public class Handler : AsyncRequestHandler<Command>
         {
+            private readonly IGitHubClient _gitHubClient;
+
+            public Handler(IGitHubClient gitHubClient)
+            {
+                _gitHubClient = gitHubClient;
+            }
+
             protected override async Task Handle(Command request, CancellationToken cancellationToken)
             {
                 throw new NotImplementedException();
