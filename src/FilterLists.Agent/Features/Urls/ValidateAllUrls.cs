d1e6649df8ae2c1d9e663b8f1e533bdab9803c29
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-07-02 14:57:49 -0500

    add TODO to handle when to auto-close the issue

--- src/FilterLists.Agent/Features/Urls/ValidateAllUrls.cs
+++ src/FilterLists.Agent/Features/Urls/ValidateAllUrls.cs
@@ -31,31 +31,33 @@ namespace FilterLists.Agent.Features.Urls
                 var results = new List<DataFileUrlValidationResults>();
 
                 var licenseUrls = await _repo.GetAllAsync<LicenseUrls>();
-                var licenseUrlErrors = (await _mediator.Send(new ValidateUrls.Command(licenseUrls), cancellationToken));
+                var licenseUrlErrors = await _mediator.Send(new ValidateUrls.Command(licenseUrls), cancellationToken);
                 if (licenseUrlErrors.Any())
                     results.Add(new DataFileUrlValidationResults("License.json", licenseUrlErrors));
 
                 var listUrls = await _repo.GetAllAsync<ListUrls>();
-                var listUrlErrors = (await _mediator.Send(new ValidateUrls.Command(listUrls), cancellationToken));
+                var listUrlErrors = await _mediator.Send(new ValidateUrls.Command(listUrls), cancellationToken);
                 if (listUrlErrors.Any())
                     results.Add(new DataFileUrlValidationResults("FilterList.json", listUrlErrors));
 
                 var maintainerUrls = await _repo.GetAllAsync<MaintainerUrls>();
-                var maintainerUrlErrors = (await _mediator.Send(new ValidateUrls.Command(maintainerUrls), cancellationToken));
+                var maintainerUrlErrors = await _mediator.Send(new ValidateUrls.Command(maintainerUrls), cancellationToken);
                 if (maintainerUrlErrors.Any())
                     results.Add(new DataFileUrlValidationResults("Maintainer.json", maintainerUrlErrors));
 
                 var softwareUrls = await _repo.GetAllAsync<SoftwareUrls>();
-                var softwareUrlErrors = (await _mediator.Send(new ValidateUrls.Command(softwareUrls), cancellationToken));
+                var softwareUrlErrors = await _mediator.Send(new ValidateUrls.Command(softwareUrls), cancellationToken);
                 if (softwareUrlErrors.Any())
                     results.Add(new DataFileUrlValidationResults("Software.json", softwareUrlErrors));
 
                 var syntaxUrls = await _repo.GetAllAsync<SyntaxUrls>();
-                var syntaxUrlErrors = (await _mediator.Send(new ValidateUrls.Command(syntaxUrls), cancellationToken));
+                var syntaxUrlErrors = await _mediator.Send(new ValidateUrls.Command(syntaxUrls), cancellationToken);
                 if (syntaxUrlErrors.Any())
                     results.Add(new DataFileUrlValidationResults("Syntax.json", syntaxUrlErrors));
 
-                await _mediator.Send(new CreateOrUpdateGitHubIssue.Command(results), cancellationToken);
+                if(results.Any())
+                    await _mediator.Send(new CreateOrUpdateGitHubIssue.Command(results), cancellationToken);
+                //TODO: else, close issue
             }
         }
     }
