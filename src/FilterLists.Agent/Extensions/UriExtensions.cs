91a724598b4eda1fe4c8b8ca67ca134b95937de6
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-07-02 17:57:21 -0500

    validate is valid syntax of a URL

--- src/FilterLists.Agent/Extensions/UriExtensions.cs
+++ src/FilterLists.Agent/Extensions/UriExtensions.cs
@@ -9,5 +9,15 @@ namespace FilterLists.Agent.Extensions
         {
             return Path.GetExtension(uri.AbsolutePath);
         }
+
+        public static bool IsValidUrl(this Uri uri)
+        {
+            var uriString = uri.OriginalString;
+            if (!Uri.IsWellFormedUriString(uriString, UriKind.Absolute))
+                return false;
+            if (!Uri.TryCreate(uriString, UriKind.Absolute, out var tmp))
+                return false;
+            return tmp.Scheme == Uri.UriSchemeHttp || tmp.Scheme == Uri.UriSchemeHttps;
+        }
     }
 }
