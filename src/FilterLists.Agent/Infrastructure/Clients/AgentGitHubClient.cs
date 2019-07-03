2688d84b8c904ac2717ccf68efdc4002e2b5f562
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-07-02 20:17:18 -0500

    add logging of GitHub API errors

--- src/FilterLists.Agent/Infrastructure/Clients/AgentGitHubClient.cs
+++ src/FilterLists.Agent/Infrastructure/Clients/AgentGitHubClient.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using FilterLists.Agent.AppSettings;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Octokit;
 
@@ -19,10 +20,12 @@ namespace FilterLists.Agent.Infrastructure.Clients
     {
         private readonly GitHubClient _gitHubClient;
         private readonly GitHub _gitHubOptions;
+        private readonly ILogger<AgentGitHubClient> _logger;
 
-        public AgentGitHubClient(IOptions<GitHub> gitHubOptions)
+        public AgentGitHubClient(IOptions<GitHub> gitHubOptions, ILogger<AgentGitHubClient> logger)
         {
             _gitHubOptions = gitHubOptions.Value;
+            _logger = logger;
             _gitHubClient = new GitHubClient(new ProductHeaderValue(_gitHubOptions.ProductHeaderValue))
             {
                 Credentials = new Credentials(_gitHubOptions.PersonalAccessToken)
@@ -31,20 +34,44 @@ namespace FilterLists.Agent.Infrastructure.Clients
 
         public async Task<IReadOnlyList<Issue>> GetAllIssues(RepositoryIssueRequest repositoryIssueRequest)
         {
-            return await _gitHubClient.Issue.GetAllForRepository(_gitHubOptions.RepositoryOwner,
-                _gitHubOptions.Repository, repositoryIssueRequest);
+            try
+            {
+                return await _gitHubClient.Issue.GetAllForRepository(_gitHubOptions.RepositoryOwner,
+                    _gitHubOptions.Repository, repositoryIssueRequest);
+            }
+            catch (ApiException ex)
+            {
+                _logger.LogError(ex, "Failed getting all Issues from the GitHub API.");
+                return null;
+            }
         }
 
         public async Task<Issue> CreateIssue(NewIssue newIssue)
         {
-            return await _gitHubClient.Issue.Create(_gitHubOptions.RepositoryOwner, _gitHubOptions.Repository,
-                newIssue);
+            try
+            {
+                return await _gitHubClient.Issue.Create(_gitHubOptions.RepositoryOwner, _gitHubOptions.Repository,
+                    newIssue);
+            }
+            catch (ApiException ex)
+            {
+                _logger.LogError(ex, "Failed creating Issue with the GitHub API.");
+                return null;
+            }
         }
 
         public async Task<Issue> UpdateIssue(int issueNumber, IssueUpdate updateIssue)
         {
-            return await _gitHubClient.Issue.Update(_gitHubOptions.RepositoryOwner, _gitHubOptions.Repository,
-                issueNumber, updateIssue);
+            try
+            {
+                return await _gitHubClient.Issue.Update(_gitHubOptions.RepositoryOwner, _gitHubOptions.Repository,
+                    issueNumber, updateIssue);
+            }
+            catch (ApiException ex)
+            {
+                _logger.LogError(ex, "Failed updating Issue with the GitHub API.");
+                return null;
+            }
         }
     }
 }
