d6c6a6976008e6cd0c409dc6827db90e6b6a50c7
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-07-03 12:41:28 -0500

    add IsValidUrl() test

---
+++ tests/FilterLists.Agent.Tests/UriExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FilterLists.Agent.Extensions;
+using Xunit;
+
+namespace FilterLists.Agent.Tests
+{
+    public class UriExtensionsTests
+    {
+        [Fact]
+        public void IsValidUrl_WithValidUrl_ReturnsTrue()
+        {
+            const string uriString = "https://www.google.com/";
+            var uri = new Uri(uriString);
+            var sut = uri.IsValidUrl();
+            Assert.True(sut);
+        }
+    }
+}
