3a157603352929bdd9acef36e106fcf57aad75ce
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-07-03 10:08:21 -0500

    ignore roslyn warning

--- src/FilterLists.Agent/AppSettings/ApplicationInsights.cs
+++ src/FilterLists.Agent/AppSettings/ApplicationInsights.cs
@@ -1,6 +1,8 @@
 ﻿namespace FilterLists.Agent.AppSettings
 {
+#pragma warning disable CA1724
     public class ApplicationInsights
+#pragma warning restore CA1724
     {
         public string InstrumentationKey { get; set; }
     }
