e8b0cb84ea03777c1e476caed241afb889313658
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-07-06 15:53:22 -0500

    add 2x GetExtension() unit tests

--- tests/FilterLists.Agent.Tests/Extensions/UriExtensionsTests.cs
+++ tests/FilterLists.Agent.Tests/Extensions/UriExtensionsTests.cs
@@ -8,6 +8,22 @@ namespace FilterLists.Agent.Tests.Extensions
     public class UriExtensionsTests
     {
         [Fact]
+        public void GetExtension_UriWithoutExtension_ReturnsEmptyString()
+        {
+            var uri = new Uri("https://www.google.com/");
+            var sut = uri.GetExtension();
+            Assert.Equal("", sut);
+        }
+
+        [Fact]
+        public void GetExtension_UriWithExtension_ReturnsExtension()
+        {
+            var uri = new Uri("https://www.google.com/robots.txt");
+            var sut = uri.GetExtension();
+            Assert.Equal(".txt", sut);
+        }
+
+        [Fact]
         public void IsValidUrl_WithValidUrl_ReturnsTrue()
         {
             const string uriString = "https://www.google.com/";
