0e2ebcc722b3d181400f1c879517f81b4a961247
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-07-13 12:29:11 -0500

    rm deprecated tests

--- tests/FilterLists.Agent.Tests/Extensions/UriExtensionsTests.cs
+++ tests/FilterLists.Agent.Tests/Extensions/UriExtensionsTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using FilterLists.Agent.Extensions;
 using Xunit;
 
@@ -21,41 +20,6 @@ namespace FilterLists.Agent.Tests.Extensions
             var uri = new Uri("https://www.google.com/robots.txt");
             var sut = uri.GetExtension();
             Assert.Equal(".txt", sut);
-        }
-
-        [Fact]
-        public void IsValidUrl_WithValidUrl_ReturnsTrue()
-        {
-            const string uriString = "https://www.google.com/";
-            var uri = new Uri(uriString);
-            var sut = uri.IsValidUrl();
-            Assert.True(sut);
-        }
-
-        [Fact]
-        public void DistributeByHost_HasOverweightHost_DistributesEvenly()
-        {
-            var uris = new List<Uri>
-            {
-                new Uri("https://www.google.com/"),
-                new Uri("https://www.google.com/"),
-                new Uri("https://www.google.com/"),
-                new Uri("https://www.facebook.com/"),
-                new Uri("https://www.facebook.com/")
-            };
-
-            var sut = uris.DistributeByHost();
-
-            var expected = new List<Uri>
-            {
-                new Uri("https://www.google.com/"),
-                new Uri("https://www.facebook.com/"),
-                new Uri("https://www.google.com/"),
-                new Uri("https://www.facebook.com/"),
-                new Uri("https://www.google.com/")
-            };
-
-            Assert.Equal(expected, sut);
         }
     }
 }
