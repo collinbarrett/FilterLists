7413b72c63d0191a496b1feeec55f1d2fa119b2f
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-07-25 20:33:27 -0500

    simplify entity configuration registration

--- src/FilterLists.Data/FilterListsDbContext.cs
+++ src/FilterLists.Data/FilterListsDbContext.cs
@@ -1,7 +1,5 @@
 ﻿using FilterLists.Data.Entities;
 using FilterLists.Data.Entities.Junctions;
-using FilterLists.Data.EntityTypeConfigurations;
-using FilterLists.Data.EntityTypeConfigurations.Junctions;
 using Microsoft.EntityFrameworkCore;
 
 namespace FilterLists.Data
@@ -33,30 +31,7 @@ namespace FilterLists.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
-            ApplyConfigurations(modelBuilder);
-        }
-
-        private static void ApplyConfigurations(ModelBuilder modelBuilder)
-        {
-            modelBuilder.ApplyConfiguration(new FilterListTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new LanguageTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new LicenseTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new MaintainerTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new RuleTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new SnapshotTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new SoftwareTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new SyntaxTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new TagTypeConfiguration());
-
-            modelBuilder.ApplyConfiguration(new DependentTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new FilterListLanguageTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new FilterListMaintainerTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new FilterListTagTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new ForkTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new MergeTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new SnapshotRuleTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new SoftwareSyntaxTypeConfiguration());
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(FilterListsDbContext).Assembly);
         }
     }
 }
