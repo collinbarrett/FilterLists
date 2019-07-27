ab9eafb9432183942528141c22c01e4e5e6823f9
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-07-26 21:07:33 -0500

    add bootstrap types, rm unneeded baseUrl

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -4,9 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import * as RoutesModule from "./Routes";
 
 const App: React.FC = () => {
-  // const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href")!;
   return (
-    <BrowserRouter children={RoutesModule.Routes}  />
+    <BrowserRouter children={RoutesModule.Routes} />
   );
 }
 
