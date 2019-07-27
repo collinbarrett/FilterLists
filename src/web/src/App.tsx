5a6ff9962a8079c6f153dee667c6faba1f232455
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-07-26 21:46:31 -0500

    fix bootstrap

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import { BrowserRouter } from "react-router-dom";
 import * as RoutesModule from "./Routes";
--- src/web/src/Layout.tsx
+++ src/web/src/Layout.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import "bootstrap";
 
 interface IProps {
     children?: React.ReactNode;
