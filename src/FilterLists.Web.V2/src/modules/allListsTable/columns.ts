cfc90adc9d30a0532f1476c046b8e33fb540f6df
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-08-04 18:16:01 -0500

    use strongly-typed column dataIndex bindings

--- src/FilterLists.Web.V2/src/modules/allListsTable/columns.ts
+++ src/FilterLists.Web.V2/src/modules/allListsTable/columns.ts
@@ -1,9 +1,23 @@
 import { ColumnProps } from "antd/lib/table";
 import { List } from './List';
 
+const nameof = <T>(name: Extract<keyof T, string>): string => name;
+
 export const columns: ColumnProps<List>[] = [
     {
         title: 'Name',
-        dataIndex: 'name',
+        dataIndex: nameof<List>("name")
+    },
+    {
+        title: 'Description',
+        dataIndex: nameof<List>("description")
+    },
+    {
+        title: 'Languages',
+        dataIndex: nameof<List>("languageIds")
+    },
+    {
+        title: 'Tags',
+        dataIndex: nameof<List>("tagIds")
     }
 ];
