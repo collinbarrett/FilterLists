dc7325b7de12bf213e51e688dc55dc912d3770d9
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-08-05 14:58:22 -0500

    rm need for separate loading boolean in state

--- src/FilterLists.Web.V2/src/modules/allListsTable/AllListsTable.tsx
+++ src/FilterLists.Web.V2/src/modules/allListsTable/AllListsTable.tsx
@@ -4,32 +4,29 @@ import { nameof } from '../../utils';
 import { List } from './List';
 
 interface State {
-    data: List[];
-    loading: boolean;
+  data: List[];
 }
 
 export class AllListsTable extends React.Component<{}, State> {
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            data: [],
-            loading: false,
-        };
-    }
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
 
-    componentDidMount() {
-        this.setState({ loading: true });
-        fetch("/api/v1/lists")
-            .then(response => response.json())
-            .then(json => { this.setState({ data: json, loading: false }); })
-    }
+  componentDidMount() {
+    fetch("/api/v1/lists")
+      .then(response => response.json())
+      .then(json => { this.setState({ data: json }); })
+  }
 
-    render() {
-        return (
-            <Table<List> dataSource={this.state.data} rowKey={record => record.id.toString()} loading={this.state.loading} size="small" pagination={{ position: "top", size: "small" }} >
-                <Table.Column<List> title="Name" dataIndex={nameof<List>("name")} width={250} fixed="left" />
-                <Table.Column<List> title="Description" dataIndex={nameof<List>("description")} />
-            </Table>
-        );
-    }
+  render() {
+    return (
+      <Table<List> dataSource={this.state.data} rowKey={record => record.id.toString()} loading={this.state.data.length > 0 ? false : true} size="small" pagination={{ position: "top", size: "small" }} >
+        <Table.Column<List> title="Name" dataIndex={nameof<List>("name")} width={250} fixed="left" />
+        <Table.Column<List> title="Description" dataIndex={nameof<List>("description")} />
+      </Table>
+    );
+  }
 }
