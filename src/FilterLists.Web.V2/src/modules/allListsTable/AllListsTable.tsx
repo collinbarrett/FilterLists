5906af845482f71f2239341950b7de2e15e45775
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-08-06 10:37:57 -0500

    fix width of Subscribe column

--- src/FilterLists.Web.V2/src/modules/allListsTable/AllListsTable.tsx
+++ src/FilterLists.Web.V2/src/modules/allListsTable/AllListsTable.tsx
@@ -40,6 +40,7 @@ export class AllListsTable extends React.Component<{}, State> {
           dataIndex={nameof<List>("description")} />
         <Table.Column<List> title="Subscribe"
           dataIndex={nameof<List>("viewUrl")}
+          width={123}
           fixed="right"
           render={(text: string, record: List, index: number) => <SubscribeButton key={index} viewUrl={text} viewUrlMirrors={record.viewUrlMirrors} name={record.name} />}
         />
