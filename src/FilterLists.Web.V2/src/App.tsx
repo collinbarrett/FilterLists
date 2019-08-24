edae92b0a5cbb4f606a50e4a496d792226e63488
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-08-24 09:50:27 -0500

    add footer links

--- src/FilterLists.Web.V2/src/App.tsx
+++ src/FilterLists.Web.V2/src/App.tsx
@@ -9,21 +9,62 @@ const App: React.FC = () => {
   return (
     <Layout>
       <Header>
-        <img src="logo_filterlists.png" alt="FilterLists logo" height="44px" style={{ background: '#fff' }} />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          style={{ lineHeight: '64px' }}
-        />
+        <Logo />
+        <Menu theme="dark" mode="horizontal" style={{ lineHeight: '64px' }} />
       </Header>
       <Content>
         <div style={{ background: '#fff', paddingLeft: 12, paddingTop: 12, paddingRight: 12, minHeight: 280 }}>
           <AllListsTable />
         </div>
       </Content>
-      <Footer style={{ textAlign: 'center' }}>©{(new Date()).getFullYear()} Collin M. Barrett</Footer>
+      <Footer style={{ textAlign: 'center' }}>
+        <CopyrightAuthor /> | <Community /> | <GitHub /> | <Api /> | <Donate />
+      </Footer>
     </Layout>
   );
 }
+
+const Logo = () =>
+  <img src="logo_filterlists.png"
+    alt="FilterLists logo"
+    height="44px"
+    style={{ background: '#fff' }} />;
+
+const CopyrightAuthor = () =>
+  <span>
+    ©{new Date().getFullYear()}&nbsp;
+    <a href="https://collinmbarrett.com"
+      title="Collin M. Barrett's Homepage"
+      target="_blank" rel="noopener noreferrer">
+      Collin M. Barrett</a>
+  </span>;
+
+const Community = () =>
+  <a href="https://hub.filterlists.com"
+    title="FilterLists Discourse community forum"
+    target="_blank" rel="noopener noreferrer">
+    Community
+    </a>;
+
+const GitHub = () =>
+  <a href="https://github.com/collinbarrett/FilterLists"
+    title="FilterLists git repository on GitHub"
+    target="_blank" rel="noopener noreferrer">
+    GitHub
+    </a>;
+
+const Api = () =>
+  <a href="/api/v1/lists"
+    title="FilterLists API lists endpoint"
+    target="_blank" rel="noopener noreferrer">
+    API
+    </a>;
+
+const Donate = () =>
+  <a href="https://beerpay.io/collinbarrett/FilterLists"
+    title="Donate to FilterLists with Beerpay"
+    target="_blank" rel="noopener noreferrer">
+    Donate
+    </a>;
 
 export default App;
