043c2790d9661ba7b1e46a4a06b4cb5ac6c39514
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-08-31 16:40:01 -0500

    tidy syntax

--- src/FilterLists.Web.V2/src/hooks/useTablePageSizer.tsx
+++ src/FilterLists.Web.V2/src/hooks/useTablePageSizer.tsx
@@ -6,15 +6,13 @@ interface TablePageSize {
 }
 
 export const useTablePageSizer = () => {
-    const calculateSize = () => {
-        return {
-            pageSize: Math.floor((window.innerHeight - 211.5) / 56),
-            isNarrowWindow: window.innerWidth < 576 ? true : false
-        };
-    }
+    const calculateSize = () => ({
+        pageSize: Math.floor((window.innerHeight - 211.5) / 56),
+        isNarrowWindow: window.innerWidth < 576 ? true : false
+    });
     const [state, setState] = useState<TablePageSize>(calculateSize());
     useEffect(() => {
-        const updatePageSize = () => { setState(calculateSize()) }
+        const updatePageSize = () => setState(calculateSize())
         window.addEventListener('resize', updatePageSize);
         return () => window.removeEventListener('resize', updatePageSize);
     }, []);
