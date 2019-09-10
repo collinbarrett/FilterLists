a706c08da2e00fab9c72f00feeb5d57457825410
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-09-10 17:59:46 -0500

    cleanup maintainer actions
    ref #941

--- src/FilterLists.Web/src/components/Maintainers.tsx
+++ src/FilterLists.Web/src/components/Maintainers.tsx
@@ -20,29 +20,39 @@ interface MaintainerComponentProps {
   maintainer: Maintainer;
 }
 
-const MaintainerComponent = (props: MaintainerComponentProps) =>
-  <Card
-    title={props.maintainer.name}
-    actions={[
-      props.maintainer.homeUrl &&
+const MaintainerComponent = (props: MaintainerComponentProps) => {
+  let actions = [];
+  if (props.maintainer.homeUrl) {
+    actions.push(
       <a href={props.maintainer.homeUrl}
         title={`View ${props.maintainer.name}'s homepage.`}
         target="_blank"
         rel="noopener noreferrer">
         <Icon type="home" key="home" />
-      </a>,
-      props.maintainer.emailAddress &&
+      </a>
+    );
+  }
+  if (props.maintainer.emailAddress) {
+    actions.push(
       <a href={`mailto:${props.maintainer.emailAddress}`}
         title={`Email ${props.maintainer.name}.`}
         target="_blank"
         rel="noopener noreferrer">
         <Icon type="mail" key="mail" />
-      </a>,
-      props.maintainer.twitterHandle &&
+      </a>
+    );
+  }
+  if (props.maintainer.twitterHandle) {
+    actions.push(
       <a href={`https://twitter.com/${props.maintainer.twitterHandle}`}
         title={`${props.maintainer.twitterHandle} on Twitter`}
         target="_blank"
         rel="noopener noreferrer">
         <Icon type="twitter" key="twitter" />
       </a>
-    ]} />;
+    );
+  }
+  return <Card
+    title={props.maintainer.name}
+    actions={actions} />;
+}
