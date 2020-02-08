9db47f05e800e2b579a4dd13b56410002e2b2209
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-02-08 14:30:16 -0600

    specify MariaDB version to differentiate CHANGE/RENAME COLUMN syntax w/MySQL

--- server/src/FilterLists.Services/DependencyInjection/Extensions/ConfigureServicesCollection.cs
+++ server/src/FilterLists.Services/DependencyInjection/Extensions/ConfigureServicesCollection.cs
@@ -12,6 +12,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Pomelo.EntityFrameworkCore.MySql.Storage;
+using System;
 
 namespace FilterLists.Services.DependencyInjection.Extensions
 {
@@ -23,7 +26,8 @@ namespace FilterLists.Services.DependencyInjection.Extensions
             services.AddEntityFrameworkMySql()
                 .AddDbContextPool<FilterListsDbContext>(o =>
                     o.UseMySql(config.GetConnectionString("FilterListsConnection"),
-                        m => m.MigrationsAssembly("FilterLists.Api")));
+                        m => m.MigrationsAssembly("FilterLists.Api")
+                              .ServerVersion(new ServerVersion(new Version(10, 4, 12), ServerType.MariaDb))));
             services.TryAddScoped<FilterListService>();
             services.TryAddScoped<LanguageService>();
             services.TryAddScoped<LicenseService>();
