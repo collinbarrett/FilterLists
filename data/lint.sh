83e2e5cef859d6ea3fdd0aa1b88306612f9d976c
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-07-04 14:08:37 -0500

    refactor(data): lint FilterListTag.json

--- data/lint.sh
+++ data/lint.sh
@@ -11,3 +11,6 @@ mv FilterListLanguage.tmp FilterListLanguage.json
 
 jq -S ".|=sort_by(.maintainerId, .filterListId)" FilterListMaintainer.json > FilterListMaintainer.tmp
 mv FilterListMaintainer.tmp FilterListMaintainer.json
+
+jq -S ".|=sort_by(.filterListId, .tagId)" FilterListTag.json > FilterListTag.tmp
+mv FilterListTag.tmp FilterListTag.json
