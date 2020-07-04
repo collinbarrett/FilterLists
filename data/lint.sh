822272b8f24f613932d049468f7e24748cd36738
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-07-04 14:03:05 -0500

    refactor(data): lint Dependent.json

--- data/lint.sh
+++ data/lint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+jq -S ".|=sort_by(.dependentFilterListId, .dependencyFilterListId)" Dependent.json > Dependent.tmp
+mv Dependent.tmp Dependent.json
+
 jq -S ".|=sort_by(.id)" FilterList.json > FilterList.tmp
 mv FilterList.tmp FilterList.json
