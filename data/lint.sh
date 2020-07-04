7c209699461e60f469203f487744d0be91069da9
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-07-04 14:05:12 -0500

    refactor(data): lint FilterListLanguage.json

--- data/lint.sh
+++ data/lint.sh
@@ -5,3 +5,6 @@ mv Dependent.tmp Dependent.json
 
 jq -S ".|=sort_by(.id)" FilterList.json > FilterList.tmp
 mv FilterList.tmp FilterList.json
+
+jq -S ".|=sort_by(.filterListId, .languageId)" FilterListLanguage.json > FilterListLanguage.tmp
+mv FilterListLanguage.tmp FilterListLanguage.json
