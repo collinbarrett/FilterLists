c7593e60e4eeaafc596e9b4c5cbbd588a5476bd1
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-08-22 12:31:13 -0500

    feat(directory): ✨ init IQueryDirectory

---
+++ directory/src/FilterLists.Directory.Infrastructure/Persistence/Queries/DirectoryQueryDbContext.cs
@@ -0,0 +1,16 @@
+﻿using FilterLists.Directory.Infrastructure.Persistence.Queries.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilterLists.Directory.Infrastructure.Persistence.Queries
+{
+    internal class DirectoryQueryDbContext : DbContext
+    {
+        public DbSet<FilterList> FilterLists { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<License> Licenses { get; set; }
+        public DbSet<Maintainer> Maintainers { get; set; }
+        public DbSet<Software> Software { get; set; }
+        public DbSet<Syntax> Syntaxes { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
---
+++ directory/src/FilterLists.Directory.Infrastructure/Persistence/Queries/IQueryDirectory.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using FilterLists.Directory.Infrastructure.Persistence.Queries.Entities;
+
+namespace FilterLists.Directory.Infrastructure.Persistence.Queries
+{
+    public interface IQueryDirectory
+    {
+        IQueryable<FilterList> FilterLists { get; }
+        IQueryable<Language> Languages { get; }
+        IQueryable<License> Licenses { get; }
+        IQueryable<Maintainer> Maintainers { get; }
+        IQueryable<Software> Software { get; }
+        IQueryable<Syntax> Syntaxes { get; }
+        IQueryable<Tag> Tags { get; }
+    }
+}
