5216d9577ddc34f42b3045a67795868a911b1970
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-08-22 17:16:26 -0500

    feat(directory): ✨ configure many-to-many relationships

---
+++ directory/src/FilterLists.Directory.Infrastructure/Persistence/Queries/Mappings/ForkTypeConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using FilterLists.Directory.Infrastructure.Persistence.Queries.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FilterLists.Directory.Infrastructure.Persistence.Queries.Mappings
+{
+    internal class ForkTypeConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : Fork
+    {
+        public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            _ = builder ?? throw new ArgumentNullException(nameof(builder));
+
+            const string upstreamFilterListId = nameof(Fork.UpstreamFilterList) + "Id";
+            const string forkFilterListId = nameof(Fork.ForkFilterList) + "Id";
+            builder.Property<ushort>(upstreamFilterListId);
+            builder.Property<ushort>(forkFilterListId);
+            builder.HasKey(upstreamFilterListId, forkFilterListId);
+            builder.HasOne(f => f.UpstreamFilterList)
+                .WithMany(f => (IEnumerable<TEntity>)f.UpstreamFilterLists)
+                .HasForeignKey(upstreamFilterListId);
+            builder.HasOne(f => f.ForkFilterList)
+                .WithMany(f => (IEnumerable<TEntity>)f.ForkFilterLists)
+                .HasForeignKey(forkFilterListId);
+        }
+    }
+}
---
+++ directory/src/FilterLists.Directory.Infrastructure/Persistence/Queries/Mappings/MergeTypeConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using FilterLists.Directory.Infrastructure.Persistence.Queries.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FilterLists.Directory.Infrastructure.Persistence.Queries.Mappings
+{
+    internal class MergeTypeConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : Merge
+    {
+        public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            _ = builder ?? throw new ArgumentNullException(nameof(builder));
+
+            const string includedInFilterListId = nameof(Merge.IncludedInFilterList) + "Id";
+            const string includesFilterListId = nameof(Merge.IncludesFilterList) + "Id";
+            builder.Property<ushort>(includedInFilterListId);
+            builder.Property<ushort>(includesFilterListId);
+            builder.HasKey(includedInFilterListId, includesFilterListId);
+            builder.HasOne(m => m.IncludedInFilterList)
+                .WithMany(f => (IEnumerable<TEntity>)f.IncludedInFilterLists)
+                .HasForeignKey(includedInFilterListId);
+            builder.HasOne(m => m.IncludesFilterList)
+                .WithMany(f => (IEnumerable<TEntity>)f.IncludesFilterLists)
+                .HasForeignKey(includesFilterListId);
+        }
+    }
+}
