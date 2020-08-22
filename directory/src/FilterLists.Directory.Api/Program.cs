6f694cf8af200b291696bb972c0aa3c723c04832
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-08-22 06:00:49 -0500

    feat(directory): ✨ init R# and run code cleanup

--- directory/src/FilterLists.Directory.Api/Program.cs
+++ directory/src/FilterLists.Directory.Api/Program.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace FilterLists.Directory.Api
 {
@@ -16,11 +10,10 @@ namespace FilterLists.Directory.Api
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        }
     }
 }
