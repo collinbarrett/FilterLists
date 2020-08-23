ed3a370ed534bdf1a9055116149589bf0801bf24
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-08-23 09:59:50 -0500

    feat(directory): 📦 add MediatR

---
+++ directory/src/FilterLists.Directory.Application/ServicesRegistrationExtension.cs
@@ -0,0 +1,16 @@
+﻿using FilterLists.Directory.Infrastructure;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FilterLists.Directory.Application
+{
+    public static class ServicesRegistrationExtension
+    {
+        public static void AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMediatR(typeof(ServicesRegistrationExtension).Assembly);
+            services.AddInfrastructureServices(configuration);
+        }
+    }
+}
