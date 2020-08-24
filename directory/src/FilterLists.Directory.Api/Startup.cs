42d634d58907c0de35b065720a345def8de94633
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-08-23 20:10:13 -0500

    build(directory): 🔨 globally suppress CA1812

--- directory/src/FilterLists.Directory.Api/Startup.cs
+++ directory/src/FilterLists.Directory.Api/Startup.cs
@@ -7,9 +7,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace FilterLists.Directory.Api
 {
-#pragma warning disable CA1812
     internal class Startup
-#pragma warning restore CA1812
     {
         public Startup(IConfiguration configuration)
         {
