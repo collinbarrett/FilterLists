0e2578401f2bae33d4d2eecb89014150cc56d97d
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-08-25 08:04:13 -0500

    feat(directory): ✨ add GET maintainers endpoint

---
+++ services/directory/src/FilterLists.Directory.Api/Controllers/MaintainersController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FilterLists.Directory.Application.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilterLists.Directory.Api.Controllers
+{
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class MaintainersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public MaintainersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(CancellationToken cancellationToken)
+        {
+            return Ok(await _mediator.Send(new GetMaintainers.Query(), cancellationToken));
+        }
+    }
+}
