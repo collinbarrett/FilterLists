daff252696df74899dde8817e376a8690d8cfdab
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-08-25 05:29:19 -0500

    refactor(directory): 🎨 extract 'Queries' Application dir

--- services/directory/src/FilterLists.Directory.Api/Controllers/FilterListsController.cs
+++ services/directory/src/FilterLists.Directory.Api/Controllers/FilterListsController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using FilterLists.Directory.Application;
+using FilterLists.Directory.Application.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
