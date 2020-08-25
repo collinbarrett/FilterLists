faf9d321f311951796291532948c2e8d0df5839e
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-08-25 18:48:21 -0500

    feat(directory): ✨ finish fleshing out base GET endpoints

---
+++ services/directory/src/FilterLists.Directory.Api/Controllers/LanguagesController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FilterLists.Directory.Application.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilterLists.Directory.Api.Controllers
+{
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class LanguagesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LanguagesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(CancellationToken cancellationToken)
+        {
+            return Ok(await _mediator.Send(new GetLanguages.Query(), cancellationToken));
+        }
+    }
+}
