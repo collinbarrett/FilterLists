eb70b7e70e000883701929fd64388dab6b0bc5af
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-08-25 05:23:40 -0500

    refactor(persistence): ♻ extract 'SegmentViewUrls' and 'SegmentViewUrlMirrors'

---
+++ services/directory/src/FilterLists.Directory.Infrastructure/Persistence/Queries/Entities/SegmentViewUrl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FilterLists.Directory.Infrastructure.Persistence.Queries.Entities
+{
+    [SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Local")]
+    public class SegmentViewUrl
+    {
+        public FilterList FilterList { get; private set; } = null!;
+        public int Position { get; private set; }
+        public Uri Url { get; private set; } = null!;
+        public IReadOnlyCollection<SegmentViewUrlMirror> SegmentViewUrlMirrors { get; private set; } = new HashSet<SegmentViewUrlMirror>();
+    }
+
+    internal class ViewUrlPartialTypeConfiguration : IEntityTypeConfiguration<SegmentViewUrl>
+    {
+        public virtual void Configure(EntityTypeBuilder<SegmentViewUrl> builder)
+        {
+            _ = builder ?? throw new ArgumentNullException(nameof(builder));
+
+            builder.ToTable(nameof(SegmentViewUrl) + "s");
+
+            const string segmentViewUrlId = "Id";
+            builder.Property<int>(segmentViewUrlId);
+            builder.HasKey(segmentViewUrlId);
+
+            builder.HasIndex(nameof(FilterList) + "Id", nameof(SegmentViewUrl.Position))
+                .IsUnique();
+        }
+    }
+}
