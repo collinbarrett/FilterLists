21208280c776e6298932b4a3689fad497cff4ae4
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-08-25 06:36:30 -0500

    feat(directory): ✨🚧 stub ListsViewModel to match legacy

--- services/directory/src/FilterLists.Directory.Application/Queries/GetLists.cs
+++ services/directory/src/FilterLists.Directory.Application/Queries/GetLists.cs
@@ -10,11 +10,11 @@ namespace FilterLists.Directory.Application.Queries
 {
     public static class GetLists
     {
-        public class Query : IRequest<IEnumerable<FilterListViewModel>>
+        public class Query : IRequest<IEnumerable<ListViewModel>>
         {
         }
 
-        public class Handler : IRequestHandler<Query, IEnumerable<FilterListViewModel>>
+        public class Handler : IRequestHandler<Query, IEnumerable<ListViewModel>>
         {
             private readonly IQueryContext _context;
 
@@ -23,23 +23,55 @@ namespace FilterLists.Directory.Application.Queries
                 _context = context;
             }
 
-            public async Task<IEnumerable<FilterListViewModel>> Handle(Query request,
+            public async Task<IEnumerable<ListViewModel>> Handle(Query request,
                 CancellationToken cancellationToken)
             {
                 return await _context.FilterLists
-                    .Select(fl => new FilterListViewModel(fl.Name))
+                    .Select(fl => new ListViewModel
+                    {
+                        Id = default,
+                        Name = fl.Name,
+                        Description = fl.Description,
+                        LicenseId = default,
+                        SyntaxId = default,
+                        LanguageIds = default,
+                        TagIds = default,
+                        ViewUrl = default,
+                        ViewUrlMirrors = default,
+                        HomeUrl = default,
+                        PolicyUrl = default,
+                        SubmissionUrl = default,
+                        IssuesUrl = default,
+                        ForumUrl = default,
+                        ChatUrl = default,
+                        EmailAddress = default,
+                        DonateUrl = default,
+                        MaintainerIds = default
+                    })
                     .ToListAsync(cancellationToken);
             }
         }
 
-        public class FilterListViewModel
+        public class ListViewModel
         {
-            public FilterListViewModel(string name)
-            {
-                Name = name;
-            }
-
-            public string Name { get; }
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string? Description { get; set; }
+            public int? LicenseId { get; set; }
+            public int? SyntaxId { get; set; }
+            public IEnumerable<int> LanguageIds { get; set; }
+            public IEnumerable<int> TagIds { get; set; }
+            public string ViewUrl { get; set; }
+            public IEnumerable<string> ViewUrlMirrors { get; set; }
+            public string HomeUrl { get; set; }
+            public string PolicyUrl { get; set; }
+            public string SubmissionUrl { get; set; }
+            public string IssuesUrl { get; set; }
+            public string ForumUrl { get; set; }
+            public string ChatUrl { get; set; }
+            public string EmailAddress { get; set; }
+            public string DonateUrl { get; set; }
+            public IEnumerable<int> MaintainerIds { get; set; }
         }
     }
 }
