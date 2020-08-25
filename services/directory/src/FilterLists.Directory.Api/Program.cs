5821f269c9976f7a321e0bd79e492081d3dae831
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-08-25 07:08:34 -0500

    refactor(directory): ♻ async main

--- services/directory/src/FilterLists.Directory.Api/Program.cs
+++ services/directory/src/FilterLists.Directory.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -5,9 +6,9 @@ namespace FilterLists.Directory.Api
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            await CreateHostBuilder(args).Build().RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args)
