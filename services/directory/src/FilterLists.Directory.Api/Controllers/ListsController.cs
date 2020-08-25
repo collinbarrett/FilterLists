992ecaba2d8e75def820f76e4767c1c1f72bd60b
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-08-25 06:36:30 -0500

    feat(directory): ✨🚧 stub ListsViewModel to match legacy

---
+++ services/directory/src/FilterLists.Directory.Api/Controllers/ListsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FilterLists.Directory.Application.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilterLists.Directory.Api.Controllers
+{
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class ListsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ListsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(CancellationToken cancellationToken)
+        {
+            return Ok(await _mediator.Send(new GetLists.Query(), cancellationToken));
+        }
+    }
+}
