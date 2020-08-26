f914f83e22c04eff953cdf7abca8cb4283aef8fd
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-08-26 18:08:32 -0500

    feat(directory): ✨🚧 update data lint script to match schema changes

--- services/directory/data/lint.sh
+++ services/directory/data/lint.sh
@@ -7,11 +7,20 @@ mv Dependent.tmp Dependent.json
 jq -S ".|=sort_by(.id)" FilterList.json > FilterList.tmp
 mv FilterList.tmp FilterList.json
 
-jq -S ".|=sort_by(.filterListId, .languageId)" FilterListLanguage.json > FilterListLanguage.tmp
+jq -S ".|=sort_by(.filterListId, .iso6391)" FilterListLanguage.json > FilterListLanguage.tmp
 mv FilterListLanguage.tmp FilterListLanguage.json
 
 jq -S ".|=sort_by(.maintainerId, .filterListId)" FilterListMaintainer.json > FilterListMaintainer.tmp
 mv FilterListMaintainer.tmp FilterListMaintainer.json
+
+jq -S ".|=sort_by(.filterListId, .id)" FilterListSegmentViewUrl.json > FilterListSegmentViewUrl.tmp
+mv FilterListSegmentViewUrl.tmp FilterListSegmentViewUrl.json
+
+jq -S ".|=sort_by(.segmentViewUrlId, .id)" FilterListSegmentViewUrlMirror.json > FilterListSegmentViewUrlMirror.tmp
+mv FilterListSegmentViewUrlMirror.tmp FilterListSegmentViewUrlMirror.json
+
+jq -S ".|=sort_by(.filterListId, .syntaxId)" FilterListSyntax.json > FilterListSyntax.tmp
+mv FilterListSyntax.tmp FilterListSyntax.json
 
 jq -S ".|=sort_by(.filterListId, .tagId)" FilterListTag.json > FilterListTag.tmp
 mv FilterListTag.tmp FilterListTag.json
@@ -19,7 +28,7 @@ mv FilterListTag.tmp FilterListTag.json
 jq -S ".|=sort_by(.upstreamFilterListId, .forkFilterListId)" Fork.json > Fork.tmp
 mv Fork.tmp Fork.json
 
-jq -S ".|=sort_by(.id)" Language.json > Language.tmp
+jq -S ".|=sort_by(.iso6391)" Language.json > Language.tmp
 mv Language.tmp Language.json
 
 jq -S ".|=sort_by(.id)" License.json > License.tmp
@@ -28,7 +37,7 @@ mv License.tmp License.json
 jq -S ".|=sort_by(.id)" Maintainer.json > Maintainer.tmp
 mv Maintainer.tmp Maintainer.json
 
-jq -S ".|=sort_by(.mergeFilterListId, .upstreamFilterListId)" Merge.json > Merge.tmp
+jq -S ".|=sort_by(.includedInFilterListId, .includesFilterListId)" Merge.json > Merge.tmp
 mv Merge.tmp Merge.json
 
 jq -S ".|=sort_by(.id)" Software.json > Software.tmp
