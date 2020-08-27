f755ee89ebc706f8ac3a3bd1e4a999bac80b5bd4
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-08-27 17:25:55 -0500

    fix(directory): 🐛 ren License.Url in seed data

--- services/directory/src/FilterLists.Directory.Infrastructure/Persistence/Queries/Entities/License.cs
+++ services/directory/src/FilterLists.Directory.Infrastructure/Persistence/Queries/Entities/License.cs
@@ -9,9 +9,14 @@ namespace FilterLists.Directory.Infrastructure.Persistence.Queries.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
+
+        // TODO: add to seed data
         public string? GitHubKey { get; set; }
+
         public Uri? Url { get; set; }
         public IReadOnlyCollection<FilterList> FilterLists { get; private set; } = new HashSet<FilterList>();
+
+        // TODO: rm or capture permissiveX from seed data
     }
 
     internal class LicenseTypeConfiguration : IEntityTypeConfiguration<License>
