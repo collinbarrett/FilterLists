a90764cf38b6ee80c5d0bcb24450ce687afa557c
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-08-29 14:57:38 -0500

    refactor(web): ♻ rm List DescriptionSourceUrl from ui

--- web/src/components/Description.tsx
+++ web/src/components/Description.tsx
@@ -2,14 +2,6 @@ import React from "react";
 
 interface Props {
   description: string;
-  descriptionSourceUrl: string;
 }
 
-export const Description = (props: Props) =>
-  props.descriptionSourceUrl ? (
-    <blockquote cite={props.descriptionSourceUrl}>
-      {props.description}
-    </blockquote>
-  ) : (
-    <p>{props.description}</p>
-  );
+export const Description = (props: Props) => <p>{props.description}</p>;
