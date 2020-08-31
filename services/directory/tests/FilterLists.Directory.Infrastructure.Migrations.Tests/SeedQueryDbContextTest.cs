307840b8d6921ecc7cbd1ba4c1f19a691d3cca28
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-08-30 19:58:34 -0500

    feat: ✨ add migrations test to pipeline

---
+++ services/directory/tests/FilterLists.Directory.Infrastructure.Migrations.Tests/SeedQueryDbContextTest.cs
@@ -0,0 +1,31 @@
+using System.Threading;
+using System.Threading.Tasks;
+using FilterLists.Directory.Infrastructure.Persistence.Queries.Context;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace FilterLists.Directory.Infrastructure.Migrations.Tests
+{
+    public class SeedQueryDbContextTest
+    {
+        [Fact]
+        public async Task Migrate_DoesNotThrowException()
+        {
+            var exception = await Record.ExceptionAsync(async () =>
+            {
+                // allow time for db to init
+                Thread.Sleep(20000);
+
+                const string connString = "Server=directory.db;Database=filterlists;Uid=filterlists;Pwd=filterlists;";
+                var options = new DbContextOptionsBuilder<QueryDbContext>()
+                    .UseNpgsql(connString, m => m.MigrationsAssembly("FilterLists.Directory.Infrastructure.Migrations"))
+                    .EnableSensitiveDataLogging()
+                    .Options;
+
+                await using var context = new QueryDbContext(options);
+                await context.Database.MigrateAsync();
+            });
+            Assert.Null(exception);
+        }
+    }
+}
