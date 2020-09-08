13db19857ecf7078858d8a1dce7d93618011dd07
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-09-08 07:03:59 -0500

    feat(directory): ✨ add more Swagger config

---
+++ services/directory/src/FilterLists.Directory.Api/SwaggerExtensions.cs
@@ -0,0 +1,44 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace FilterLists.Directory.Api
+{
+    internal static class SwaggerExtensions
+    {
+        public static void AddSwaggerGen(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "FilterLists Directory API",
+                    Description =
+                        "FilterLists is the independent, comprehensive directory of filter and host lists for advertisements, trackers, malware, and annoyances.",
+                    Version = "v1",
+                    //TermsOfService = "",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Collin M. Barrett",
+                        Url = new Uri("https://collinmbarrett.com/contact/")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "MIT License",
+                        Url = new Uri("https://github.com/collinbarrett/FilterLists/blob/master/LICENSE")
+                    }
+                }));
+        }
+
+        public static void UseSwagger(this IApplicationBuilder app)
+        {
+            app.UseSwagger(o => o.RouteTemplate = "{documentName}/swagger.json");
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("v1/swagger.json", "FilterLists Directory API V1");
+                c.DocumentTitle = "FilterLists Directory API";
+                c.RoutePrefix = string.Empty;
+            });
+        }
+    }
+}
