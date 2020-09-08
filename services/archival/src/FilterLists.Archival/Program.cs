257f8e3156af1f50939ab7b9453341dd8c5e59cf
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-09-08 17:39:02 -0500

    feat(archival): ✨ stub new archival proj

---
+++ services/archival/src/FilterLists.Archival/Program.cs
@@ -0,0 +1,21 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace FilterLists.Archival
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            await host.RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+        }
+    }
+}
---
+++ services/archival/src/FilterLists.Archival/Startup.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FilterLists.Archival
+{
+    public class Startup
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+        }
+    }
+}
