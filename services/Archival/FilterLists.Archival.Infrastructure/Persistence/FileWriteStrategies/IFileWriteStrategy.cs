8166dec254dd4df0f6ac46bc67727f0a93001c23
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-09-16 08:28:48 -0500

    feat(archival): ✨ implement strategy pattern for file writing

---
+++ services/Archival/FilterLists.Archival.Infrastructure/Persistence/FileWriteStrategies/IFileWriteStrategy.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace FilterLists.Archival.Infrastructure.Persistence.FileWriteStrategies
+{
+    internal interface IFileWriteStrategy
+    {
+        Task WriteAsync(IFileToArchive file, CancellationToken cancellationToken = default);
+    }
+}
---
+++ services/Archival/FilterLists.Archival.Infrastructure/Persistence/IFileToArchive.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace FilterLists.Archival.Infrastructure.Persistence
+{
+    public interface IFileToArchive
+    {
+        FileInfo Target { get; }
+        IEnumerable<Stream> Contents { get; }
+    }
+}
