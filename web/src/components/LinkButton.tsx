01bb175c15cfd47fffac882f4ff6117c0eae9aad
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-09-20 17:58:12 -0500

    fix(web): 🐛💄 reset/tighten up some css

--- web/src/components/LinkButton.tsx
+++ web/src/components/LinkButton.tsx
@@ -17,7 +17,6 @@ export const LinkButton = (props: Props) =>
       title={props.title}
       type={props.type || "default"}
       block
-      style={{ borderLeftColor: "rgb(217, 217, 217)" }} //HACK: override buggy style in antd
       icon={props.icon}
       target="_blank"
       rel="noopener noreferrer"
