5b4bb6f8bc1c3d3677a6e340a23db43e8b99e35c
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2021-11-02 14:48:44 -0500

    refactor(dir): ♻ limit scope of contract props

--- services/Directory/FilterLists.Directory.Api.Contracts/Models/ListDetailsVm.cs
+++ services/Directory/FilterLists.Directory.Api.Contracts/Models/ListDetailsVm.cs
@@ -5,36 +5,36 @@ namespace FilterLists.Directory.Api.Contracts.Models
 {
     public class ListDetailsVm
     {
-        public int Id { get; set; }
-        public string Name { get; set; } = null!;
-        public string? Description { get; set; }
-        public int? LicenseId { get; set; }
-        public IEnumerable<int>? SyntaxIds { get; set; }
-        public IEnumerable<string>? Iso6391s { get; set; }
-        public IEnumerable<int>? TagIds { get; set; }
-        public IEnumerable<ViewUrlVm>? ViewUrls { get; set; }
-        public Uri? HomeUrl { get; set; }
-        public Uri? OnionUrl { get; set; }
-        public Uri? PolicyUrl { get; set; }
-        public Uri? SubmissionUrl { get; set; }
-        public Uri? IssuesUrl { get; set; }
-        public Uri? ForumUrl { get; set; }
-        public Uri? ChatUrl { get; set; }
-        public string? EmailAddress { get; set; }
-        public Uri? DonateUrl { get; set; }
-        public IEnumerable<int>? MaintainerIds { get; set; }
-        public IEnumerable<int>? UpstreamFilterListIds { get; set; }
-        public IEnumerable<int>? ForkFilterListIds { get; set; }
-        public IEnumerable<int>? IncludedInFilterListIds { get; set; }
-        public IEnumerable<int>? IncludesFilterListIds { get; set; }
-        public IEnumerable<int>? DependencyFilterListIds { get; set; }
-        public IEnumerable<int>? DependentFilterListIds { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; } = null!;
+        public string? Description { get; private set; }
+        public int? LicenseId { get; private set; }
+        public IEnumerable<int>? SyntaxIds { get; private set; }
+        public IEnumerable<string>? Iso6391s { get; private set; }
+        public IEnumerable<int>? TagIds { get; private set; }
+        public IEnumerable<ViewUrlVm>? ViewUrls { get; private set; }
+        public Uri? HomeUrl { get; private set; }
+        public Uri? OnionUrl { get; private set; }
+        public Uri? PolicyUrl { get; private set; }
+        public Uri? SubmissionUrl { get; private set; }
+        public Uri? IssuesUrl { get; private set; }
+        public Uri? ForumUrl { get; private set; }
+        public Uri? ChatUrl { get; private set; }
+        public string? EmailAddress { get; private set; }
+        public Uri? DonateUrl { get; private set; }
+        public IEnumerable<int>? MaintainerIds { get; private set; }
+        public IEnumerable<int>? UpstreamFilterListIds { get; private set; }
+        public IEnumerable<int>? ForkFilterListIds { get; private set; }
+        public IEnumerable<int>? IncludedInFilterListIds { get; private set; }
+        public IEnumerable<int>? IncludesFilterListIds { get; private set; }
+        public IEnumerable<int>? DependencyFilterListIds { get; private set; }
+        public IEnumerable<int>? DependentFilterListIds { get; private set; }
 
         public class ViewUrlVm
         {
-            public short SegmentNumber { get; set; }
-            public short Primariness { get; set; }
-            public Uri Url { get; set; } = null!;
+            public short SegmentNumber { get; private set; }
+            public short Primariness { get; private set; }
+            public Uri Url { get; private set; } = null!;
         }
     }
 }
--- services/Directory/FilterLists.Directory.Api.Contracts/Models/ListVm.cs
+++ services/Directory/FilterLists.Directory.Api.Contracts/Models/ListVm.cs
@@ -4,13 +4,13 @@ namespace FilterLists.Directory.Api.Contracts.Models
 {
     public class ListVm
     {
-        public int Id { get; set; }
-        public string Name { get; set; } = null!;
-        public string? Description { get; set; }
-        public int? LicenseId { get; set; }
-        public IEnumerable<int>? SyntaxIds { get; set; }
-        public IEnumerable<string>? Iso6391s { get; set; }
-        public IEnumerable<int>? TagIds { get; set; }
-        public IEnumerable<int>? MaintainerIds { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; } = null!;
+        public string? Description { get; private set; }
+        public int? LicenseId { get; private set; }
+        public IEnumerable<int>? SyntaxIds { get; private set; }
+        public IEnumerable<string>? Iso6391s { get; private set; }
+        public IEnumerable<int>? TagIds { get; private set; }
+        public IEnumerable<int>? MaintainerIds { get; private set; }
     }
 }
