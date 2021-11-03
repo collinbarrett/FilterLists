005334552ceceb0d78c4a1e5bb606e8f4dda8976
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2021-11-03 04:24:14 -0500

    feat(dir): ✨ scaffold Domain proj and ICommandContext

---
+++ services/Directory/FilterLists.Directory.Domain/Aggregates/FilterList.cs
@@ -0,0 +1,18 @@
+﻿namespace FilterLists.Directory.Domain.Aggregates;
+
+public class FilterList
+{
+    public int Id { get; private set; }
+    public string Name { get; private set; } = null!;
+    public string? Description { get; private set; }
+    public int? LicenseId { get; private set; }
+    public Uri? HomeUrl { get; private set; }
+    public Uri? OnionUrl { get; private set; }
+    public Uri? PolicyUrl { get; private set; }
+    public Uri? SubmissionUrl { get; private set; }
+    public Uri? IssuesUrl { get; private set; }
+    public Uri? ForumUrl { get; private set; }
+    public Uri? ChatUrl { get; private set; }
+    public string? EmailAddress { get; private set; }
+    public Uri? DonateUrl { get; private set; }
+}
---
+++ services/Directory/FilterLists.Directory.Infrastructure/Persistence/Commands/Context/CommandContext.cs
@@ -0,0 +1,16 @@
+﻿namespace FilterLists.Directory.Infrastructure.Persistence.Commands.Context;
+
+internal class CommandContext : ICommandContext, IAsyncDisposable
+{
+    private readonly CommandDbContext _dbContext;
+
+    public CommandContext(CommandDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await _dbContext.DisposeAsync();
+    }
+}
