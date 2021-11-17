4ddeb60d071b8983084c3ec1124fa402315cc714
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2021-11-17 16:37:37 -0600

    refactor(dir): ♻ decouple FilterListViewUrl app model from domain model

--- services/Directory/FilterLists.Directory.Domain/Aggregates/FilterLists/FilterListViewUrl.cs
+++ services/Directory/FilterLists.Directory.Domain/Aggregates/FilterLists/FilterListViewUrl.cs
@@ -7,4 +7,9 @@ public sealed class FilterListViewUrl
     public short SegmentNumber { get; private init; }
     public short Primariness { get; private init; }
     public Uri Url { get; private init; } = null!;
+
+    internal static FilterListViewUrl Create(short segmentNumber, short primariness, Uri url)
+    {
+        return new FilterListViewUrl { SegmentNumber = segmentNumber, Primariness = primariness, Url = url };
+    }
 }
