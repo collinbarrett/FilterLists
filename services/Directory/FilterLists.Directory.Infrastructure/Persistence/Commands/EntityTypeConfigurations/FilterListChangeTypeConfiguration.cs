891678f8d4f512e5bbb9ab706453e7dcee930df1
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2021-11-18 15:10:51 -0600

    fix(dr): 🐛 fk name

--- services/Directory/FilterLists.Directory.Infrastructure/Persistence/Commands/EntityTypeConfigurations/FilterListChangeTypeConfiguration.cs
+++ services/Directory/FilterLists.Directory.Infrastructure/Persistence/Commands/EntityTypeConfigurations/FilterListChangeTypeConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using FilterLists.Directory.Domain.Aggregates.Changes;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Change = FilterLists.Directory.Infrastructure.Persistence.Queries.Entities.Change;
 
 namespace FilterLists.Directory.Infrastructure.Persistence.Commands.EntityTypeConfigurations;
 
@@ -8,6 +9,11 @@ internal class FilterListChangeTypeConfiguration : IEntityTypeConfiguration<Filt
 {
     public virtual void Configure(EntityTypeBuilder<FilterListChange> builder)
     {
+        builder.Property<int>(nameof(Change.FilterListId));
+        builder.HasOne(c => c.Current)
+            .WithMany(f => f.Changes)
+            .HasForeignKey(nameof(Change.FilterListId));
+
         // TODO: share by configuring IChange<TAggregate>
         // TODO: serialize/deserialize json via value converter
         builder.Ignore(c => c.Before);
