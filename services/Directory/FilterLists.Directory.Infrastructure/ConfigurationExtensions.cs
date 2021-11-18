255e9bb622e497432a26d91f87d24d9d27c23d63
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2021-11-18 03:57:09 -0600

    refactor(dir): 🔥📦 rm EFCore.NamingConventions (too much hassle / buggy)

--- services/Directory/FilterLists.Directory.Infrastructure/ConfigurationExtensions.cs
+++ services/Directory/FilterLists.Directory.Infrastructure/ConfigurationExtensions.cs
@@ -25,7 +25,6 @@ public static class ConfigurationExtensions
         {
             o.UseNpgsql(configuration.GetConnectionString("DirectoryConnection"),
                     po => po.MigrationsAssembly("FilterLists.Directory.Infrastructure.Migrations"))
-                .UseSnakeCaseNamingConvention()
 #if DEBUG
                 .LogTo(Console.WriteLine, LogLevel.Information);
             o.EnableSensitiveDataLogging();
