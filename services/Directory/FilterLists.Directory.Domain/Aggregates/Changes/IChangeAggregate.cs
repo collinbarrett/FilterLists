a626dada3e45aad8b5b1f2e5e9d81f4881db03a6
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2021-11-20 15:10:33 -0600

    refactor(dir): ♻ extract FilterListRecord

---
+++ services/Directory/FilterLists.Directory.Domain/Aggregates/Changes/IChangeAggregate.cs
@@ -0,0 +1,7 @@
+﻿namespace FilterLists.Directory.Domain.Aggregates.Changes;
+
+public interface IChangeAggregate<out TAggregateRecord>
+{
+    TAggregateRecord? Before { get; }
+    TAggregateRecord? After { get; }
+}
