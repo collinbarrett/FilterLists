8175e7988d6f956dc9d305512b56702d0fe2edd2
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2021-11-20 12:20:11 -0600

    refactor(dir): ♻ extract AggregateRootCore to resolve circular refs in Change model

--- services/Directory/FilterLists.Directory.Domain/Aggregates/Changes/IChangeAggregateRoot.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FilterLists.Directory.Domain.Aggregates.Changes;
-
-public interface IChange<out TAggregate> where TAggregate : AggregateRoot
-{
-    TAggregate? Current { get; }
-    TAggregate? Before { get; }
-    TAggregate? After { get; }
-}
--- services/Directory/FilterLists.Directory.Infrastructure/Persistence/Commands/EntityTypeConfigurations/FilterListTypeConfiguration.cs
+++ services/Directory/FilterLists.Directory.Infrastructure/Persistence/Commands/EntityTypeConfigurations/FilterListTypeConfiguration.cs
@@ -1,6 +1,7 @@
-﻿using FilterLists.Directory.Domain.Aggregates.FilterLists;
+﻿using FilterLists.Directory.Infrastructure.Persistence.Queries.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using FilterList = FilterLists.Directory.Domain.Aggregates.FilterLists.FilterList;
 
 namespace FilterLists.Directory.Infrastructure.Persistence.Commands.EntityTypeConfigurations;
 
@@ -9,6 +10,9 @@ internal class FilterListTypeConfiguration : IEntityTypeConfiguration<FilterList
     public virtual void Configure(EntityTypeBuilder<FilterList> builder)
     {
         builder.Property<int>(nameof(Queries.Entities.FilterList.Id));
+        builder.HasMany(c => c.Changes)
+            .WithOne()
+            .HasForeignKey(nameof(Change.FilterListId));
         builder.Navigation(f => f.Changes)
             .AutoInclude();
         builder.Navigation(f => f.ViewUrls)
