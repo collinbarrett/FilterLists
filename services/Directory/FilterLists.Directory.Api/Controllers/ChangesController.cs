f7f3e00639881d736ba8399da0aed3818a45c25a
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2021-11-20 15:08:56 -0600

    feat(dir): ✨ scaffold GET changes endpoint

---
+++ services/Directory/FilterLists.Directory.Api/Controllers/ChangesController.cs
@@ -0,0 +1,29 @@
+﻿using FilterLists.Directory.Application.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace FilterLists.Directory.Api.Controllers;
+
+public class ChangesController : BaseController
+{
+    private readonly IMediator _mediator;
+
+    public ChangesController(IMemoryCache cache, IMediator mediator) : base(cache)
+    {
+        _mediator = mediator;
+    }
+
+    /// <summary>
+    ///     Gets the changes.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The changes.</returns>
+    [HttpGet]
+    //[ProducesResponseType(typeof(IEnumerable<GetChanges.ChangesVm>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> Get(CancellationToken cancellationToken)
+    {
+        return Ok(await _mediator.Send(new GetChanges.Query(), cancellationToken));
+        //return CacheGetOrCreateAsync(() => _mediator.Send(new GetChanges.Query(), cancellationToken));
+    }
+}
