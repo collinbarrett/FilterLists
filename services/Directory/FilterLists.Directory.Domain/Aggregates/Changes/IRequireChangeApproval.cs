522c204ecd5c5fdfaab68769eb50aeff6230a5ee
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2021-11-24 07:23:16 -0600

    refactor(dir): ♻ prefer IEnumerable over IReadOnlyCollection for immutability (force copy-by-value to mutate element's props)

--- services/Directory/FilterLists.Directory.Domain/Aggregates/Changes/IRequireChangeApproval.cs
+++ services/Directory/FilterLists.Directory.Domain/Aggregates/Changes/IRequireChangeApproval.cs
@@ -2,9 +2,9 @@
 
 public interface IRequireChangeApproval<out TChange> where TChange : Change
 {
-    public IReadOnlyCollection<TChange> Changes { get; }
-    public IReadOnlyCollection<TChange> PendingChanges => (IReadOnlyCollection<TChange>)Changes.Where(c => c.ApprovedAt == null && c.RejectedAt == null);
-    public IReadOnlyCollection<TChange> ApprovedChanges => (IReadOnlyCollection<TChange>)Changes.Where(c => c.ApprovedAt != null);
-    public IReadOnlyCollection<TChange> RejectedChanges => (IReadOnlyCollection<TChange>)Changes.Where(c => c.RejectedAt != null);
+    public IEnumerable<TChange> Changes { get; }
+    public IEnumerable<TChange> PendingChanges => Changes.Where(c => c.ApprovedAt == null && c.RejectedAt == null);
+    public IEnumerable<TChange> ApprovedChanges => Changes.Where(c => c.ApprovedAt != null);
+    public IEnumerable<TChange> RejectedChanges => Changes.Where(c => c.RejectedAt != null);
     public bool IsApproved { get; }
 }
