fec59aaf6d542ecd2d81471717e58d56b1f2c548
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2021-11-26 15:56:39 -0600

    feat(dir): ✨ scaffold GET Change by Id endpoint
    ref https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/1607

--- services/Directory/FilterLists.Directory.Api/Controllers/ChangesController.cs
+++ services/Directory/FilterLists.Directory.Api/Controllers/ChangesController.cs
@@ -20,9 +20,23 @@ public class ChangesController : BaseController
     /// <param name="cancellationToken">The cancellation token.</param>
     /// <returns>The changes.</returns>
     [HttpGet]
-    [ProducesResponseType(typeof(List<GetChanges.ChangesVm>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(List<GetChanges.ChangeVm>), StatusCodes.Status200OK)]
     public Task<IActionResult> Get(CancellationToken cancellationToken)
     {
         return CacheGetOrCreateAsync(() => _mediator.Send(new GetChanges.Query(), cancellationToken));
+    }
+
+    /// <summary>
+    ///     Gets the change.
+    /// </summary>
+    /// <param name="id">The identifier of the change.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The change.</returns>
+    [HttpGet("{id:long}")]
+    [ProducesResponseType(typeof(GetChange.ChangeVm), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public Task<IActionResult> GetDetails(long id, CancellationToken cancellationToken)
+    {
+        return CacheGetOrCreateAsync(() => _mediator.Send(new GetChange.Query(id), cancellationToken), id);
     }
 }
