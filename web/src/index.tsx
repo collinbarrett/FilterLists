719ec80aba6b8d0843fbc1e308a5f3c2e9c81397
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2022-04-21 09:15:47 -0500

    fix(ui): 🐛📦 rollback pkg upgrades to pre-react@18.x to try fix #2910

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,16 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import ReactDOM from "react-dom";
 import { App } from "./App";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
