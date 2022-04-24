7f3bc97a39735dd31e4af1390c68ec809c95e2b4
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2022-04-24 09:46:36 -0500

    feat(api): ✨🚧 seed languages

--- api/FilterLists.Api/Api/Lists/PostSeed/PostSeedLists.cs
+++ api/FilterLists.Api/Api/Lists/PostSeed/PostSeedLists.cs
@@ -21,10 +21,7 @@ namespace FilterLists.Api.Api.Lists.PostSeed;
 public class PostSeedLists
 {
     private const string SeedDataDir = "../../../../../../FilterLists/services/Directory/data/";
-
-    // https://stackoverflow.com/a/37749583/2343739
     private static readonly Regex DisallowedCharsInTableKeys = new(@"[\\\\#%+/?\u0000-\u001F\u007F-\u009F]");
-
     private readonly TableServiceClient _tableServiceClient;
 
     public PostSeedLists(TableServiceClient tableServiceClient)
@@ -56,36 +53,49 @@ public class PostSeedLists
         var syntaxes = await GetSeedEntities<Syntax>();
         var tags = await GetSeedEntities<Tag>();
 
-        var entities = filterLists.Select(l =>
+        var entities = filterLists.Select(list =>
         {
-            var rowKey = l.Name;
-            if (DisallowedCharsInTableKeys.IsMatch(l.Name))
-                rowKey = DisallowedCharsInTableKeys.Replace(rowKey, "");
+            var rowKey = list.Name;
+            if (DisallowedCharsInTableKeys.IsMatch(list.Name)) // https://stackoverflow.com/a/37749583/2343739
+            {
+                var oldRowKey = rowKey;
+                rowKey = DisallowedCharsInTableKeys.Replace(oldRowKey, "");
+                log.LogWarning(
+                    "Replaced disallowed characters in Azure Table Storage row key: '{oldRowKey}'. New row key: '{newRowKey}'.",
+                    oldRowKey, rowKey);
+            }
 
-            var listLicense = licenses.Single(li => li.Id == l.LicenseId);
-            var listViewUrls = filterListViewUrls.Where(u => u.FilterListId == l.Id).ToList();
+            var listLicense = licenses.Single(li => li.Id == list.LicenseId);
+            var listViewUrls = filterListViewUrls.Where(u => u.FilterListId == list.Id).ToList();
             var listSyntaxes = syntaxes.Where(s =>
-                    filterListSyntaxes.Where(fls => fls.FilterListId == l.Id).Select(fls => fls.SyntaxId)
+                    filterListSyntaxes.Where(fls => fls.FilterListId == list.Id)
+                        .Select(fls => fls.SyntaxId)
                         .Contains(s.Id))
                 .ToList();
             //var listSoftware = software.Where(s =>
             //    listSyntaxes.Select(ls => ls.Id).ToList()
             //        .Contains(softwareSyntaxes.Where(ss => ss.SoftwareId == s.Id).Select(ss => ss.SyntaxId)));
+            var listLanguageIso6391s = languages.Where(la =>
+                    filterListLanguages.Where(fll => fll.FilterListId == list.Id)
+                        .Select(fl => fl.LanguageId)
+                        .Contains(la.Id))
+                .ToList();
+
             var values = new Dictionary<string, object?>
             {
                 { nameof(IFilterListTableEntity.PartitionKey), "filterlist" },
                 { nameof(IFilterListTableEntity.RowKey), rowKey },
-                { nameof(IFilterListTableEntity.Id), l.Id },
-                { nameof(IFilterListTableEntity.Description), l.Description },
-                { nameof(IFilterListTableEntity.HomeUrl), l.HomeUrl },
-                { nameof(IFilterListTableEntity.OnionUrl), l.OnionUrl },
-                { nameof(IFilterListTableEntity.PolicyUrl), l.PolicyUrl },
-                { nameof(IFilterListTableEntity.SubmissionUrl), l.SubmissionUrl },
-                { nameof(IFilterListTableEntity.IssuesUrl), l.IssuesUrl },
-                { nameof(IFilterListTableEntity.ForumUrl), l.ForumUrl },
-                { nameof(IFilterListTableEntity.ChatUrl), l.ChatUrl },
-                { nameof(IFilterListTableEntity.EmailAddress), l.EmailAddress },
-                { nameof(IFilterListTableEntity.DonateUrl), l.DonateUrl },
+                { nameof(IFilterListTableEntity.Id), list.Id },
+                { nameof(IFilterListTableEntity.Description), list.Description },
+                { nameof(IFilterListTableEntity.HomeUrl), list.HomeUrl },
+                { nameof(IFilterListTableEntity.OnionUrl), list.OnionUrl },
+                { nameof(IFilterListTableEntity.PolicyUrl), list.PolicyUrl },
+                { nameof(IFilterListTableEntity.SubmissionUrl), list.SubmissionUrl },
+                { nameof(IFilterListTableEntity.IssuesUrl), list.IssuesUrl },
+                { nameof(IFilterListTableEntity.ForumUrl), list.ForumUrl },
+                { nameof(IFilterListTableEntity.ChatUrl), list.ChatUrl },
+                { nameof(IFilterListTableEntity.EmailAddress), list.EmailAddress },
+                { nameof(IFilterListTableEntity.DonateUrl), list.DonateUrl },
                 { nameof(IFilterListTableEntity.LicenseId), listLicense.Id },
                 { nameof(IFilterListTableEntity.LicenseName), listLicense.Name },
                 { nameof(IFilterListTableEntity.LicenseUrl), listLicense.Url },
@@ -96,18 +106,28 @@ public class PostSeedLists
 
             for (var i = 0; i < listViewUrls.Count; i++)
             {
-                values.Add($"{nameof(IFilterListTableEntity.ViewUrlSegmentNumber)}{i + 1}",
-                    listViewUrls[i].SegmentNumber);
-                values.Add($"{nameof(IFilterListTableEntity.ViewUrlPrimariness)}{i + 1}", listViewUrls[i].Primariness);
-                values.Add($"{nameof(IFilterListTableEntity.ViewUrl)}{i + 1}", listViewUrls[i].Url);
+                values.Add($"{nameof(IFilterListTableEntity.ViewUrlSegmentNumber)}{ToIndexerSuffix(i)}", listViewUrls[i].SegmentNumber);
+                values.Add($"{nameof(IFilterListTableEntity.ViewUrlPrimariness)}{ToIndexerSuffix(i)}", listViewUrls[i].Primariness);
+                values.Add($"{nameof(IFilterListTableEntity.ViewUrl)}{ToIndexerSuffix(i)}", listViewUrls[i].Url);
             }
+
 
             for (var i = 0; i < listSyntaxes.Count; i++)
             {
-                values.Add($"{nameof(IFilterListTableEntity.SyntaxId)}{i + 1}", listSyntaxes[i].Id);
-                values.Add($"{nameof(IFilterListTableEntity.SyntaxName)}{i + 1}", listSyntaxes[i].Name);
-                values.Add($"{nameof(IFilterListTableEntity.SyntaxDescription)}{i + 1}", listSyntaxes[i].Description);
-                values.Add($"{nameof(IFilterListTableEntity.SyntaxUrl)}{i + 1}", listSyntaxes[i].Url);
+                values.Add($"{nameof(IFilterListTableEntity.SyntaxId)}{ToIndexerSuffix(i)}", listSyntaxes[i].Id);
+                values.Add($"{nameof(IFilterListTableEntity.SyntaxName)}{ToIndexerSuffix(i)}", listSyntaxes[i].Name);
+                values.Add($"{nameof(IFilterListTableEntity.SyntaxDescription)}{ToIndexerSuffix(i)}", listSyntaxes[i].Description);
+                values.Add($"{nameof(IFilterListTableEntity.SyntaxUrl)}{ToIndexerSuffix(i)}", listSyntaxes[i].Url);
+            }
+
+            for (var i = 0; i < listLanguageIso6391s.Count; i++)
+            {
+                values.Add($"{nameof(IFilterListTableEntity.LanguageIso6391)}{ToIndexerSuffix(i)}", listLanguageIso6391s[i].Iso6391);
+            }
+
+            static string ToIndexerSuffix(int i)
+            {
+                return (i + 1).ToString().PadLeft(2, '0');
             }
 
             return new TableEntity(values);
