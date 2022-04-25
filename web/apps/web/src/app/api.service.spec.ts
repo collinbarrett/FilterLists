11b7cfba23e99551f8fe1eb15db3f378bc94fed5
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2022-04-25 05:33:18 -0500

    fix(web): 🐛 failing test import

--- web/apps/web/src/app/api.service.spec.ts
+++ web/apps/web/src/app/api.service.spec.ts
@@ -1,14 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ApiService } from './api.service';
 
 describe('ApiService', () => {
   let service: ApiService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiService);
-  });
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      providers: [HttpClientModule],
+    }).compileComponents();
+  }));
 
   it('should be created', () => {
     expect(service).toBeTruthy();
