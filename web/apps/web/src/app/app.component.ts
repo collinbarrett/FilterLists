147e2ddad14dd20dcb9032391e9ffc08a8677619
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2022-06-18 10:37:03 -0500

    refactor(web): 🔥 rm nx-based web

--- web/apps/web/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'filterlists-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class AppComponent {}
