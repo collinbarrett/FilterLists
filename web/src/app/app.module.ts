8f4c154b860c8f64a20f11aa2ad4abc09479b23c
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2022-06-18 11:07:49 -0500

    feat(web): ✨ ng add @angular/material

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
