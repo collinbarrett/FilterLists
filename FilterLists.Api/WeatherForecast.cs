d69d4672d70db9aef83d7f3af2383833b6cb04e3
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2023-04-01 13:26:13 -0500

    refactor(api): ♻️  downgrade to dotnet LTS (6.0)

--- FilterLists.Api/WeatherForecast.cs
+++ FilterLists.Api/WeatherForecast.cs
@@ -2,7 +2,7 @@ namespace FilterLists.Api;
 
 public class WeatherForecast
 {
-    public DateOnly Date { get; set; }
+    public DateTime Date { get; set; }
 
     public int TemperatureC { get; set; }
 
