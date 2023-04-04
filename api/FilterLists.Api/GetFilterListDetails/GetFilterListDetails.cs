d1ad9f92c428df6d4c5806c1fab73e04d70e9a6e
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2023-04-03 19:12:13 -0500

    feat(api): ✨impl list details endpoint

---
+++ api/FilterLists.Api/GetFilterListDetails/GetFilterListDetails.cs
@@ -0,0 +1,86 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FilterLists.Api.Infrastructure.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilterLists.Api.GetFilterListDetails;
+
+internal class GetFilterListDetails
+{
+    private readonly IQueryContext _queryContext;
+
+    public GetFilterListDetails(IQueryContext queryContext)
+    {
+        _queryContext = queryContext;
+    }
+
+    [FunctionName("GetFilterListDetails")]
+    public Task<FilterListDetails?> RunAsync(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = "lists/{id}")]
+        HttpRequest req,
+        int id,
+        CancellationToken cancellationToken)
+    {
+        return _queryContext.FilterLists
+            .Where(f => f.Id == id)
+            .Select(fl => new FilterListDetails
+            {
+                Id = fl.Id,
+                Name = fl.Name,
+                Description = fl.Description,
+                LicenseId = fl.LicenseId,
+                SyntaxIds = fl.FilterListSyntaxes
+                    .OrderBy(fls => fls.SyntaxId)
+                    .Select(fls => fls.SyntaxId),
+                LanguageIds = fl.FilterListLanguages
+                    .OrderBy(fll => fll.LanguageId)
+                    .Select(fll => fll.LanguageId),
+                TagIds = fl.FilterListTags.OrderBy(flt => flt.TagId)
+                    .Select(flt => flt.TagId),
+                ViewUrls = fl.ViewUrls
+                    .OrderBy(u => u.SegmentNumber)
+                    .ThenBy(u => u.Primariness)
+                    .Select(u =>
+                        new FilterListViewUrl
+                        {
+                            SegmentNumber = u.SegmentNumber,
+                            Primariness = u.Primariness,
+                            Url = u.Url
+                        }),
+                HomeUrl = fl.HomeUrl,
+                OnionUrl = fl.OnionUrl,
+                PolicyUrl = fl.PolicyUrl,
+                SubmissionUrl = fl.SubmissionUrl,
+                IssuesUrl = fl.IssuesUrl,
+                ForumUrl = fl.ForumUrl,
+                ChatUrl = fl.ChatUrl,
+                EmailAddress = fl.EmailAddress,
+                DonateUrl = fl.DonateUrl,
+                MaintainerIds = fl.FilterListMaintainers
+                    .OrderBy(flm => flm.MaintainerId)
+                    .Select(flm => flm.MaintainerId),
+                UpstreamFilterListIds = fl.UpstreamFilterLists.OrderBy(f => f.UpstreamFilterListId)
+                    .Select(f => f.UpstreamFilterListId),
+                ForkFilterListIds = fl.ForkFilterLists
+                    .OrderBy(f => f.ForkFilterListId)
+                    .Select(f => f.ForkFilterListId),
+                IncludedInFilterListIds = fl.IncludedInFilterLists
+                    .OrderBy(m => m.IncludedInFilterListId)
+                    .Select(m => m.IncludedInFilterListId),
+                IncludesFilterListIds = fl.IncludesFilterLists
+                    .OrderBy(m => m.IncludesFilterListId)
+                    .Select(m => m.IncludesFilterListId),
+                DependencyFilterListIds = fl.DependencyFilterLists
+                    .OrderBy(d => d.DependencyFilterListId)
+                    .Select(d => d.DependencyFilterListId),
+                DependentFilterListIds = fl.DependentFilterLists
+                    .OrderBy(d => d.DependentFilterListId)
+                    .Select(d => d.DependentFilterListId)
+            })
+            .SingleOrDefaultAsync(cancellationToken);
+    }
+}
