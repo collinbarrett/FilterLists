10e96b76d369a038181de308ff22da74cbb6754c
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2023-04-10 16:27:24 -0500

    fix(web): 🐛🚧 temp fallback to img
    ref https://github.com/Azure/static-web-apps/discussions/1066#discussioncomment-5570779

--- web/components/header/logo/logo.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import Link from "next/link";
-import logo from "./logo_filterlists.png";
-
-export const Logo = () => (
-  <Link href="/">
-    <Image src={logo} alt="FilterLists logo" />
-  </Link>
-);
--- web/components/index.ts
+++ web/components/index.ts
@@ -4,4 +4,4 @@ export * from "./footer/donate";
 export * from "./footer/github";
 export * from "./footer/menu";
 export * from "./footer/twitter";
-export * from "./header/logo/logo";
+export * from "./header/logo";
