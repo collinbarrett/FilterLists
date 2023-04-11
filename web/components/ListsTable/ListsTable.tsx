28f799c364e8f446b9dae89b2c8d4d108120a225
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2023-04-11 07:36:23 -0500

    feat(web): ✨ add Description column

--- web/components/ListsTable/ListsTable.tsx
+++ web/components/ListsTable/ListsTable.tsx
@@ -7,6 +7,21 @@ interface Props {
 
 export const ListsTable = (props: Props) => (
   <Table<FilterList> dataSource={props.lists} rowKey={(record) => record.id}>
-    <Table.Column<FilterList> key="name" title="Name" dataIndex="name" />
+    <Table.Column<FilterList>
+      key="name"
+      title="Name"
+      dataIndex="name"
+      sorter={(a, b) => a.name.localeCompare(b.name)}
+    />
+    <Table.Column<FilterList>
+      key="description"
+      title="Description"
+      dataIndex="description"
+      sorter={(a, b) => localeCompare(a.description, b.description)}
+    />
   </Table>
 );
+
+function localeCompare(a?: string, b?: string): number {
+  return a ? (b ? a.localeCompare(b) : -1) : 1;
+}
