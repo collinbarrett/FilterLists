ea6d66158337556f2deeee5765c00036e15712c3
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2023-04-11 13:03:38 -0500

    fix(web): 🐛📦🚧 downgrade next to support <Image> in standalone
    ref https://github.com/vercel/next.js/issues/48173

---
+++ web/components/Header/Logo/Logo.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+import logo from "./logo_filterlists.png";
+import Image from "next/image";
+
+export const Logo = () => (
+  <Link href="/">
+    <Image src={logo} alt="FilterLists logo" />
+  </Link>
+);
