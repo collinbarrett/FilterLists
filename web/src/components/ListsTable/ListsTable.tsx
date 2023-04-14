03025a32bfc85c6485a97b93eb813327ff86ef0d
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2023-04-14 12:03:18 -0500

    refactor(web): ♻️ extract useListsTable()

--- web/src/components/ListsTable/ListsTable.tsx
+++ web/src/components/ListsTable/ListsTable.tsx
@@ -1,96 +1,18 @@
+import { Space, Table } from "antd";
+import { useListsTable } from "./useListsTable";
 import { OData, FilterList } from "@/src/interfaces";
-import { Space, Table, TablePaginationConfig } from "antd";
-import { coalesceToArray, localeCompare } from "@/src/utils";
-import { useState } from "react";
-import {
-  FilterValue,
-  SorterResult,
-  TableCurrentDataSource,
-} from "antd/es/table/interface";
+import { localeCompare } from "@/src/utils";
 import { ShowListButton } from "./ShowListButton";
 
 export const ListsTable = (props: OData<FilterList>) => {
-  const [data, setData] = useState(props.value);
-  const [pagination, setPagination] = useState({
-    current: 1,
-    pageSize: 10,
-    total: props["@odata.count"] ?? 0,
-  });
-  const [loading, setLoading] = useState(false);
-
-  const handleChange = async (
-    pagination: TablePaginationConfig,
-    _filters: Record<string, FilterValue | null>,
-    sorter: SorterResult<FilterList> | SorterResult<FilterList>[],
-    _extra: TableCurrentDataSource<FilterList>
-  ) => {
-    setLoading(true);
-    const params = new URLSearchParams({
-      $count: "true",
-      $skip: (
-        ((pagination.current ?? 0) - 1) *
-        (pagination.pageSize ?? 0)
-      ).toString(),
-      $top: (pagination.pageSize ?? 0).toString(),
-    });
-    const orderBy = toODataOrderBy(sorter);
-    if (orderBy.length) {
-      params.append("$orderby", orderBy);
-    }
-    const url =
-      `${process.env.NEXT_PUBLIC_FILTERLISTS_API_URL}/lists?` + params;
-    const response = await fetch(url);
-    const jsonData = await response.json();
-    setData(jsonData.value);
-    setPagination({
-      current: pagination.current ?? 0,
-      pageSize: pagination.pageSize ?? 0,
-      total: jsonData["@odata.count"] ?? 0,
-    });
-    setLoading(false);
-  };
-
+  const listsTableProps = useListsTable(props);
   return (
-    <Table<FilterList>
-      dataSource={data}
-      rowKey={(filterlist) => filterlist.id}
-      pagination={pagination}
-      loading={loading}
-      onChange={handleChange}
-    >
+    <Table<FilterList> {...listsTableProps}>
       {nameColumn}
       {descriptionColumn}
       {actionsColumn}
     </Table>
   );
-};
-
-const toODataOrderBy = (
-  sorter: SorterResult<FilterList> | SorterResult<FilterList>[]
-) => {
-  const sorterArray = coalesceToArray(sorter);
-  return sorterArray
-    .sort((a, b) => {
-      const aMultiple = (
-        a.column?.sorter as {
-          multiple?: number;
-        }
-      ).multiple;
-      const bMultiple = (
-        b.column?.sorter as {
-          multiple?: number;
-        }
-      ).multiple;
-      return (aMultiple ?? Number.MAX_VALUE) - (bMultiple ?? Number.MAX_VALUE);
-    })
-    .map((element) => {
-      if (element.order) {
-        return `${element.field} ${
-          element.order === "descend" ? "desc" : "asc"
-        }`;
-      }
-    })
-    .join(",");
 };
 
 const nameColumn = (
@@ -117,8 +39,8 @@ const descriptionColumn = (
 
 const actionsColumn = (
   <Table.Column<FilterList>
-    title="Actions"
     dataIndex="id"
+    title="Actions"
     render={(id, filterlist) => (
       <Space>
         <ShowListButton listId={id} listName={filterlist.name} />
---
+++ web/src/components/ListsTable/useListsTable.ts
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import { TablePaginationConfig, SorterResult } from "antd/lib/table/interface";
+import { FilterValue } from "antd/lib/table/interface";
+import { TableCurrentDataSource } from "antd/lib/table/interface";
+import { FilterList, OData } from "@/src/interfaces";
+import { coalesceToArray } from "@/src/utils";
+
+export const useListsTable = (props: OData<FilterList>) => {
+  const [dataSource, setData] = useState(props.value);
+  const [pagination, setPagination] = useState({
+    current: 1,
+    pageSize: 10,
+    total: props["@odata.count"] ?? 0,
+  });
+  const [loading, setLoading] = useState(false);
+
+  const onChange = async (
+    pagination: TablePaginationConfig,
+    _filters: Record<string, FilterValue | null>,
+    sorter: SorterResult<FilterList> | SorterResult<FilterList>[],
+    _extra: TableCurrentDataSource<FilterList>
+  ) => {
+    setLoading(true);
+
+    const params = new URLSearchParams({
+      $count: "true",
+      $skip: (
+        ((pagination.current ?? 0) - 1) *
+        (pagination.pageSize ?? 0)
+      ).toString(),
+      $top: (pagination.pageSize ?? 0).toString(),
+    });
+    const orderBy = toODataOrderBy(sorter);
+    if (orderBy.length) {
+      params.append("$orderby", orderBy);
+    }
+
+    const url =
+      `${process.env.NEXT_PUBLIC_FILTERLISTS_API_URL}/lists?` + params;
+    const response = await fetch(url);
+    const jsonData = await response.json();
+
+    setData(jsonData.value);
+    setPagination({
+      current: pagination.current ?? 0,
+      pageSize: pagination.pageSize ?? 0,
+      total: jsonData["@odata.count"] ?? 0,
+    });
+
+    setLoading(false);
+  };
+
+  const rowKey = (filterlist: FilterList) => filterlist.id;
+
+  return { dataSource, pagination, loading, onChange, rowKey };
+};
+
+const toODataOrderBy = (
+  sorter: SorterResult<FilterList> | SorterResult<FilterList>[]
+) => {
+  const sorterArray = coalesceToArray(sorter);
+  return sorterArray
+    .sort((a, b) => {
+      const aMultiple = (
+        a.column?.sorter as {
+          multiple?: number;
+        }
+      ).multiple;
+      const bMultiple = (
+        b.column?.sorter as {
+          multiple?: number;
+        }
+      ).multiple;
+      return (aMultiple ?? Number.MAX_VALUE) - (bMultiple ?? Number.MAX_VALUE);
+    })
+    .map((element) => {
+      if (element.order) {
+        return `${element.field} ${
+          element.order === "descend" ? "desc" : "asc"
+        }`;
+      }
+    })
+    .join(",");
+};
