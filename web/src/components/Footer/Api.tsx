41352a6e5bf6a7fc245a723ac6f542950895bff5
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2023-04-13 20:58:06 -0500

    feat(web): ✨ implement server-side pagination

--- web/src/components/Footer/Api.tsx
+++ web/src/components/Footer/Api.tsx
@@ -4,7 +4,7 @@ import { Tag } from "antd";
 export const Api = () => (
   <Tag>
     <a
-      href={process.env.FILTERLISTS_API_URL}
+      href={process.env.NEXT_PUBLIC_FILTERLISTS_API_URL}
       title="FilterLists API"
       target="_blank"
       rel="noopener noreferrer"
--- web/src/components/ListsTable/ListsTable.tsx
+++ web/src/components/ListsTable/ListsTable.tsx
@@ -1,10 +1,50 @@
 import { OData, FilterList } from "@/src/interfaces";
-import { Table } from "antd";
+import { Table, TablePaginationConfig } from "antd";
 import { localeCompare } from "@/src/utils";
+import { useState } from "react";
 
 export const ListsTable = (props: OData<FilterList>) => {
+  const [data, setData] = useState(props.value);
+  const [pagination, setPagination] = useState({
+    current: 1,
+    pageSize: 10,
+    total: props["@count"] ?? 0,
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleTableChange = async (pagination: TablePaginationConfig) => {
+    setLoading(true);
+    try {
+      const res = await fetch(
+        process.env.NEXT_PUBLIC_FILTERLISTS_API_URL +
+          "/lists?$skip=" +
+          ((pagination.current ?? 0) - 1) * (pagination.pageSize ?? 0) +
+          "$top=" +
+          pagination.pageSize +
+          "&$count=true"
+      );
+      const filterlists = (await res.json()) as OData<FilterList>;
+      setData(filterlists.value);
+      setPagination({
+        current: pagination.current ?? 0,
+        pageSize: pagination.pageSize ?? 0,
+        total: filterlists["@count"] ?? 0,
+      });
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.error("Failed to fetch data:", error);
+    }
+  };
+
   return (
-    <Table<FilterList> dataSource={props.value} rowKey={(record) => record.id}>
+    <Table<FilterList>
+      dataSource={data}
+      rowKey={(record) => record.id}
+      pagination={pagination}
+      loading={loading}
+      onChange={handleTableChange}
+    >
       {nameColumn}
       {descriptionColumn}
     </Table>
