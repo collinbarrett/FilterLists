1c90142b1621047d3d870940c83cc0a8b199ee3d
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2023-04-14 15:17:39 -0500

    feat(web): ✨ colorize LicenseTag

--- web/src/components/ListsTable/components/LicenseTag.tsx
+++ web/src/components/ListsTable/components/LicenseTag.tsx
@@ -2,7 +2,7 @@ import { License } from "@/src/interfaces";
 import { Tag } from "antd";
 
 export const LicenseTag = (license: License) => (
-  <Tag>
+  <Tag color={getColor(license)}>
     {license.url ? (
       <a
         href={license.url}
@@ -17,3 +17,28 @@ export const LicenseTag = (license: License) => (
     )}
   </Tag>
 );
+
+function getColor(license: License) {
+  switch (
+    `${license.permitsDistribution}-${license.permitsModification}-${license.permitsCommercialUse}`
+  ) {
+    case "true-true-true":
+      return "cyan";
+    case "true-true-false":
+      return "green";
+    case "true-false-true":
+      return "lime";
+    case "true-false-false":
+      return "gold";
+    case "false-true-true":
+      return "orange";
+    case "false-true-false":
+      return "volcano";
+    case "false-false-true":
+      return "red";
+    case "false-false-false":
+      return "magenta";
+    default:
+      return undefined;
+  }
+}
