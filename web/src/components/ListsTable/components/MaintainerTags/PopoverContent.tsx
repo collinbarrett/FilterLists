4fce37d28b833b2237438dec3aa5a1944baca184
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2023-04-15 21:16:16 -0500

    feat(web): ✨ button-ify maintainer column popovers

--- web/src/components/ListsTable/components/MaintainerTags/PopoverContent.tsx
+++ web/src/components/ListsTable/components/MaintainerTags/PopoverContent.tsx
@@ -1,16 +1,57 @@
-import { List } from "antd";
+import { Button, List } from "antd";
 
 import { Maintainer } from "@/src/interfaces";
+import { HomeOutlined, MailOutlined, TwitterOutlined } from "@ant-design/icons";
 
 export const PopoverContent = ({
+  name,
   url,
   emailAddress,
   twitterHandle,
 }: Maintainer) => (
   <List size="small">
-    {/* TODO: linkify/buttonify */}
-    {url && <List.Item>{url}</List.Item>}
-    {emailAddress && <List.Item>{emailAddress}</List.Item>}
-    {twitterHandle && <List.Item>{twitterHandle}</List.Item>}
+    {url && (
+      <List.Item>
+        <Button
+          block
+          type="primary"
+          href={url}
+          title={`View ${name}'s homepage.`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <HomeOutlined />
+          &nbsp;Website
+        </Button>
+      </List.Item>
+    )}
+    {emailAddress && (
+      <List.Item>
+        <Button
+          block
+          href={`mailto:${emailAddress}`}
+          title={`Email ${name}.`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <MailOutlined />
+          &nbsp;Email
+        </Button>
+      </List.Item>
+    )}
+    {twitterHandle && (
+      <List.Item>
+        <Button
+          block
+          href={`https://twitter.com/${twitterHandle}`}
+          title={`${twitterHandle} on Twitter`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <TwitterOutlined />
+          &nbsp;Twitter
+        </Button>
+      </List.Item>
+    )}
   </List>
 );
