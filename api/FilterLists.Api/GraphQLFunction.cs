8b67cafc9fff73b834fb1b9d6b4908a755d2e34a
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2023-04-19 18:19:13 -0500

    refactor(api): ♻️ undo last

--- api/FilterLists.Api/GraphQLFunction.cs
+++ api/FilterLists.Api/GraphQLFunction.cs
@@ -1,6 +1,4 @@
-using System.Net;
 using HotChocolate.AzureFunctions;
-using HotChocolate.Execution;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 
@@ -10,32 +8,18 @@ namespace FilterLists.Api;
 public class GraphQLFunction
 {
     private readonly IGraphQLRequestExecutor _executor;
-    private readonly IRequestExecutorResolver _executorResolver;
 
-    public GraphQLFunction(IGraphQLRequestExecutor executor, IRequestExecutorResolver executorResolver)
+    public GraphQLFunction(IGraphQLRequestExecutor executor)
     {
         _executor = executor;
-        _executorResolver = executorResolver;
     }
 
     [Function(nameof(GraphQLFunction))]
     public async Task<HttpResponseData> Run(
-        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "graphql/{**slug}")]
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "graphql" /*"graphql/{**slug}"*/)]
         HttpRequestData request,
         CancellationToken cancellationToken)
     {
-        // serve SDL in production for API Gateway
-        var environmentVariable = Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT");
-        if (environmentVariable != "Development" &&
-            request.Method.ToUpperInvariant() == "GET")
-        {
-            var executor = await _executorResolver.GetRequestExecutorAsync(cancellationToken: cancellationToken);
-            var schemaText = executor.Schema.ToString();
-            var response = request.CreateResponse(HttpStatusCode.OK);
-            await response.WriteStringAsync(schemaText, cancellationToken);
-            return response;
-        }
-
         return await _executor.ExecuteAsync(request);
     }
 }
