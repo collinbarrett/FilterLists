ee4b112144ad4b37872102119dba939ed7ef4643
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2023-04-25 16:58:20 -0500

    fix(api): 🐛 default Url value for EF

--- api/FilterLists.Api/Persistence/Entities/FilterListViewUrl.cs
+++ api/FilterLists.Api/Persistence/Entities/FilterListViewUrl.cs
@@ -7,7 +7,5 @@ public record FilterListViewUrl
     public FilterList FilterList { get; init; } = new();
     public int SegmentNumber { get; init; }
     public int Primariness { get; init; }
-
-    // ReSharper disable once AutoPropertyCanBeMadeGetOnly.Global
-    public Uri Url { get; init; } = new(string.Empty);
+    public Uri Url { get; init; } = default!;
 }
