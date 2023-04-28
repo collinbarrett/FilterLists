3625f6f80a6c63ebaf97f293d49cc7707caa6992
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2023-04-28 07:31:25 -0500

    fix(api): 🐛 apply Global IDs to FilterListViewUrl

---
+++ api/FilterLists.Api/Api/Types/FilterListViewUrl.cs
@@ -0,0 +1,14 @@
+﻿using FilterLists.Api.Persistence.Entities;
+using HotChocolate.Types;
+
+namespace FilterLists.Api.Api.Types;
+
+[ObjectType]
+public class FilterListViewUrlType : ObjectType<FilterListViewUrl>
+{
+    protected override void Configure(IObjectTypeDescriptor<FilterListViewUrl> descriptor)
+    {
+        descriptor.Field(u => u.Id).ID(nameof(FilterListViewUrl));
+        descriptor.Field(u => u.FilterListId).ID(nameof(FilterList));
+    }
+}
