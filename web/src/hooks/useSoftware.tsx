97dd05456848235d35f44ede3d60922050e335a9
Collin Barrett <collinbarrett@users.noreply.github.com>
2023-11-05 12:16:06 -0600

    refactor(web): 🔥 purge web to init new nextjs

--- web/src/hooks/useSoftware.tsx
+++
@@ -1,7 +0,0 @@
-import { Software } from "../interfaces/Software";
-import { useApiData } from "./useApiData";
-
-export const useSoftware = () =>
-  (useApiData<Software[]>("/api/directory/software") || []).sort(
-    (a: Software, b: Software) => a.name.localeCompare(b.name)
-  );
