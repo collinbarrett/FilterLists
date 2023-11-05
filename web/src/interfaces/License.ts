b08391745ff934f4e9b25f6f3e01f41b97f3ddac
Collin Barrett <collinbarrett@users.noreply.github.com>
2023-11-05 12:16:06 -0600

    refactor(web): 🔥 purge web to init new nextjs

--- web/src/interfaces/License.ts
+++
@@ -1,9 +0,0 @@
-export interface License {
-  id: number;
-  name: string;
-  url: string;
-  permitsModification: boolean;
-  permitsDistribution: boolean;
-  permitsCommercialUse: boolean;
-  filterListIds: number[];
-}
