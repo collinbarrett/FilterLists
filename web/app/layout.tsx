d88f1923595f7edd0222d637dccb9c6850f7a672
Collin Barrett <collinbarrett@users.noreply.github.com>
2023-11-07 20:23:16 -0600

    feat(web): ✨ add light/dark mode toggle
    ref https://ui.shadcn.com/docs/dark-mode/next

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/mode-toggle";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,7 +28,15 @@ export default function RootLayout({
           inter.variable,
         )}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ModeToggle />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
