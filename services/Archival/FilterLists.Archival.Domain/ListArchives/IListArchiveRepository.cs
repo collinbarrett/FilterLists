d9b0fb7ad92838793d7deaba2d863462ca41f564
Collin Barrett <collinbarrett@users.noreply.github.com>
2024-05-25 15:12:38 -0500

    refactor: 🔥 purge unused Archival API

--- services/Archival/FilterLists.Archival.Domain/ListArchives/IListArchiveRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using FilterLists.SharedKernel.Domain.SeedWork;
-
-namespace FilterLists.Archival.Domain.ListArchives;
-
-public interface IListArchiveRepository : IUnitOfWork
-{
-    Task AddAsync(ListArchive listArchive, CancellationToken cancellationToken);
-}
