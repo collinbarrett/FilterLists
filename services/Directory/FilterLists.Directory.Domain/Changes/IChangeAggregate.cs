2ac8ecfa020939b58d648537f1658412c01b7a08
Collin Barrett <collinbarrett@users.noreply.github.com>
2024-05-27 09:23:23 -0500

    refactor: 🔥🚧 purge docker, nginx, and Directory

--- services/Directory/FilterLists.Directory.Domain/Changes/IChangeAggregate.cs
+++
@@ -1,9 +0,0 @@
-﻿using FilterLists.SharedKernel.Domain.SeedWork;
-
-namespace FilterLists.Directory.Domain.Changes;
-
-public interface IChangeAggregate<out TAggregateValueObject> where TAggregateValueObject : ValueObject
-{
-    TAggregateValueObject? Before { get; }
-    TAggregateValueObject? After { get; }
-}
