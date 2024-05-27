eeebb0ec5604260a121f07b5fca2b3f4fc0df940
Collin Barrett <collinbarrett@users.noreply.github.com>
2024-05-27 17:19:18 -0500

    feat(svcs): ✨ add AppInsights ref https://learn.microsoft.com/en-us/dotnet/aspire/deployment/azure/application-insights

--- services/FilterLists.ServiceDefaults/Extensions.cs
+++ services/FilterLists.ServiceDefaults/Extensions.cs
@@ -1,3 +1,4 @@
+using Azure.Monitor.OpenTelemetry.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.DependencyInjection;
@@ -68,12 +69,9 @@ public static class Extensions
 
         if (useOtlpExporter) builder.Services.AddOpenTelemetry().UseOtlpExporter();
 
-        // Uncomment the following lines to enable the Azure Monitor exporter (requires the Azure.Monitor.OpenTelemetry.AspNetCore package)
-        //if (!string.IsNullOrEmpty(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]))
-        //{
-        //    builder.Services.AddOpenTelemetry()
-        //       .UseAzureMonitor();
-        //}
+        if (!string.IsNullOrEmpty(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]))
+            builder.Services.AddOpenTelemetry()
+                .UseAzureMonitor();
 
         return builder;
     }
