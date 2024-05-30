2dbbdd981e4482858c14c6495f792306360a279c
Collin Barrett <collinbarrett@users.noreply.github.com>
2024-05-30 17:18:27 -0500

    feat(dir): ✨ migrate QueryDbContext

---
+++ services/Directory/FilterLists.Directory.Infrastructure/Persistence/Queries/Entities/FilterListLanguage.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FilterLists.Directory.Infrastructure.Persistence.Queries.Entities;
+
+public record FilterListLanguage
+{
+    public long FilterListId { get; init; }
+    public FilterList FilterList { get; init; } = default!;
+    public long LanguageId { get; init; }
+    public Language Language { get; init; } = default!;
+}
+
+internal class FilterListLanguageTypeConfiguration : IEntityTypeConfiguration<FilterListLanguage>
+{
+    public virtual void Configure(EntityTypeBuilder<FilterListLanguage> builder)
+    {
+        builder.HasKey(fll => new { fll.FilterListId, fll.LanguageId });
+        builder.HasDataJsonFile<FilterListLanguage>();
+    }
+}
---
+++ services/Directory/FilterLists.Directory.Infrastructure/Persistence/Queries/Entities/Fork.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FilterLists.Directory.Infrastructure.Persistence.Queries.Entities;
+
+public record Fork
+{
+    public long UpstreamFilterListId { get; init; }
+    public FilterList UpstreamFilterList { get; init; } = default!;
+    public long ForkFilterListId { get; init; }
+    public FilterList ForkFilterList { get; init; } = default!;
+}
+
+internal class ForkTypeConfiguration : IEntityTypeConfiguration<Fork>
+{
+    public virtual void Configure(EntityTypeBuilder<Fork> builder)
+    {
+        builder.HasKey(f => new { f.UpstreamFilterListId, f.ForkFilterListId });
+        builder.HasOne(f => f.UpstreamFilterList)
+            .WithMany(fl => fl.ForkFilterLists)
+            .HasForeignKey(f => f.UpstreamFilterListId);
+        builder.HasOne(f => f.ForkFilterList)
+            .WithMany(fl => fl.UpstreamFilterLists)
+            .HasForeignKey(f => f.ForkFilterListId);
+        builder.HasDataJsonFile<Fork>();
+    }
+}
