15e9d493fbe8eaee458b12615534e753baebe781
Collin Barrett <collinbarrett@users.noreply.github.com>
2024-05-31 16:43:26 -0500

    feat(dir): ✨ apply migrations on startup

--- services/Directory/FilterLists.Directory.Infrastructure.MigrationService/Worker.cs
+++ services/Directory/FilterLists.Directory.Infrastructure.MigrationService/Worker.cs
@@ -1,21 +1,85 @@
+using System.Diagnostics;
+using FilterLists.Directory.Infrastructure.Persistence.Queries.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using OpenTelemetry.Trace;
+
 namespace FilterLists.Directory.Infrastructure.MigrationService;
 
-public class Worker : BackgroundService
+/// <remarks>https://learn.microsoft.com/en-us/dotnet/aspire/database/ef-core-migrations#create-the-migration-service</remarks>
+public class Worker(
+    IServiceProvider serviceProvider,
+    IHostApplicationLifetime hostApplicationLifetime,
+    IHostEnvironment hostEnvironment) : BackgroundService
 {
-    private readonly ILogger<Worker> _logger;
+    private const string ActivitySourceName = "Migrations";
+    private static readonly ActivitySource ActivitySource = new(ActivitySourceName);
 
-    public Worker(ILogger<Worker> logger)
+    protected override async Task ExecuteAsync(CancellationToken cancellationToken)
     {
-        _logger = logger;
-    }
+        // allow SQL Server container time to start
+        await Task.Delay(3000, cancellationToken);
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        while (!stoppingToken.IsCancellationRequested)
+        using var activity = ActivitySource.StartActivity("Migrating database", ActivityKind.Client);
+
+        try
         {
-            if (_logger.IsEnabled(LogLevel.Information))
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            await Task.Delay(1000, stoppingToken);
+            using var scope = serviceProvider.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<QueryDbContext>();
+
+            if (hostEnvironment.IsDevelopment()) await EnsureDatabaseAsync(dbContext, cancellationToken);
+            await RunMigrationAsync(dbContext, cancellationToken);
+            // await SeedDataAsync(dbContext, cancellationToken);
         }
+        catch (Exception ex)
+        {
+            activity?.RecordException(ex);
+            throw;
+        }
+
+        hostApplicationLifetime.StopApplication();
     }
+
+    private static async Task EnsureDatabaseAsync(QueryDbContext dbContext, CancellationToken cancellationToken)
+    {
+        var dbCreator = dbContext.GetService<IRelationalDatabaseCreator>();
+
+        var strategy = dbContext.Database.CreateExecutionStrategy();
+        await strategy.ExecuteAsync(async () =>
+        {
+            if (!await dbCreator.ExistsAsync(cancellationToken)) await dbCreator.CreateAsync(cancellationToken);
+        });
+    }
+
+    private static async Task RunMigrationAsync(QueryDbContext dbContext, CancellationToken cancellationToken)
+    {
+        var strategy = dbContext.Database.CreateExecutionStrategy();
+        await strategy.ExecuteAsync(async () =>
+        {
+            await using var transaction = await dbContext.Database.BeginTransactionAsync(cancellationToken);
+            await dbContext.Database.MigrateAsync(cancellationToken);
+            await transaction.CommitAsync(cancellationToken);
+        });
+    }
+
+    // private static async Task SeedDataAsync(QueryDbContext dbContext, CancellationToken cancellationToken)
+    // {
+    //     SupportTicket firstTicket = new()
+    //     {
+    //         Title = "Test Ticket",
+    //         Description = "Default ticket, please ignore!",
+    //         Completed = true
+    //     };
+    //
+    //     var strategy = dbContext.Database.CreateExecutionStrategy();
+    //     await strategy.ExecuteAsync(async () =>
+    //     {
+    //         // Seed the database
+    //         await using var transaction = await dbContext.Database.BeginTransactionAsync(cancellationToken);
+    //         await dbContext.Tickets.AddAsync(firstTicket, cancellationToken);
+    //         await dbContext.SaveChangesAsync(cancellationToken);
+    //         await transaction.CommitAsync(cancellationToken);
+    //     });
+    // }
 }
