3dfe473df2730d6d6c62054a05dd182837490f98
Collin Barrett <collinbarrett@users.noreply.github.com>
2024-06-01 14:38:47 -0500

    refactor(svcs): ♻️🎨 ren/tidy resources

--- services/FilterLists.AppHost/Program.cs
+++ services/FilterLists.AppHost/Program.cs
@@ -2,10 +2,10 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<FilterLists_ApiService>("apiservice");
+var directoryApi = builder.AddProject<FilterLists_ApiService>("directoryapi");
 
-builder.AddProject<FilterLists_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithReference(apiService);
+builder.AddProject<FilterLists_Web>("web")
+    .WithReference(directoryApi)
+    .WithExternalHttpEndpoints();
 
 builder.Build().Run();
