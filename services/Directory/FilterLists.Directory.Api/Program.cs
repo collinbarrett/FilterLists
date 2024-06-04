e0647a1303606d4c8a198fb2e8a85c29ea24c297
Collin Barrett <collinbarrett@users.noreply.github.com>
2024-06-04 10:53:21 -0500

    feat(dir): ✨ migrate OpenAPI & SwaggerUI

--- services/Directory/FilterLists.Directory.Api/Program.cs
+++ services/Directory/FilterLists.Directory.Api/Program.cs
@@ -1,4 +1,5 @@
 using FilterLists.Directory.Api;
+using FilterLists.Directory.Api.OpenApi;
 using FilterLists.Directory.Application;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.ConfigureKestrel(serverOptions => serverOptions.AddServerHeader = false);
 builder.AddServiceDefaults();
 builder.Services.AddProblemDetails();
+builder.Services.AddOpenApiGen();
 builder.AddApplication();
 
 var app = builder.Build();
@@ -13,5 +15,7 @@ var app = builder.Build();
 app.UseExceptionHandler();
 app.MapEndpoints();
 app.MapDefaultEndpoints();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
