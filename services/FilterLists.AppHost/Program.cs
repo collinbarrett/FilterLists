b7c857545561cd4fdeed881ff9e276a412b67fdf
Collin Barrett <collinbarrett@users.noreply.github.com>
2024-06-11 19:08:57 -0500

    test(dir): 🚧🧪 WIP int test /alive

--- services/FilterLists.AppHost/Program.cs
+++ services/FilterLists.AppHost/Program.cs
@@ -2,15 +2,12 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var appInsights = builder.AddAzureApplicationInsights("appinsights");
-
 var directoryDb = builder.AddSqlServer("directorysqlserver")
     .WithDataVolume()
     .AddDatabase("directorydb");
 
 builder.AddProject<FilterLists_Directory_Api>("directoryapi")
     .WithReference(directoryDb)
-    .WithReference(appInsights)
     .WithExternalHttpEndpoints();
 
 builder.Build().Run();
