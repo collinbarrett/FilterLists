05edc6506ae5cc2e1bd7fb711baf0cbfdf5b67c9
Collin Barrett <collinbarrett@users.noreply.github.com>
2024-06-13 15:43:46 -0500

    refactor(web): ♻️ extract staging-web GH environment

--- services/Directory/FilterLists.Directory.Api/Program.cs
+++ services/Directory/FilterLists.Directory.Api/Program.cs
@@ -5,9 +5,16 @@ using FilterLists.Directory.Application;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
-    policy.WithOrigins("https://filterlists.com")
+{
+    policy.SetIsOriginAllowed(origin =>
+        {
+            if (origin == "https://filterlists.com") return true;
+            return origin.StartsWith("https://nice-water-05873140f", StringComparison.InvariantCulture) &&
+                   origin.EndsWith(".eastus2.5.azurestaticapps.net", StringComparison.InvariantCulture);
+        })
         .WithMethods("GET")
-        .AllowAnyHeader()));
+        .AllowAnyHeader();
+}));
 builder.WebHost.ConfigureKestrel(serverOptions => serverOptions.AddServerHeader = false);
 builder.AddServiceDefaults();
 builder.Services.AddProblemDetails();
