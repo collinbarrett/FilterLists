2ad05c493a1f3b048f67920e4552241e79cff102
Collin Barrett <collinbarrett@users.noreply.github.com>
2024-06-15 14:59:00 -0500

    feat(web): ✨🌗 add dark mode
    ref https://ui.shadcn.com/docs/dark-mode/next

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,5 +1,6 @@
 import { FilterList, columns } from "./columns";
 import { DataTable } from "../components/ui/data-table";
+import { ModeToggle } from "@/components/mode-toggle";
 
 async function getData(): Promise<FilterList[]> {
   const response = await fetch("https://api.filterlists.com/lists");
@@ -15,6 +16,9 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="absolute top-0 right-0 m-4">
+        <ModeToggle />
+      </div>
       <div className="container mx-auto py-10">
         <DataTable columns={columns} data={data} />
       </div>
