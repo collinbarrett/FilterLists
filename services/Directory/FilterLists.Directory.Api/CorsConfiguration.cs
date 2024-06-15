c9cdd0ace5b9678c2d2398d29016bb3b58a89c84
Collin Barrett <collinbarrett@users.noreply.github.com>
2024-06-15 12:21:18 -0500

    feat(dir): ✨🔓 allow CORS from Next.js staging

--- services/Directory/FilterLists.Directory.Api/CorsConfiguration.cs
+++ services/Directory/FilterLists.Directory.Api/CorsConfiguration.cs
@@ -7,13 +7,14 @@ internal static class CorsConfiguration
     private const string ProductionClientOrigin = "https://filterlists.com";
     private const string StagingClientOriginStart = "https://nice-water-05873140f";
     private const string StagingClientOriginEnd = ".eastus2.5.azurestaticapps.net";
+    private const string NextJsStagingClientOrigin = "https://next.filterlists.com";
     private const string LocalClientOrigin = "http://localhost:3000";
 
     internal static readonly Action<CorsOptions> SetupAction = options => options.AddDefaultPolicy(policy =>
     {
         policy.SetIsOriginAllowed(origin => origin switch
             {
-                ProductionClientOrigin or LocalClientOrigin => true,
+                ProductionClientOrigin or LocalClientOrigin or NextJsStagingClientOrigin => true,
                 _ => origin.StartsWith(StagingClientOriginStart, StringComparison.InvariantCulture) &&
                      origin.EndsWith(StagingClientOriginEnd, StringComparison.InvariantCulture)
             })
