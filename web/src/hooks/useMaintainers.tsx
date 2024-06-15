4a60b006cc3499fe23f6c3c9435dff3be37f2603
Collin Barrett <collinbarrett@users.noreply.github.com>
2024-06-15 09:18:22 -0500

    refactor(web): ♻️🔥 purge existing create-react-app site

--- web/src/hooks/useMaintainers.tsx
+++
@@ -1,7 +0,0 @@
-import { Maintainer } from "../interfaces/Maintainer";
-import { useApiData } from "./useApiData";
-
-export const useMaintainers = () =>
-  (
-    useApiData<Maintainer[]>("https://api.filterlists.com/maintainers") || []
-  ).sort((a: Maintainer, b: Maintainer) => a.name.localeCompare(b.name));
