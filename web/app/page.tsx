956b5320cfd56d1823abeebc3b8aee2269987f87
Collin Barrett <collinbarrett@users.noreply.github.com>
2024-06-15 14:43:55 -0500

    feat(web): ✨🚧 WIP scaffold FilterLists table

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,5 +1,23 @@
-export default function Home() {
+import { FilterList, columns } from "./columns";
+import { DataTable } from "../components/ui/data-table";
+
+async function getData(): Promise<FilterList[]> {
+  const response = await fetch("https://api.filterlists.com/lists");
+  if (!response.ok) {
+    throw new Error(`Failed to fetch data: ${response.statusText}`);
+  }
+  const data: FilterList[] = await response.json();
+  return data;
+}
+
+export default async function Home() {
+  const data = await getData();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="container mx-auto py-10">
+        <DataTable columns={columns} data={data} />
+      </div>
+    </main>
   );
 }
