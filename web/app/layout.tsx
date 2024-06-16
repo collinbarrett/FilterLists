e6e766ad71bcbe493d91c97f38dc5c973632f0a0
Collin Barrett <collinbarrett@users.noreply.github.com>
2024-06-16 18:02:41 -0500

    refactor(web): ♻️ misc. tidy

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Header } from "@/components/header";
+import { Header } from "@/components/header/header";
 import { metadata as siteMetadata } from "./metadata";
 
 const inter = Inter({ subsets: ["latin"] });
