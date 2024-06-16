8072092e54b1637378fa34a8bfcd863b2da57000
Collin Barrett <collinbarrett@users.noreply.github.com>
2024-06-15 20:12:02 -0500

    refactor(web): ♻️ set fixed row height

--- web/components/ui/data-table-pagination.tsx
+++ web/components/ui/data-table-pagination.tsx
@@ -24,10 +24,7 @@ export function DataTablePagination<TData>({
 }: DataTablePaginationProps<TData>) {
   return (
     <div className="flex items-center justify-between px-2">
-      <div className="flex-1 text-sm text-muted-foreground">
-        {table.getFilteredSelectedRowModel().rows.length} of{" "}
-        {table.getFilteredRowModel().rows.length} row(s) selected.
-      </div>
+      <div className="flex-1 text-sm text-muted-foreground"></div>
       <div className="flex items-center space-x-6 lg:space-x-8">
         <div className="flex items-center space-x-2">
           <p className="text-sm font-medium">Rows per page</p>
