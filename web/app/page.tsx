2327eb5363e6aba902b7c4a3b20ae4fb1e40d3af
Collin Barrett <collinbarrett@users.noreply.github.com>
2024-06-18 08:17:32 -0500

    feat(web): ✨🚧 adopt shadcn/ui dashboard-01 Block

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,5 +1,32 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { FilterListsTable } from "@/components/filterlists-table/filterlists-table";
 
 export default function Home() {
-  return <FilterListsTable />;
+  return (
+    <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
+      <Card
+        className="col-span-full w-full max-w-full overflow-hidden"
+        x-chunk="dashboard-01-chunk-4"
+      >
+        <CardHeader className="flex flex-row items-center">
+          <div className="grid gap-2">
+            <CardTitle>FilterLists</CardTitle>
+            <CardDescription>
+              The independent, comprehensive directory of filter and host lists
+              for advertisements, trackers, malware, and annoyances.
+            </CardDescription>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <FilterListsTable />
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
