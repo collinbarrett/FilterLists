ccc9f1b17e2cbaaf04591848c8f314bf8903c008
Collin Barrett <collinbarrett@users.noreply.github.com>
2024-06-18 08:17:32 -0500

    feat(web): ✨🚧 adopt shadcn/ui dashboard-01 Block

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -21,8 +21,12 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" enableSystem disableTransitionOnChange>
-          <Header />
-          <main>{children}</main>
+          <div className="flex min-h-screen w-full flex-col">
+            <Header />
+            <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+              {children}
+            </main>
+          </div>
         </ThemeProvider>
       </body>
     </html>
--- web/app/sitemap.ts
+++ web/app/sitemap.ts
@@ -8,5 +8,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "daily",
       priority: 1,
     },
+    {
+      url: "https://filterlists.com/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 2,
+    },
   ];
 }
