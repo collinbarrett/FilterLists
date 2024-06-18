bcfa100140503001906c765c65583b8d1c53f3f4
Collin Barrett <collinbarrett@users.noreply.github.com>
2024-06-18 08:17:32 -0500

    feat(web): ✨🚧 adopt shadcn/ui dashboard-01 Block

--- web/components/header/header.tsx
+++ web/components/header/header.tsx
@@ -1,20 +1,64 @@
 import * as React from "react";
 import Link from "next/link";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 
+import { Button } from "../ui/button";
 import { ModeToggle } from "../ui/mode-toggle";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
 import { Logo } from "./logo/logo";
 
 export function Header() {
   return (
     <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-      <nav className="flex flex-col gap-6 text-lg font-medium md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+      <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link
-          href="#"
+          href="/"
           className="flex items-center gap-2 text-lg font-semibold md:text-base"
         >
           <Logo />
+          <span className="sr-only">FilterLists</span>
+        </Link>
+        <Link
+          href="/"
+          className="text-foreground transition-colors hover:text-foreground"
+        >
+          FilterLists
+        </Link>
+        <Link
+          href="/about"
+          className="text-muted-foreground transition-colors hover:text-foreground"
+        >
+          About
         </Link>
       </nav>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="outline" size="icon" className="shrink-0 md:hidden">
+            <HamburgerMenuIcon className="h-5 w-5" />
+            <span className="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <nav className="grid gap-6 text-lg font-medium">
+            <Link
+              href="/"
+              className="flex items-center gap-2 text-lg font-semibold"
+            >
+              <Logo />
+              <span className="sr-only">FilterLists</span>
+            </Link>
+            <Link href="/" className="hover:text-foreground">
+              FilterLists
+            </Link>
+            <Link
+              href="/about"
+              className="text-muted-foreground hover:text-foreground"
+            >
+              About
+            </Link>
+          </nav>
+        </SheetContent>
+      </Sheet>
       <div className="ml-auto">
         <ModeToggle />
       </div>
