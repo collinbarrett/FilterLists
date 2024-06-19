88759aa200b57fc28203cc45d0694279c94b5fc6
Collin Barrett <collinbarrett@users.noreply.github.com>
2024-06-18 21:05:38 -0500

    feat(web): ✨🚧 WIP add button->modal
    ref https://github.com/vercel/nextgram

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -12,11 +12,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = siteMetadata;
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+export default function RootLayout(props: {
   children: React.ReactNode;
-}>) {
+  modal: React.ReactNode;
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
@@ -24,7 +23,9 @@ export default function RootLayout({
           <div className="flex min-h-screen w-full flex-col">
             <Header />
             <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-              {children}
+              {props.children}
+              {props.modal}
+              <div id="modal-root" />
             </main>
           </div>
         </ThemeProvider>
