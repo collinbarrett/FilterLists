5de5724cf5c9f0d55cb70e8213407cc8a4adbb22
Collin Barrett <collinbarrett@users.noreply.github.com>
2024-08-11 11:34:05 -0500

    chore(dir-data): migrate #4458

--- services/Directory/FilterLists.Directory.Infrastructure.Migrations/Migrations/20240811163339_4458.Designer.cs
+++ services/Directory/FilterLists.Directory.Infrastructure.Migrations/Migrations/20240811163339_4458.Designer.cs
@@ -3,6 +3,7 @@ using FilterLists.Directory.Infrastructure.Persistence.Queries.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FilterLists.Directory.Infrastructure.Migrations.Migrations
 {
     [DbContext(typeof(QueryDbContext))]
-    partial class QueryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240811163339_4458")]
+    partial class _4458
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
