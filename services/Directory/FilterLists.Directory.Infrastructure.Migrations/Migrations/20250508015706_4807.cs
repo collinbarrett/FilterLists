ebdf4a494eddb67c4db6e35cda61f085fb5e7622
Collin Barrett <6483057+collinbarrett@users.noreply.github.com>
2025-05-07 20:57:21 -0500

    chore: migrate #4807

---
+++ services/Directory/FilterLists.Directory.Infrastructure.Migrations/Migrations/20250508015706_4807.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilterLists.Directory.Infrastructure.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class _4807 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "FilterList",
+                columns: new[] { "Id", "ChatUrl", "Description", "DonateUrl", "EmailAddress", "ForumUrl", "HomeUrl", "IssuesUrl", "LicenseId", "Name", "OnionUrl", "PolicyUrl", "SubmissionUrl" },
+                values: new object[] { 2737, null, "A blocklist of Bolloré, alt-right and french fascist web sites.", null, null, null, "https://bloquebollore.codeberg.page/desarmons-bollore-et-l-extreme-droite.txt", "https://codeberg.org/BloqueBollore/pages/issues", 43, "French far-right blocklist", null, null, null });
+
+            migrationBuilder.InsertData(
+                table: "Maintainer",
+                columns: new[] { "Id", "EmailAddress", "Name", "TwitterHandle", "Url" },
+                values: new object[] { 199, null, "BloqueBollore", null, "https://codeberg.org/BloqueBollore/" });
+
+            migrationBuilder.InsertData(
+                table: "FilterListLanguage",
+                columns: new[] { "FilterListId", "LanguageId" },
+                values: new object[] { 2737, (short)47 });
+
+            migrationBuilder.InsertData(
+                table: "FilterListMaintainer",
+                columns: new[] { "FilterListId", "MaintainerId" },
+                values: new object[] { 2737, 199 });
+
+            migrationBuilder.InsertData(
+                table: "FilterListTag",
+                columns: new[] { "FilterListId", "TagId" },
+                values: new object[] { 2737, 25 });
+
+            migrationBuilder.InsertData(
+                table: "FilterListViewUrl",
+                columns: new[] { "FilterListId", "Id", "Primariness", "Url" },
+                values: new object[] { 2737, 3069, (short)1, "https://codeberg.org/BloqueBollore/pages/src/branch/main/desarmons-bollore-et-l-extreme-droite.txt" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "FilterListLanguage",
+                keyColumns: new[] { "FilterListId", "LanguageId" },
+                keyValues: new object[] { 2737, (short)47 });
+
+            migrationBuilder.DeleteData(
+                table: "FilterListMaintainer",
+                keyColumns: new[] { "FilterListId", "MaintainerId" },
+                keyValues: new object[] { 2737, 199 });
+
+            migrationBuilder.DeleteData(
+                table: "FilterListTag",
+                keyColumns: new[] { "FilterListId", "TagId" },
+                keyValues: new object[] { 2737, 25 });
+
+            migrationBuilder.DeleteData(
+                table: "FilterListViewUrl",
+                keyColumns: new[] { "FilterListId", "Id" },
+                keyValues: new object[] { 2737, 3069 });
+
+            migrationBuilder.DeleteData(
+                table: "FilterList",
+                keyColumn: "Id",
+                keyValue: 2737);
+
+            migrationBuilder.DeleteData(
+                table: "Maintainer",
+                keyColumn: "Id",
+                keyValue: 199);
+        }
+    }
+}
