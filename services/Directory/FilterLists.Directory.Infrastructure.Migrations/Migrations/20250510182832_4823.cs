3b578dec156ffbf3969ef8629a43a70359c32073
Collin Barrett <6483057+collinbarrett@users.noreply.github.com>
2025-05-10 13:29:27 -0500

    chore: migrate 4823

---
+++ services/Directory/FilterLists.Directory.Infrastructure.Migrations/Migrations/20250510182832_4823.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilterLists.Directory.Infrastructure.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class _4823 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "FilterList",
+                keyColumn: "Id",
+                keyValue: 2737,
+                column: "HomeUrl",
+                value: "https://codeberg.org/BloqueBollore/pages/src/branch/main/desarmons-bollore-et-l-extreme-droite.txt");
+
+            migrationBuilder.UpdateData(
+                table: "FilterListViewUrl",
+                keyColumns: new[] { "FilterListId", "Id" },
+                keyValues: new object[] { 2737, 3069 },
+                column: "Url",
+                value: "https://bloquebollore.codeberg.page/desarmons-bollore-et-l-extreme-droite.txt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "FilterList",
+                keyColumn: "Id",
+                keyValue: 2737,
+                column: "HomeUrl",
+                value: "https://bloquebollore.codeberg.page/desarmons-bollore-et-l-extreme-droite.txt");
+
+            migrationBuilder.UpdateData(
+                table: "FilterListViewUrl",
+                keyColumns: new[] { "FilterListId", "Id" },
+                keyValues: new object[] { 2737, 3069 },
+                column: "Url",
+                value: "https://codeberg.org/BloqueBollore/pages/src/branch/main/desarmons-bollore-et-l-extreme-droite.txt");
+        }
+    }
+}
