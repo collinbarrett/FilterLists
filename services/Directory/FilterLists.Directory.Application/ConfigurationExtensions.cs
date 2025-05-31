b6e51bea84173147693d547bcca9ae3d26e246e0
Collin Barrett <6483057+collinbarrett@users.noreply.github.com>
2025-05-30 19:28:01 -0500

    refactor(api): WIP rm mediatr

--- services/Directory/FilterLists.Directory.Application/ConfigurationExtensions.cs
+++ services/Directory/FilterLists.Directory.Application/ConfigurationExtensions.cs
@@ -1,5 +1,4 @@
 using FilterLists.Directory.Infrastructure;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace FilterLists.Directory.Application;
@@ -9,6 +8,5 @@ public static class ConfigurationExtensions
     public static void AddApplication(this IHostApplicationBuilder builder)
     {
         builder.AddInfrastructure();
-        builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ConfigurationExtensions).Assembly));
     }
 }
