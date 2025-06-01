df641bd4ef0aff0af5f9df451ce1bdec301b7944
Collin Barrett <6483057+collinbarrett@users.noreply.github.com>
2025-06-01 12:16:23 -0500

    Revert "refactor(api): loosen CORS to allow CF edge cache"
    This reverts commit ec1e006dc837079bb251d1dde6ee376ee8af73f9.

--- services/Directory/FilterLists.Directory.Api/CorsConfiguration.cs
+++ services/Directory/FilterLists.Directory.Api/CorsConfiguration.cs
@@ -4,11 +4,18 @@ namespace FilterLists.Directory.Api;
 
 internal static class CorsConfiguration
 {
-    internal static readonly Action<CorsOptions> SetupAction = options =>
-        options.AddDefaultPolicy(policy =>
-            policy
-                .AllowAnyOrigin()
-                .WithMethods("GET")
-                .AllowAnyHeader()
-        );
+    private const string ProductionClientOrigin = "https://filterlists.com";
+    private const string StagingClientOriginStart = "https://nice-water-05873140f";
+    private const string StagingClientOriginEnd = ".eastus2.5.azurestaticapps.net";
+    private const string LocalClientOrigin = "http://localhost:3000";
+
+    internal static readonly Action<CorsOptions> SetupAction = options => options.AddDefaultPolicy(policy =>
+    {
+        policy.SetIsOriginAllowed(origin =>
+                origin is ProductionClientOrigin or LocalClientOrigin ||
+                (origin.StartsWith(StagingClientOriginStart, StringComparison.InvariantCulture) &&
+                 origin.EndsWith(StagingClientOriginEnd, StringComparison.InvariantCulture)))
+            .WithMethods("GET")
+            .AllowAnyHeader();
+    });
 }
