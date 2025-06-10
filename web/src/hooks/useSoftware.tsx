9a099ad74bf44d0703845cf8b00d704528e2f385
Collin Barrett <6483057+collinbarrett@users.noreply.github.com>
2025-06-10 18:45:45 -0500

    chore(web): reset web dir

--- web/src/hooks/useSoftware.tsx
+++
@@ -1,7 +0,0 @@
-import { Software } from "../interfaces/Software";
-import { useApiData } from "./useApiData";
-
-export const useSoftware = () =>
-  (useApiData<Software[]>("https://api.filterlists.com/software") || []).sort(
-    (a: Software, b: Software) => a.name.localeCompare(b.name),
-  );
