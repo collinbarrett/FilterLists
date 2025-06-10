2680967b047f125a80b6d4e57d432177098e34be
Collin Barrett <6483057+collinbarrett@users.noreply.github.com>
2025-06-10 18:45:45 -0500

    chore(web): reset web dir

--- web/src/hooks/useLicenses.tsx
+++
@@ -1,7 +0,0 @@
-import { License } from "../interfaces/License";
-import { useApiData } from "./useApiData";
-
-export const useLicenses = () =>
-  (useApiData<License[]>("https://api.filterlists.com/licenses") || []).sort(
-    (a: License, b: License) => a.name.localeCompare(b.name),
-  );
