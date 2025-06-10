5d0387a543625379ab307f910d2b680b2fe67858
Collin Barrett <6483057+collinbarrett@users.noreply.github.com>
2025-06-10 18:45:45 -0500

    chore(web): reset web dir

--- web/src/interfaces/Software.ts
+++
@@ -1,9 +0,0 @@
-export interface Software {
-  id: number;
-  name: string;
-  description?: string;
-  homeUrl?: string;
-  downloadUrl?: string;
-  supportsAbpUrlScheme: boolean;
-  syntaxIds: number[];
-}
