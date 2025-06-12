e956bf1fcf36ea9ab7b47839ec116e18c7d7a6fb
Collin Barrett <6483057+collinbarrett@users.noreply.github.com>
2025-06-12 18:07:43 -0500

    feat(web): npm i prettier

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -10,7 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
 ];
 
 export default eslintConfig;
