9de10b666333e14ff1a62f0dacb47ff10ce13f5a
Collin Barrett <6483057+collinbarrett@users.noreply.github.com>
2025-06-14 14:40:52 -0500

    refactor(web): org assets

---
+++ web/src/components/footer/github-link/index.tsx
@@ -0,0 +1,24 @@
+import GitHubMarkLight from "./github-mark-light.svg";
+import GitHubMarkDark from "./github-mark-dark.svg";
+import { ImageThemed } from "@/components/image-themed";
+
+export function GitHubLink() {
+  return (
+    <a
+      className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+      href="https://github.com/collinbarrett/FilterLists"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <ImageThemed
+        aria-hidden
+        srcLight={GitHubMarkLight}
+        srcDark={GitHubMarkDark}
+        alt="GitHub Logomark"
+        width={16}
+        height={16}
+      />
+      GitHub
+    </a>
+  );
+}
