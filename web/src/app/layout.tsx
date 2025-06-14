a675b607f63e47f3f4eadf0c2743b164a200499c
Collin Barrett <6483057+collinbarrett@users.noreply.github.com>
2025-06-14 14:40:52 -0500

    refactor(web): org assets

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import { SiteHeader } from "@/components/site-header";
-import { SiteFooter } from "@/components/site-footer";
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,10 +37,10 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <SiteHeader />
+          <Header />
           <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             {children}
-            <SiteFooter />
+            <Footer />
           </div>
         </ThemeProvider>
       </body>
