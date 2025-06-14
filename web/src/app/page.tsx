4a9d58b7452513a70b2df375b213660a1d83e398
Collin Barrett <6483057+collinbarrett@users.noreply.github.com>
2025-06-14 15:22:16 -0500

    refactor(web): tweak layout

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,3 @@
 export default function Home() {
-  return (
-    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start"></main>
-  );
+  return <main className="p-8 gap-16 sm:p-20"></main>;
 }
