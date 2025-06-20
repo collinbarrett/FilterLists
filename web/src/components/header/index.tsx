3f5f5731f042afe20d1f3bc0292942b8236ea105
Collin Barrett <6483057+collinbarrett@users.noreply.github.com>
2025-06-20 05:21:45 -0500

    feat(ui): make logo a home link

--- web/src/components/header/index.tsx
+++ web/src/components/header/index.tsx
@@ -1,5 +1,6 @@
 import { ImageThemed } from "@/components/image-themed";
 import { ThemeToggle } from "../theme-toggle";
+import Link from "next/link";
 import LogoLight from "./logo-filterlists-light.png";
 import logoDark from "./logo-filterlists-dark.png";
 import styles from "./logoHeader.module.css";
@@ -9,15 +10,17 @@ export function Header() {
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="flex h-20 items-center justify-between px-6">
         <div className="flex items-center">
-          <ImageThemed
-            className={styles.logoHeader}
-            srcLight={LogoLight}
-            srcDark={logoDark}
-            alt="FilterLists logo"
-            width={429}
-            height={100}
-            priority
-          />
+          <Link href="/" aria-label="Go to homepage">
+            <ImageThemed
+              className={styles.logoHeader}
+              srcLight={LogoLight}
+              srcDark={logoDark}
+              alt="FilterLists logo"
+              width={429}
+              height={100}
+              priority
+            />
+          </Link>
         </div>
         <div className="flex items-center">
           <ThemeToggle />
