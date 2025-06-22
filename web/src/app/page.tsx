1c5027bb1c2b0d2706f7621e0d9624b21dd6bb1a
Collin Barrett <6483057+collinbarrett@users.noreply.github.com>
2025-06-22 16:49:28 -0500

    refactor(web): client cache FilterLists data

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,19 +1,22 @@
 import { FilterListTable } from "@/components/filterlist-table";
 import { columns, FilterList } from "@/components/filterlist-table/columns";
 
-async function getFilterLists(): Promise<FilterList[]> {
-  const filterLists = await fetch("https://api.filterlists.com/lists", {
+async function getInitialFilterLists(): Promise<FilterList[]> {
+  const response = await fetch("https://api.filterlists.com/lists", {
     next: { revalidate: 86400 },
   });
-  return await filterLists.json();
+  const lists = await response.json();
+
+  // TODO: add pagination support to API
+  return lists.slice(0, 20);
 }
 
 export default async function Home() {
-  const filterLists = await getFilterLists();
+  const initialData = await getInitialFilterLists();
 
   return (
     <main className="pt-4 p-8 gap-16 sm:p-20 sm:pt-8">
-      <FilterListTable columns={columns} data={filterLists} />
+      <FilterListTable columns={columns} initialData={initialData} />
     </main>
   );
 }
---
+++ web/src/components/filterlist-table/pagination.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { Button } from "@/components/ui/button";
+import { Table } from "@tanstack/react-table";
+import { FilterList } from "./columns";
+
+interface PaginationProps {
+  table: Table<FilterList>;
+}
+
+export function FilterListTablePagination({ table }: PaginationProps) {
+  return (
+    <div className="flex items-center justify-between gap-2">
+      <Button
+        variant="outline"
+        size="sm"
+        onClick={() => table.previousPage()}
+        disabled={!table.getCanPreviousPage()}
+      >
+        Previous
+      </Button>
+      <span className="text-sm">
+        Page {table.getState().pagination.pageIndex + 1} of{" "}
+        {table.getPageCount()}
+      </span>
+      <Button
+        variant="outline"
+        size="sm"
+        onClick={() => table.nextPage()}
+        disabled={!table.getCanNextPage()}
+      >
+        Next
+      </Button>
+    </div>
+  );
+}
