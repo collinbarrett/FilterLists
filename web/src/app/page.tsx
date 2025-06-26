62e8a5a0ebe3b0507079f5360bdf48401f8ecc26
Collin Barrett <6483057+collinbarrett@users.noreply.github.com>
2025-06-25 19:51:32 -0500

    refactor(web): extract getFilterLists, misc re-org

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,18 +1,12 @@
 import { FilterListTable } from "@/components/filterlist-table";
-import { columns, FilterList } from "@/components/filterlist-table/columns";
-
-async function getInitialFilterLists(): Promise<FilterList[]> {
-  const response = await fetch("https://api.filterlists.com/lists", {
-    next: { revalidate: 86400 },
-  });
-  const lists = await response.json();
-
-  // TODO: add pagination support to API
-  return lists.slice(0, 20);
-}
+import { columns } from "@/components/filterlist-table/columns";
+import { getFilterLists } from "@/services/get-filterlists";
 
 export default async function Home() {
-  const initialData = await getInitialFilterLists();
+  const filterLists = await getFilterLists();
+
+  // TODO: add pagination support to API
+  const initialData = filterLists.slice(0, 20);
 
   return (
     <main className="pt-4 p-8 gap-16 sm:p-20 sm:pt-8">
