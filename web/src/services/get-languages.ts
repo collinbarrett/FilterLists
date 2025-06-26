6845bcc92f7bbe69a86d5329779fd39aa1fef0d9
Collin Barrett <6483057+collinbarrett@users.noreply.github.com>
2025-06-25 20:02:43 -0500

    feat(web): scaffold API clients

---
+++ web/src/services/get-languages.ts
@@ -0,0 +1,20 @@
+import { API_BASE_URL, DEFAULT_REVALIDATE_SECS } from "./constants";
+
+export type Language = {
+  id: number;
+  iso6391: string | null;
+  name: string | null;
+  filterListIds: number[] | null;
+};
+
+export async function getLanguages(): Promise<Language[]> {
+  const response = await fetch(`${API_BASE_URL}/languages`, {
+    next: { revalidate: DEFAULT_REVALIDATE_SECS },
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch languages: ${response.statusText}`);
+  }
+
+  return await response.json();
+}
