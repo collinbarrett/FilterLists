88bef565c4d00c646455c5109466eb14a67beed8
Collin Barrett <6483057+collinbarrett@users.noreply.github.com>
2025-06-25 21:20:45 -0500

    feat(web): fetch related data

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,16 +1,47 @@
 import { FilterListTable } from "@/components/filterlist-table";
 import { columns } from "@/components/filterlist-table/columns";
 import { getFilterLists } from "@/services/get-filterlists";
+import { getLanguages } from "@/services/get-languages";
+import { getLicenses } from "@/services/get-licenses";
+import { getMaintainers } from "@/services/get-maintainers";
+import { getSoftware } from "@/services/get-software";
+import { getSyntaxes } from "@/services/get-syntaxes";
+import { getTags } from "@/services/get-tags";
 
 export default async function Home() {
-  const filterLists = await getFilterLists();
+  const [
+    filterLists,
+    languages,
+    licenses,
+    maintainers,
+    software,
+    syntaxes,
+    tags,
+  ] = await Promise.all([
+    getFilterLists(),
+    getLanguages(),
+    getLicenses(),
+    getMaintainers(),
+    getSoftware(),
+    getSyntaxes(),
+    getTags(),
+  ]);
 
   // TODO: add pagination support to API
-  const initialData = filterLists.slice(0, 20);
+  const initialFilterLists = filterLists.slice(0, 20);
 
   return (
     <main className="pt-4 p-8 gap-16 sm:p-20 sm:pt-8">
-      <FilterListTable columns={columns} initialData={initialData} />
+      <FilterListTable
+        columns={columns}
+        initialFilterLists={initialFilterLists}
+        languages={languages}
+        licenses={licenses}
+        maintainers={maintainers}
+        software={software}
+        syntaxes={syntaxes}
+        tags={tags}
+      />
     </main>
   );
 }
