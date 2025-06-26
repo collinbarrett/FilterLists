24799ca806c21e5d89703e59ea4637ac1a7c3c8a
Collin Barrett <6483057+collinbarrett@users.noreply.github.com>
2025-06-26 17:56:52 -0500

    feat(web): add licenses column

--- web/src/components/filterlist-table/columns.ts
+++ web/src/components/filterlist-table/columns.ts
@@ -1,13 +1,27 @@
+"use client";
+
 import { FilterList } from "@/services/get-filterlists";
 import { createColumnHelper } from "@tanstack/react-table";
+import { FilterListsTableMeta } from ".";
 
 const columnHelper = createColumnHelper<FilterList>();
 
 export const columns = [
-  columnHelper.accessor("name", {
+  columnHelper.accessor((row) => row.name as string, {
+    id: "name",
     header: "Name",
   }),
-  columnHelper.accessor("description", {
+  columnHelper.accessor((row) => row.description as string, {
+    id: "description",
     header: "Description",
+  }),
+  columnHelper.accessor((row) => row.licenseId as number | null, {
+    id: "licenseId",
+    header: "License",
+    cell: (info) => {
+      const meta = info.table.options.meta as FilterListsTableMeta;
+      const license = meta.licenses?.find((l) => l.id === info.getValue());
+      return license?.name;
+    },
   }),
 ];
