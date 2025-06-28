e646f1bcbc705b3e5454be7e58410a6fccaae739
Collin Barrett <6483057+collinbarrett@users.noreply.github.com>
2025-06-28 13:51:24 -0500

    refactor(web): tweak header and footer

--- web/src/components/footer/collinmbarrett-link/index.tsx
+++ web/src/components/footer/collinmbarrett-link/index.tsx
@@ -4,21 +4,23 @@ import { ThemeImage } from "@/components/theme-image";
 
 export function CollinMBarrettLink() {
   return (
-    <a
-      className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-      href="https://collinmbarrett.com/"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <ThemeImage
-        aria-hidden
-        srcLight={CollinMBarrettIconLight}
-        srcDark={CollinMBarrettIconDark}
-        alt="Collin M. Barrett logo"
-        width={16}
-        height={16}
-      />
-      by Collin M. Barrett
-    </a>
+    <div className="text-sm sm:text-base">
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href="https://collinmbarrett.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <ThemeImage
+          aria-hidden
+          srcLight={CollinMBarrettIconLight}
+          srcDark={CollinMBarrettIconDark}
+          alt="Collin M. Barrett logo"
+          width={16}
+          height={16}
+        />
+        by Collin M. Barrett
+      </a>
+    </div>
   );
 }
