be6480f394ed1860c32ff1ff2ec62ebb8b74c613
Collin Barrett <6483057+collinbarrett@users.noreply.github.com>
2025-06-28 12:41:01 -0500

    refactor(api): rm unused prop

--- services/Directory/FilterLists.Directory.Application/Queries/GetLists.cs
+++ services/Directory/FilterLists.Directory.Application/Queries/GetLists.cs
@@ -26,11 +26,6 @@ public static class GetLists
                     f.FilterListTags
                         .OrderBy(ft => ft.TagId)
                         .Select(ft => ft.TagId),
-                    f.ViewUrls
-                        .OrderBy(u => u.SegmentNumber)
-                        .ThenBy(u => u.Primariness)
-                        .Select(u => u.Url)
-                        .FirstOrDefault(),
                     f.FilterListMaintainers
                         .OrderBy(fm => fm.MaintainerId)
                         .Select(fm => fm.MaintainerId)
@@ -50,7 +45,6 @@ public static class GetLists
     /// <param name="SyntaxIds" example="[ 3 ]">The identifiers of the Syntaxes implemented by this FilterList.</param>
     /// <param name="LanguageIds" example="[ 37 ]">The identifiers of the Languages targeted by this FilterList.</param>
     /// <param name="TagIds" example="[ 2 ]">The identifiers of the Tags applied to this FilterList.</param>
-    /// <param name="PrimaryViewUrl" example="https://easylist.to/easylist/easylist.txt">The primary view URL.</param>
     /// <param name="MaintainerIds" example="[ 7 ]">The identifiers of the Maintainers of this FilterList.</param>
     public sealed record Response(
         int Id,
@@ -60,6 +54,5 @@ public static class GetLists
         IEnumerable<short> SyntaxIds,
         IEnumerable<short> LanguageIds,
         IEnumerable<int> TagIds,
-        Uri? PrimaryViewUrl,
         IEnumerable<int> MaintainerIds);
 }
