47ca5a848997914587ffc9589489990294312b2a
Collin Barrett <6483057+collinbarrett@users.noreply.github.com>
2025-06-28 13:51:24 -0500

    refactor(web): tweak header and footer

--- web/src/components/footer/github-link/index.tsx
+++ web/src/components/footer/github-link/index.tsx
@@ -4,21 +4,23 @@ import { ThemeImage } from "@/components/theme-image";
 
 export function GitHubLink() {
   return (
-    <a
-      className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-      href="https://github.com/collinbarrett/FilterLists"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <ThemeImage
-        aria-hidden
-        srcLight={GitHubMarkLight}
-        srcDark={GitHubMarkDark}
-        alt="GitHub Logomark"
-        width={16}
-        height={16}
-      />
-      GitHub
-    </a>
+    <div className="text-sm sm:text-base">
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href="https://github.com/collinbarrett/FilterLists"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <ThemeImage
+          aria-hidden
+          srcLight={GitHubMarkLight}
+          srcDark={GitHubMarkDark}
+          alt="GitHub Logomark"
+          width={16}
+          height={16}
+        />
+        GitHub
+      </a>
+    </div>
   );
 }
