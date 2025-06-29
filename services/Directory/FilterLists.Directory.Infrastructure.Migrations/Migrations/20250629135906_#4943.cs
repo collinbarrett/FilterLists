ed7bcc3fca4435377fd81587f332ba51055c1844
Collin Barrett <6483057+collinbarrett@users.noreply.github.com>
2025-06-29 09:00:11 -0500

    chore: migrate #4943

---
+++ services/Directory/FilterLists.Directory.Infrastructure.Migrations/Migrations/20250629135906_#4943.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilterLists.Directory.Infrastructure.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class _4943 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Maintainer",
+                keyColumn: "Id",
+                keyValue: 196,
+                column: "Url",
+                value: "https://তৌহিদুর.বাংলা/");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Maintainer",
+                keyColumn: "Id",
+                keyValue: 196,
+                column: "Url",
+                value: "তৌহিদুর.বাংলা");
+        }
+    }
+}
